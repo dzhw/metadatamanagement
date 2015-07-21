7de7a4d4a8d9e7ef4833409cd16513992c6447b0
Amine limouri <limouri@dzhw.eu>
2015-07-21 15:38:44 +0200

    Added resources to the application

--- src/main/java/eu/dzhw/fdz/metadatamanagement/MetaDataManagementApplication.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/MetaDataManagementApplication.java
@@ -1,5 +1,7 @@
 package eu.dzhw.fdz.metadatamanagement;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MetaDataManagementApplication {
 
     public static void main(String[] args) {
+    	System.setProperty("info.app.startuptime", new Date().toString());
         SpringApplication.run(MetaDataManagementApplication.class, args);
     }
 }
