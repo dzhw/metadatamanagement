dfa71b06a3d6fe54db670cb73b382f6246927be8
René Reitmann <reitmann@dzhw.eu>
2015-07-21 09:59:29 +0200

    Created Spring Boot Maven Project

---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/MetaDataManagementApplication.java
@@ -0,0 +1,12 @@
+package eu.dzhw.fdz.metadatamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaDataManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaDataManagementApplication.class, args);
+    }
+}
---
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/MetaDataManagementApplicationSmokeTest.java
@@ -0,0 +1,18 @@
+package eu.dzhw.fdz.metadatamanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MetaDataManagementApplication.class)
+@WebAppConfiguration
+public class MetaDataManagementApplicationSmokeTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
