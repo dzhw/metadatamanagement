2dbd87d3df1d4dd837a7f461c9f683c00623fac4
Amine limouri <limouri@dzhw.eu>
2015-07-22 17:17:10 +0200

    Changed structur of html files

---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/web/VariablesController.java
@@ -0,0 +1,23 @@
+package eu.dzhw.fdz.metadatamanagement.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.async.DeferredResult;
+
+/**
+ * @author Amine Limouri
+ */
+@Controller
+public class VariablesController {
+    
+    /**
+     * Show variables page
+     * @return variables.html
+     */
+    @RequestMapping("/variables")
+    public DeferredResult<String> showVariablesFile() {
+	DeferredResult<String> result = new DeferredResult<String>();
+	result.setResult("variables/variables");
+	return result;
+    }
+}
