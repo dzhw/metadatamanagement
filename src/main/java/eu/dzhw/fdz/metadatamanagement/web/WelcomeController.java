e80b497035a2050fe17471caa9f03cdea9af9914
René Reitmann <reitmann@dzhw.eu>
2015-07-23 11:26:19 +0200

    Moved request URL to class level annotation.

--- src/main/java/eu/dzhw/fdz/metadatamanagement/web/WelcomeController.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/web/WelcomeController.java
@@ -2,21 +2,25 @@ package eu.dzhw.fdz.metadatamanagement.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.async.DeferredResult;
 
 /**
  * Simple Controller which returns a welcome page.
+ * 
  * @author René Reitmann
  */
 @Controller
+@RequestMapping("/")
 public class WelcomeController {
 
   /**
    * Show welcome page.
+   * 
    * @return welcome.html
    */
-  @RequestMapping("/")
-  public final DeferredResult<String> showWelcomeFile() {
+  @RequestMapping(method = RequestMethod.GET)
+  public final DeferredResult<String> showWelcomePage() {
     DeferredResult<String> result = new DeferredResult<String>();
     result.setResult("welcome");
     return result;
