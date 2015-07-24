2f7abb59c1fa345de7afffd90a5dfd7a3c4033dc
Daniel Katzberg <katzberg@dzhw.eu>
2015-07-24 12:20:01 +0200

    added empty unit test file for variable

---
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/domain/variablemanagement/VariableTest.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package eu.dzhw.fdz.metadatamanagement.domain.variablemanagement;
+
+/**
+ * @author Daniel Katzberg
+ *
+ */
+//TODO prepare unit tests for the variable
+public class VariableTest {
+
+}
