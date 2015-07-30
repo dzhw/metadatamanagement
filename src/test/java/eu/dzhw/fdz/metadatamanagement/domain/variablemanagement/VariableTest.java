401aceb128f7ca37c8fd8cb7464a9b021deb03c6
Daniel Katzberg <katzberg@dzhw.eu>
2015-07-30 10:37:45 +0200

    extended and added junit tests for better code coverage

--- src/test/java/eu/dzhw/fdz/metadatamanagement/domain/variablemanagement/VariableTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/domain/variablemanagement/VariableTest.java
@@ -32,12 +32,12 @@ public class VariableTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(VariableTest.class);
 
-  private static Validator validator;
+  private static Validator VALIDATOR;
 
   @BeforeClass
   public static void setUpValidatorAndDates() {
     ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-    validator = validatorFactory.getValidator();
+    VALIDATOR = validatorFactory.getValidator();
   }
 
   @Test
@@ -45,7 +45,7 @@ public class VariableTest {
 
     Variable variable = new Variable();
     variable.setFdzId("");
-    Set<ConstraintViolation<Variable>> variableVialations = validator.validate(variable);
+    Set<ConstraintViolation<Variable>> variableVialations = VALIDATOR.validate(variable);
 
     // two empty field
     assertEquals(2, variableVialations.size());
@@ -63,7 +63,7 @@ public class VariableTest {
     variable.setFdzId("ThisIDisOkay.");
     variable.setName("ThisNameIsOkay.");
 
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // now is everything okay
     assertEquals(0, variableVialations.size());
@@ -76,7 +76,7 @@ public class VariableTest {
     Variable variable = new Variable();
     variable.setFdzId("ThisIDisOkay.");
     variable.setName("ThisNameIsTooLongAndThrowAnException.");
-    Set<ConstraintViolation<Variable>> variableVialations = validator.validate(variable);
+    Set<ConstraintViolation<Variable>> variableVialations = VALIDATOR.validate(variable);
 
     // one field for the name is too long error
     assertEquals(1, variableVialations.size());
@@ -92,7 +92,7 @@ public class VariableTest {
 
     variable.setName("ThisNameIsOkay.");
 
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // now is everything okay
     assertEquals(0, variableVialations.size());
@@ -106,7 +106,7 @@ public class VariableTest {
     variable.setName("ThisNameIsOkay.");
     variable.setLabel("ThisLabelIsNotOkay.ItIsTooLongAndThrowsAnException."
         + "ButTheLabelLengthIsVeryLong.ItNeedsManyWordsForTheException.");
-    Set<ConstraintViolation<Variable>> variableVialations = validator.validate(variable);
+    Set<ConstraintViolation<Variable>> variableVialations = VALIDATOR.validate(variable);
 
     // one field for the name is too long error
     assertEquals(1, variableVialations.size());
@@ -121,7 +121,7 @@ public class VariableTest {
     }
 
     variable.setLabel("ThisLabelIsOkay.");
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // now is everything okay
     assertEquals(0, variableVialations.size());
@@ -135,7 +135,7 @@ public class VariableTest {
     variable.setName("ThisNameIsOkay.");
     variable.setDataType(DataType.STRING);
     variable.setScaleLevel(ScaleLevel.ORDINAL);
-    Set<ConstraintViolation<Variable>> variableVialations = validator.validate(variable);
+    Set<ConstraintViolation<Variable>> variableVialations = VALIDATOR.validate(variable);
 
     // no errors. enum fields are okay
     assertEquals(0, variableVialations.size());
@@ -157,7 +157,7 @@ public class VariableTest {
     answerOptions.add(answerOption);
 
     variable.setAnswerOptions(answerOptions);
-    Set<ConstraintViolation<Variable>> variableVialations = validator.validate(variable);
+    Set<ConstraintViolation<Variable>> variableVialations = VALIDATOR.validate(variable);
 
     // one field for the name is too long error
     // needed label
@@ -184,7 +184,7 @@ public class VariableTest {
     variable.getAnswerOptions().get(0)
         .setLabel("AddAExtraLabelForAnTestValidationError.AddAExtraLabelForAnTestValidationError.");
     variable.getAnswerOptions().get(0).setCode("This code is okay.");
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // one field for the name is too long error
     assertEquals(1, variableVialations.size());
@@ -199,7 +199,7 @@ public class VariableTest {
     }
 
     variable.getAnswerOptions().get(0).setLabel("AddAExtraLabelForAnTestValidationError");
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // now is everything okay
     assertEquals(0, variableVialations.size());
@@ -218,7 +218,7 @@ public class VariableTest {
     VariableSurvey variableSurvey = new VariableSurvey();
 
     variable.setVariableSurvey(variableSurvey);
-    Set<ConstraintViolation<Variable>> variableVialations = validator.validate(variable);
+    Set<ConstraintViolation<Variable>> variableVialations = VALIDATOR.validate(variable);
 
     // one field for the name is too long error
     assertEquals(2, variableVialations.size());
@@ -234,7 +234,7 @@ public class VariableTest {
 
     variable.getVariableSurvey().setSurveyId("SurveyIdIsTooLong.ItWillThrowAnException");
     variable.getVariableSurvey().setTitle("TitleIsOkay.");
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // one field for the survey id is too long error
     assertEquals(1, variableVialations.size());
@@ -250,7 +250,7 @@ public class VariableTest {
 
     variable.getVariableSurvey().setSurveyId("SurveyIdIsOkay.");
     variable.getVariableSurvey().setTitle("TitleIsNotOkay.TheTitleIsTooLong.");
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // one field for the title is too long error
     assertEquals(1, variableVialations.size());
@@ -265,7 +265,7 @@ public class VariableTest {
     }
     variable.getVariableSurvey().setTitle("TitleIsOkay.");
 
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // now is everything okay
     assertEquals(0, variableVialations.size());
@@ -289,7 +289,7 @@ public class VariableTest {
     variableSurvey.setDateRange(dateRange);
 
     variable.setVariableSurvey(variableSurvey);
-    Set<ConstraintViolation<Variable>> variableVialations = validator.validate(variable);
+    Set<ConstraintViolation<Variable>> variableVialations = VALIDATOR.validate(variable);
 
     // one field for the name is too long error
     assertEquals(1, variableVialations.size());
@@ -307,7 +307,7 @@ public class VariableTest {
     variable.getVariableSurvey().getDateRange().setEndDate(dateRange.getStartDate().plusDays(2));
 
 
-    variableVialations = validator.validate(variable);
+    variableVialations = VALIDATOR.validate(variable);
 
     // now is everything okay
     assertEquals(0, variableVialations.size());
@@ -321,8 +321,9 @@ public class VariableTest {
         "Survey [fdzId=null, null, name=null, dataType=null, label=null, scaleLevel=null, AnswerOptions.size=0]",
         variable.toString());
 
-    // Variable with VariableSurvey
+    // Variable with VariableSurvey and answerOptions
     variable.setVariableSurvey(new VariableSurvey());
+    variable.setAnswerOptions(new ArrayList<>());
     assertEquals("Survey [fdzId=null, SurveyVariableSurvey [surveyId=null, "
         + "title=null, null], name=null, dataType=null, label=null, "
         + "scaleLevel=null, AnswerOptions.size=0]", variable.toString());
@@ -334,5 +335,33 @@ public class VariableTest {
             + "title=null, DateRange [StartDate=null, EndDate=null]], name=null, "
             + "dataType=null, label=null, scaleLevel=null, AnswerOptions.size=0]",
         variable.toString());
+    
+    variable.getVariableSurvey().getDateRange().setStartDate(LocalDate.of(2015, 1, 1));
+    variable.getVariableSurvey().getDateRange().setEndDate(LocalDate.of(2015, 2, 1));
+    assertEquals(
+        "Survey [fdzId=null, SurveyVariableSurvey [surveyId=null, title=null, "
+        + "DateRange [StartDate=2015-01-01, EndDate=2015-02-01]], name=null, dataType=null, "
+        + "label=null, scaleLevel=null, AnswerOptions.size=0]",
+        variable.toString());
+  }  
+  
+  @Test
+  public void testEmptyDateRangeValidator(){
+    
+    Variable variable = new Variable();
+    variable.setFdzId("ID");
+    variable.setName("name");
+    variable.setVariableSurvey(new VariableSurvey());
+    variable.getVariableSurvey().setSurveyId("ID_Survey");
+    variable.getVariableSurvey().setTitle("TitleIsOkay.");
+    
+    //set a date, but without a start and end date
+    variable.getVariableSurvey().setDateRange(new DateRange());
+    
+    
+    Set<ConstraintViolation<Variable>> variableVialations = VALIDATOR.validate(variable);
+
+    // everything okay
+    assertEquals(0, variableVialations.size());
   }
 }
