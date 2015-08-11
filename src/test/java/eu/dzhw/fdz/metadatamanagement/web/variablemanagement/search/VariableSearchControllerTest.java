2c92b514292dc05eff88314bf42fbe039341ef52
René Reitmann <reitmann@dzhw.eu>
2015-08-11 11:41:34 +0200

    Added i18n test to VariableSearchControllerTest

--- src/test/java/eu/dzhw/fdz/metadatamanagement/web/variablemanagement/search/VariableSearchControllerTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/web/variablemanagement/search/VariableSearchControllerTest.java
@@ -1,6 +1,7 @@
 package eu.dzhw.fdz.metadatamanagement.web.variablemanagement.search;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNot.not;
@@ -26,38 +27,59 @@ import eu.dzhw.fdz.metadatamanagement.web.AbstractWebTest;
 public class VariableSearchControllerTest extends AbstractWebTest {
 
   @Test
-  public void testTemplate() throws Exception {
-    MvcResult mvcResult =
-        this.mockMvc.perform(get("/de/variables/search")).andExpect(status().isOk())
-            .andExpect(request().asyncStarted())
-            .andExpect(request().asyncResult(instanceOf(ModelAndView.class))).andReturn();
+  public void testGermanTemplate() throws Exception {
+    MvcResult mvcResult = this.mockMvc.perform(get("/de/variables/search"))
+        .andExpect(status().isOk()).andExpect(request().asyncStarted())
+        .andExpect(request().asyncResult(instanceOf(ModelAndView.class))).andReturn();
 
     this.mockMvc.perform(asyncDispatch(mvcResult)).andExpect(status().isOk())
         .andExpect(content().string((containsString("Sprache"))))
         .andExpect(content().string(not(containsString("#{"))))
         .andExpect(content().string(not(containsString("${"))))
         .andExpect(content().string(not(containsString("??"))));
+
+    VariableSearchPageResource resource =
+        (VariableSearchPageResource) ((ModelAndView) mvcResult.getAsyncResult()).getModelMap()
+            .get("resource");
+    assertThat(resource.getPage().getContent().size(), is(greaterThan(0)));
+  }
+
+  @Test
+  public void testEnglishTemplate() throws Exception {
+    MvcResult mvcResult = this.mockMvc.perform(get("/en/variables/search"))
+        .andExpect(status().isOk()).andExpect(request().asyncStarted())
+        .andExpect(request().asyncResult(instanceOf(ModelAndView.class))).andReturn();
+
+    this.mockMvc.perform(asyncDispatch(mvcResult)).andExpect(status().isOk())
+        .andExpect(content().string((containsString("Language"))))
+        .andExpect(content().string(not(containsString("#{"))))
+        .andExpect(content().string(not(containsString("${"))))
+        .andExpect(content().string(not(containsString("??"))));
+
+    VariableSearchPageResource resource =
+        (VariableSearchPageResource) ((ModelAndView) mvcResult.getAsyncResult()).getModelMap()
+            .get("resource");
+    assertThat(resource.getPage().getContent().size(), is(greaterThan(0)));
   }
 
   @Test
   public void testSearch() throws Exception {
-    MvcResult mvcResult =
-        this.mockMvc.perform(get("/de/variables/search?query=ALLBUS")).andExpect(status().isOk())
-            .andExpect(request().asyncStarted())
-            .andExpect(request().asyncResult(instanceOf(ModelAndView.class))).andReturn();
+    MvcResult mvcResult = this.mockMvc.perform(get("/de/variables/search?query=ALLBUS"))
+        .andExpect(status().isOk()).andExpect(request().asyncStarted())
+        .andExpect(request().asyncResult(instanceOf(ModelAndView.class))).andReturn();
 
     ModelAndViewAssert.assertViewName((ModelAndView) mvcResult.getAsyncResult(),
         "variables/variableSearch");
-    ModelAndViewAssert.assertModelAttributeValue((ModelAndView) mvcResult.getAsyncResult(),
-        "query", "ALLBUS");
+    ModelAndViewAssert.assertModelAttributeValue((ModelAndView) mvcResult.getAsyncResult(), "query",
+        "ALLBUS");
     ModelAndViewAssert.assertModelAttributeAvailable((ModelAndView) mvcResult.getAsyncResult(),
         "resource");
     ModelAndViewAssert.assertAndReturnModelAttributeOfType(
         (ModelAndView) mvcResult.getAsyncResult(), "resource", VariableSearchPageResource.class);
 
     VariableSearchPageResource resource =
-        (VariableSearchPageResource) ((ModelAndView) mvcResult.getAsyncResult()).getModelMap().get(
-            "resource");
+        (VariableSearchPageResource) ((ModelAndView) mvcResult.getAsyncResult()).getModelMap()
+            .get("resource");
     assertThat(resource.getPage().getContent().size(), is(8));
   }
 
@@ -70,27 +92,24 @@ public class VariableSearchControllerTest extends AbstractWebTest {
 
     ModelAndViewAssert.assertViewName((ModelAndView) mvcResult.getAsyncResult(),
         "variables/variableSearch");
-    ModelAndViewAssert.assertModelAttributeValue((ModelAndView) mvcResult.getAsyncResult(),
-        "query", "ALLBUS");
+    ModelAndViewAssert.assertModelAttributeValue((ModelAndView) mvcResult.getAsyncResult(), "query",
+        "ALLBUS");
     ModelAndViewAssert.assertModelAttributeAvailable((ModelAndView) mvcResult.getAsyncResult(),
         "resource");
     ModelAndViewAssert.assertAndReturnModelAttributeOfType(
         (ModelAndView) mvcResult.getAsyncResult(), "resource", VariableSearchPageResource.class);
 
     VariableSearchPageResource resource =
-        (VariableSearchPageResource) ((ModelAndView) mvcResult.getAsyncResult()).getModelMap().get(
-            "resource");
+        (VariableSearchPageResource) ((ModelAndView) mvcResult.getAsyncResult()).getModelMap()
+            .get("resource");
 
     assertThat(resource.getPage().getContent().size(), is(3));
-    assertThat(
-        resource.getPage().getId().getHref()
-            .contains("/de/variables/search?query=ALLBUS&page=1&size=3"), is(true));
-    assertThat(
-        resource.getPage().getPreviousLink().getHref()
-            .contains("/de/variables/search?query=ALLBUS&page=0&size=3"), is(true));
-    assertThat(
-        resource.getPage().getNextLink().getHref()
-            .contains("/de/variables/search?query=ALLBUS&page=2&size=3"), is(true));
+    assertThat(resource.getPage().getId().getHref()
+        .contains("/de/variables/search?query=ALLBUS&page=1&size=3"), is(true));
+    assertThat(resource.getPage().getPreviousLink().getHref()
+        .contains("/de/variables/search?query=ALLBUS&page=0&size=3"), is(true));
+    assertThat(resource.getPage().getNextLink().getHref()
+        .contains("/de/variables/search?query=ALLBUS&page=2&size=3"), is(true));
 
   }
 }
