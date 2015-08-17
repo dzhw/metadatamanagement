0d6d36fe2c6ae604841d435ac6e3048d8d30d046
Daniel Katzberg <katzberg@dzhw.eu>
2015-08-17 14:54:43 +0200

    reorganized unit tests for modify controller. added one new test

--- src/test/java/eu/dzhw/fdz/metadatamanagement/web/variablemanagement/modify/VariableModifyControllerTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/web/variablemanagement/modify/VariableModifyControllerTest.java
@@ -5,35 +5,39 @@ import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.servlet.ModelAndView;
 
-import eu.dzhw.fdz.metadatamanagement.data.common.documents.DateRange;
-import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.AnswerOption;
 import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.VariableDocument;
-import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.VariableSurvey;
+import eu.dzhw.fdz.metadatamanagement.service.variablemanagement.VariableService;
 import eu.dzhw.fdz.metadatamanagement.web.AbstractWebTest;
 
-
+/**
+ * 
+ * @author Daniel Katzberg
+ * @author Amine Limouri
+ *
+ */
 public class VariableModifyControllerTest extends AbstractWebTest {
+  
+  @Autowired
+  private VariableService variableService;
 
   @Test
-  public void testgetForm() throws Exception {
-    MvcResult mvcResult =
-        this.mockMvc.perform(get("/de/variables/create")).andExpect(status().isOk())
-            .andExpect(request().asyncStarted())
-            .andExpect(request().asyncResult(instanceOf(ModelAndView.class))).andReturn();
+  public void testGetForm() throws Exception {
+    // Check the Requestpath of the VariableModifyControllerPath
+    MvcResult mvcResult = this.mockMvc.perform(get("/de/variables/create"))
+        .andExpect(status().isOk()).andExpect(request().asyncStarted())
+        .andExpect(request().asyncResult(instanceOf(ModelAndView.class))).andReturn();
 
     this.mockMvc.perform(asyncDispatch(mvcResult)).andExpect(status().isOk())
         .andExpect(content().string((containsString("Sprache"))))
@@ -45,62 +49,21 @@ public class VariableModifyControllerTest extends AbstractWebTest {
   }
 
   @Test
-  public void testPost() throws Exception {
-
-    /*
-     * MvcResult mvcResult = this.mockMvc.perform(get("/")).andExpect(status().isOk())
-     * .andExpect(request().asyncStarted())
-     * .andExpect(request().asyncResult(instanceOf(String.class))).andReturn();
-     * 
-     * this.mockMvc.perform(asyncDispatch(mvcResult)).andExpect(status().isOk())
-     * .andExpect(model().attributeExists("person"))
-     * .andExpect(content().string((not(containsString("</html>")))))
-     * .andExpect(content().string((not(containsString("<body>")))))
-     * .andExpect(content().string((containsString("has-error"))))
-     * .andExpect(content().string(not(containsString("#{"))))
-     * .andExpect(content().string(not(containsString("${"))));
-     */
-  }
-
-  @Test
-  public void testPostInvalidVariableDocument() throws Exception {
-
-  }
-
-  @Test
   public void testPostValidVariableDocument() throws Exception {
-
-    VariableDocument document = new VariableDocument();
-    VariableSurvey survey = new VariableSurvey();
-    List<AnswerOption> answerOpt = new ArrayList<>();
-    AnswerOption answer = new AnswerOption();
-    DateRange range = new DateRange();
-    LocalDate today = LocalDate.now();
-    LocalDate tomorrow = today.plus(1, ChronoUnit.DAYS);
-
-    range.setStartDate(today);
-    range.setEndDate(tomorrow);
-
-    // answer.setCode("Code");
-    answer.setCode(2);
-    answer.setLabel("Label");
-    answerOpt.add(answer);
-
-    survey.setSurveyId("SurveyID");
-    survey.setTitle("SurveyTitle");
-    survey.setVariableAlias("VariableAlias");
-    survey.setSurveyPeriod(range);
-
-    // survey.getSurveyPeriod().setStartDate(startDate);
-
-    document.setId("123");
-    document.setLabel("Label");
-    document.setName("name");
-    document.setAnswerOptions(answerOpt);
-    document.setScaleLevel("NOMINAL");
-    document.setVariableSurvey(survey);
-
-
-
+    // Arrange
+    MvcResult mvcResult = this.mockMvc.perform(post("/de/variables/create")
+        .param(VariableDocument.ID_FIELD, "ID007")
+        .param(VariableDocument.NAME_FIELD, "Ein Name")
+        .param(VariableDocument.QUESTION_FIELD,"Eine Frage?"))
+        .andExpect(status().isOk()).andExpect(request().asyncStarted())
+        .andExpect(request().asyncResult(instanceOf(ModelAndView.class))).andReturn();
+    
+    //Act and Assert
+    this.mockMvc
+        .perform(asyncDispatch(mvcResult)).andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/de/variables/ID007"));
+    
+    //Delete
+    this.variableService.delete("ID007");
   }
 }
