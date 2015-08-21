e73d4c649c972424c620460fc2e001bf27497106
Daniel Katzberg <katzberg@dzhw.eu>
2015-08-21 12:05:35 +0200

    added search-as-you-type bookmarkable urls and browser forward/backward

--- src/main/resources/static/js/variables/search.js
+++ src/main/resources/static/js/variables/search.js
@@ -1,12 +1,23 @@
 VariableSearchForm = {};
 
-VariableSearchForm.search = function(form){
+VariableSearchForm.search = function(form) {
+	"use strict";
 	var formData = $(form).serialize();
 	var searchUrl = $(form).attr('action');
-	
-	// return only a div container of the search results 
-	//and replace the searchResults div contrainer
+	// return only a div container of the search results
+	// and replace the searchResults div contrainer
 	$.get(searchUrl, formData, function(response) {
-	     $("#searchResults").replaceWith(response);
+		$("#searchResults").replaceWith(response);
+
+		var newUrl = searchUrl + '?' + formData;
+		history.pushState({}, '', newUrl);
 	});
+};
+
+window.onpopstate = function(event) {
+	"use strict";
+	// browser is not loading page automatically for back/forward
+	if (event.state) {
+		location.reload();
+	}
 };
