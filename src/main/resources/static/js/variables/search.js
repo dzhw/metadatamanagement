82bfd16cc82fea86ee8dfa5dc806b2412d2c7a2a
René Reitmann <reitmann@dzhw.eu>
2015-08-22 16:38:50 +0200

    Implemented caching of search results in browser history.

--- src/main/resources/static/js/variables/search.js
+++ src/main/resources/static/js/variables/search.js
@@ -5,20 +5,26 @@ VariableSearchForm.search = _.debounce(function(form) {
 	"use strict";
 	var formData = $(form).serialize();
 	var searchUrl = $(form).attr('action');
+	//fix for missing query string in document.documentElement.innerHTML
+	$('#query').attr('value',$('#query').val());
 	// return only a div container of the search results
 	// and replace the searchResults div contrainer
 	$.get(searchUrl, formData, function(response) {
 		$("#searchResults").replaceWith(response);
 
 		var newUrl = searchUrl + '?' + formData;
-		history.pushState({}, '', newUrl);
+		// change the browsers url and cache the current search results
+		history.pushState({html: document.documentElement.innerHTML},'' , newUrl);
 	});
 },200);
 
 window.onpopstate = function(event) {
 	"use strict";
-	// browser is not loading page automatically for back/forward
 	if (event.state) {
+		// if there is a cached search result display it
+		document.documentElement.innerHTML = event.state.html;
+	} else {
+		// if not reload the page from the server
 		location.reload();
 	}
 };
