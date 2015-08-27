078cc9441b2a7d1e5b3ca91f93320aec323c2dd8
Daniel Katzberg <katzberg@dzhw.eu>
2015-08-27 09:21:32 +0200

    fixed a unit test which was depending at the populator data

--- src/test/java/eu/dzhw/fdz/metadatamanagement/web/variablemanagement/details/VariableDetailsControllerTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/web/variablemanagement/details/VariableDetailsControllerTest.java
@@ -6,10 +6,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MvcResult;
 
+import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.DataTypesProvider;
+import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.ScaleLevelProvider;
+import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.VariableDocument;
+import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.VariableSurvey;
+import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.builders.VariableDocumentBuilder;
+import eu.dzhw.fdz.metadatamanagement.data.variablemanagement.documents.builders.VariableSurveyBuilder;
+import eu.dzhw.fdz.metadatamanagement.service.variablemanagement.VariableService;
 import eu.dzhw.fdz.metadatamanagement.web.AbstractWebTest;
 
 /**
@@ -19,9 +32,45 @@ import eu.dzhw.fdz.metadatamanagement.web.AbstractWebTest;
  */
 public class VariableDetailsControllerTest extends AbstractWebTest {
 
+  @Autowired
+  private VariableService variableService;
+
+  @Autowired
+  private DataTypesProvider dataTypesProvider;
+
+  @Autowired
+  private ScaleLevelProvider scaleLevelProvider;
+
+  @Before
+  public void before() {
+
+    LocaleContextHolder.setLocale(Locale.GERMAN);
+    for (int i = 1; i <= 9; i++) {
+      VariableSurvey variableSurvey = new VariableSurveyBuilder()
+          .withSurveyId("GetByValidTest_Survey_ID").withTitle("GetByValidTestTitle 0" + i)
+          .withVariableAlias("GetByValidTestVariableAlias 0" + i).build();
+
+      VariableDocument variableDocument = new VariableDocumentBuilder()
+          .withId("GetByValidTest_ID0" + i).withName("GetByValidTestName 0" + i)
+          .withLabel("GetByValidTestLabel 0" + i).withQuestion("GetByValidTestQuestion 0" + i)
+          .withDataType(this.dataTypesProvider.getNumericValueByLocale())
+          .withScaleLevel(this.scaleLevelProvider.getMetricByLocal())
+          .withVariableSurvey(variableSurvey).build();
+      this.variableService.save(variableDocument);
+    }
+  }
+
+  @After
+  public void after() {
+    // Delete
+    for (int i = 1; i <= 9; i++) {
+      this.variableService.delete("GetByValidTest_ID0" + i);
+    }
+  }
+
   @Test
   public void testGetByValidId() throws Exception {
-    MvcResult mvcResult = this.mockMvc.perform(get("/de/variables/FdZ_ID01"))
+    MvcResult mvcResult = this.mockMvc.perform(get("/de/variables/GetByValidTest_ID01"))
         .andExpect(status().isOk()).andExpect(request().asyncStarted())
         .andExpect(request().asyncResult(instanceOf(ResponseEntity.class))).andReturn();
 
