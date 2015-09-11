c7b6daffcd598b535b7639f05baa3241c45fe480
Amine limouri <limouri@dzhw.eu>
2015-09-11 10:47:18 +0200

    #7 fixed datepicker error

--- src/main/resources/static/js/variables/datepicker.js
+++ src/main/resources/static/js/variables/datepicker.js
@@ -1,58 +1,64 @@
-function initDatePicker(mode){
-		 $(".datepicker").each(function() {
-			 locale = $('html').attr('lang');
-			 
-		//	 $(replaceId($(this).attr('id'))).val($("#"+replaceId($(this).attr('id'))+"_alt").val());
-			 
-			$.datepicker.setDefaults($.datepicker.regional[locale]);
-			$(this).datepicker({
-				altField :"#"+replaceId($(this).attr('id'))+"_alt",
-				altFormat : "yy-mm-dd",
-				gotoCurrent : true,
-				onSelect : function() {
-					$("form").trigger("submit");
-				},
-				changeMonth : true,
-				changeYear : true,
-				yearRange: '1970:2040',
-				firstDay: 1 
-			}).on('keyup',
-					function() {
-						$(this).datepicker("hide");
-						var date='';
-						try {
-							switch(locale) {
-						    case 'de':
-						    	date = $.datepicker.parseDate('dd.mm.yy', $(this).val());
-						        break;
-						    case 'en':
-						    	date = $.datepicker.parseDate('mm/dd/yy', $(this).val());
-						        break;
-						    default:
-						        break;
-						    }
-								var day = ("0" + (date.getDate())).slice(-2);
-								var year = date.getFullYear();
-								// JavaScript months are 0-11
-								var month = ("0" + (date.getMonth() + 1))
-										.slice(-2);
-								$("#"+replaceId($(this).attr('id'))+"_alt").val(
-										year + '-' + month + '-' + day);
-								$("form").trigger("submit");
-						} catch (e) {
-							if(mode=='modify'){
-							$("#"+replaceId($(this).attr('id'))+"_alt").val('invalid date');
-							}else{
-								$("#"+replaceId($(this).attr('id'))+"_alt").val('');
-								if($(this).val() === ''){
-									$("form").trigger("submit");
-								}
-							}
-						}
-					});
-		 });
+function initDatePicker(mode) {
+	$(".datepicker").each(function() {
+		locale = $('html').attr('lang');
+		$.datepicker.setDefaults($.datepicker.regional[locale]);
+		$(this).datepicker({
+			altField : "#" + replaceId($(this).attr('id')) + "_alt",
+			altFormat : "yy-mm-dd",
+			gotoCurrent : true,
+			onSelect : function() {
+				triggerOnValidDate(mode,this.form);
+			},
+			changeMonth : true,
+			changeYear : true,
+			yearRange : '1970:2040',
+			firstDay : 1
+		}).on('keyup', function() {
+			$(this).datepicker("hide");
+			$altFiledInput = $("#" + replaceId($(this).attr('id')) + "_alt");
+			var date = '';
+			try {
+				switch (locale) {
+				case 'de':
+					date = $.datepicker.parseDate('dd.mm.yy', $(this).val());
+					break;
+				case 'en':
+					date = $.datepicker.parseDate('mm/dd/yy', $(this).val());
+					break;
+				default:
+					break;
+				}
+				var day = ("0" + (date.getDate())).slice(-2);
+				var year = date.getFullYear();
+				// JavaScript months are 0-11
+				var month = ("0" + (date.getMonth() + 1)).slice(-2);
+				$altFiledInput.val(year + '-' + month + '-' + day);
+				triggerOnValidDate(mode,this.form);
+			} catch (e) {
+				triggerOnInvalidDate(mode,$(this).val(),this.form);
+			}
+		});
+	});
 }
-		 
-function replaceId(oldId){
-	return oldId.replace(/\./g,"\\.");
+
+function replaceId(oldId) {
+	return oldId.replace(/\./g, "\\.");
+}
+
+function triggerOnValidDate(mode,form){
+	if(mode === 'modify'){
+		$("form").trigger("submit");
+		}else{
+			VariableSearchForm.search(form);
+		}
+}
+function triggerOnInvalidDate(mode,value,form){
+	if (mode === 'modify') {
+		$altFiledInput.val('invalid date');
+	} else {
+		$altFiledInput.val('');
+		if (value === '') {
+			VariableSearchForm.search(form);
+		}
+	}
 }
