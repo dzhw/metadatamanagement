d9a6716870b242592d6acd4a30ab3b4d6224c45a
Daniel Katzberg <katzberg@dzhw.eu>
2015-09-21 09:51:24 +0200

    #3 fixed history back function with the correct input text

--- src/main/resources/static/js/variables/pjaxvariablesearch.js
+++ src/main/resources/static/js/variables/pjaxvariablesearch.js
@@ -1,47 +1,74 @@
-/*$("#searchVariables").on('input', function() {
+$("#searchVariables").on('input', function() {
 	updateSearch();
-});*/
+});
 
-/*$("#searchVariables").on('click', function() {
+$("#searchVariables").on('click', function() {
 	updateSearch();
-});*/
+});
 
-/*
 function updateSearch() {
-	var value = $("#query").val();
-	$("#query").val(value);
+
+	// sets the value in the html input field
+	$("#query").attr("value", $("#query").val());
+
+	// init the date picker
+	initDatepicker();
+
+	// serialize the form
 	var form = $('#searchVariables');
 	$.pjax({
 		container : "#searchResults",
 		data : form.serialize()
 	});
-}*/
-function variableSearch(){
-	$( ".data-pjax" ).submit();
-	getLastFocused();
-	initDatepicker();
+
+	// $.pjax.submit(event, '#searchVariables');
 }
-$(document).on('submit', 'form[data-pjax]', function(event) {
-  $.pjax.submit(event, '#pjax-container');
+
+//this method set the correct value to the input field and the value field on history back
+window.addEventListener('popstate', function(e) {
+	$("#query").val(getUrlParameter('query'));
+	$("#query").attr("value", $("#query").val());
 });
+
+// function variableSearch(){
+// $( ".data-pjax" ).submit();
+// getLastFocused();
+// initDatepicker();
+// }
+// $(document).on('submit', 'form[data-pjax]', function(event) {
+// $.pjax.submit(event, '#pjax-container');
+// });
 $(document).ready(function() {
 	var lastFocused = $.cookie("lastFocused");
-	if(!lastFocused){
-		lastFocused="query";
+	if (!lastFocused) {
+		lastFocused = "query";
 	}
-	if(lastFocused.search("surveyPeriod") >= 0){
+	if (lastFocused.search("surveyPeriod") >= 0) {
 		initDatepicker();
 	}
 	$("#" + lastFocused).focus();
 	getLastFocused();
 });
-//set the mode for datepicker
+// set the mode for datepicker
 function getMode() {
 	return 'search';
 }
-//get id of last focused element and save it as cookie
+// get id of last focused element and save it as cookie
 function getLastFocused() {
 	$("input").focus(function() {
-		$.cookie("lastFocused",replaceId($(this).attr('id')));
+		$.cookie("lastFocused", replaceId($(this).attr('id')));
 	});
 }
+
+var getUrlParameter = function getUrlParameter(sParam) {
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)), sURLVariables = sPageURL
+			.split('&'), sParameterName, i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
+
+		if (sParameterName[0] === sParam) {
+			return sParameterName[1] === undefined ? true : sParameterName[1];
+		}
+	}
+};
