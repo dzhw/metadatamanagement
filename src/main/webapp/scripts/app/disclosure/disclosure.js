8e1ebcb63c30e6238f53a7a2ba056383517e4734
Amine limouri <limouri@dzhw.eu>
2015-10-29 11:37:12 +0100

    #74 added disclosure to the application

---
+++ src/main/webapp/scripts/app/disclosure/disclosure.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('metadatamanagementApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('disclosure', {
+                parent: 'site',
+                url: '/disclosure',
+                data: {
+                    authorities: []
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/disclosure/disclosure.html',
+                        controller: 'DisclosureController'
+                    }
+                },
+                resolve: {
+                    mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                        $translatePartialLoader.addPart('disclosure');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
