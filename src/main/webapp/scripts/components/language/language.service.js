d7fb314d37eff9e7572f9aa7f4e866295b94f485
Amine limouri <limouri@dzhw.eu>
2015-10-29 11:28:44 +0100

    #74 added german language to the application and set it as default. french is removed

--- src/main/webapp/scripts/components/language/language.service.js
+++ src/main/webapp/scripts/components/language/language.service.js
@@ -27,7 +27,7 @@ angular.module('metadatamanagementApp')
  They are written in English to avoid character encoding issues (not a perfect solution)
  */
     .constant('LANGUAGES', [
-        'en', 'fr'
+        'en', 'de'
         //JHipster will add new languages here
     ]
 );
