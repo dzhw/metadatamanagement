fc5832b18ed1658fb6f6f8f2196c8665a62e0833
René Reitmann <reitmann@dzhw.eu>
2015-11-10 15:17:58 +0100

    #83 Enabled Swagger for development system

--- src/main/java/eu/dzhw/fdz/metadatamanagement/config/apidoc/SwaggerConfiguration.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/config/apidoc/SwaggerConfiguration.java
@@ -1,20 +1,21 @@
 package eu.dzhw.fdz.metadatamanagement.config.apidoc;
 
-import eu.dzhw.fdz.metadatamanagement.config.Constants;
-import eu.dzhw.fdz.metadatamanagement.config.JHipsterProperties;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
+
+import eu.dzhw.fdz.metadatamanagement.config.JHipsterProperties;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * Springfox Swagger configuration.
@@ -25,7 +26,6 @@ import static springfox.documentation.builders.PathSelectors.regex;
  */
 @Configuration
 @EnableSwagger2
-@Profile("!"+Constants.SPRING_PROFILE_PRODUCTION)
 public class SwaggerConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(SwaggerConfiguration.class);
