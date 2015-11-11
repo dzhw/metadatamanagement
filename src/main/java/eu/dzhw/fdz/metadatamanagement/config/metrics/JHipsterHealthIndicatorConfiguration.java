796096fbb31725f838bbb337ef3b95d122e10778
René Reitmann <reitmann@dzhw.eu>
2015-11-11 12:15:47 +0100

    #78 Added JestClient for connecting to ElasticSearch

--- src/main/java/eu/dzhw/fdz/metadatamanagement/config/metrics/JHipsterHealthIndicatorConfiguration.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/config/metrics/JHipsterHealthIndicatorConfiguration.java
@@ -1,7 +1,26 @@
 package eu.dzhw.fdz.metadatamanagement.config.metrics;
 
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.searchbox.client.JestClient;
+
+/**
+ * Create additional {@link HealthIndicator} for spring boot actuator.
+ * 
+ * @author René Reitmann
+ */
 @Configuration
 public class JHipsterHealthIndicatorConfiguration {
+  /**
+   * Add custom {@link ElasticsearchHealthIndicator} to the application context.
+   * @param jestClient The client for pinging elasticsearch
+   * @return {@link ElasticsearchHealthIndicator}
+   * @author René Reitmann
+   */
+  @Bean()
+  public HealthIndicator elasticsearchHealthIndicator(JestClient jestClient) {
+    return new ElasticsearchHealthIndicator(jestClient);
+  }
 }
