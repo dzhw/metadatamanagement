825fa53f865f6e6a9b8644ba3038de60a083fa84
Daniel Katzberg <katzberg@dzhw.eu>
2015-11-25 11:38:21 +0100

    #88 fixed failed unit test. added auditresource unit tests. added utils.

--- src/test/java/eu/dzhw/fdz/metadatamanagement/web/rest/AuditResourceTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/web/rest/AuditResourceTest.java
@@ -1,10 +1,20 @@
 package eu.dzhw.fdz.metadatamanagement.web.rest;
 
-import eu.dzhw.fdz.metadatamanagement.Application;
-import eu.dzhw.fdz.metadatamanagement.config.audit.AuditEventConverter;
-import eu.dzhw.fdz.metadatamanagement.domain.PersistentAuditEvent;
-import eu.dzhw.fdz.metadatamanagement.repository.PersistenceAuditEventRepository;
-import eu.dzhw.fdz.metadatamanagement.service.AuditEventService;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+
+import javax.inject.Inject;
+
+import org.json.JSONArray;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,18 +25,19 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.inject.Inject;
-import java.time.LocalDateTime;
-
-import static org.hamcrest.Matchers.hasItem;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import eu.dzhw.fdz.metadatamanagement.Application;
+import eu.dzhw.fdz.metadatamanagement.config.audit.AuditEventConverter;
+import eu.dzhw.fdz.metadatamanagement.domain.PersistentAuditEvent;
+import eu.dzhw.fdz.metadatamanagement.repository.PersistenceAuditEventRepository;
+import eu.dzhw.fdz.metadatamanagement.service.AuditEventService;
 
 /**
  * Test class for the AuditResource REST controller.
+ * 
+ * @author Daniel Katzberg
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -35,69 +46,100 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @IntegrationTest
 public class AuditResourceTest {
 
-    private static final String SAMPLE_PRINCIPAL = "SAMPLE_PRINCIPAL";
-    private static final String SAMPLE_TYPE = "SAMPLE_TYPE";
-    private static final LocalDateTime SAMPLE_TIMESTAMP = LocalDateTime.parse("2015-08-04T10:11:30");
+  private static final String SAMPLE_PRINCIPAL = "SAMPLE_PRINCIPAL";
+  private static final String SAMPLE_TYPE = "SAMPLE_TYPE";
+  private static final LocalDateTime SAMPLE_TIMESTAMP = LocalDateTime.parse("2015-08-04T10:11:30");
 
-    @Inject
-    private PersistenceAuditEventRepository auditEventRepository;
+  @Inject
+  private PersistenceAuditEventRepository auditEventRepository;
 
-    @Inject
-    private AuditEventConverter auditEventConverter;
+  @Inject
+  private AuditEventConverter auditEventConverter;
 
-    private PersistentAuditEvent auditEvent;
+  private PersistentAuditEvent auditEvent;
 
-    private MockMvc restAuditMockMvc;
+  private MockMvc restAuditMockMvc;
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        AuditEventService auditEventService =
-                new AuditEventService(auditEventRepository, auditEventConverter);
-        AuditResource auditResource = new AuditResource(auditEventService);
-        this.restAuditMockMvc = MockMvcBuilders.standaloneSetup(auditResource).build();
-    }
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    AuditEventService auditEventService =
+        new AuditEventService(auditEventRepository, auditEventConverter);
+    AuditResource auditResource = new AuditResource(auditEventService);
+    this.restAuditMockMvc = MockMvcBuilders.standaloneSetup(auditResource)
+      .build();
+  }
 
-    @Before
-    public void initTest() {
-        auditEventRepository.deleteAll();
-        auditEvent = new PersistentAuditEvent();
-        auditEvent.setAuditEventType(SAMPLE_TYPE);
-        auditEvent.setPrincipal(SAMPLE_PRINCIPAL);
-        auditEvent.setAuditEventDate(SAMPLE_TIMESTAMP);
-    }
+  @Before
+  public void initTest() {
+    auditEventRepository.deleteAll();
+    auditEvent = new PersistentAuditEvent();
+    auditEvent.setAuditEventType(SAMPLE_TYPE);
+    auditEvent.setPrincipal(SAMPLE_PRINCIPAL);
+    auditEvent.setAuditEventDate(SAMPLE_TIMESTAMP);
+  }
 
+  @Test
+  public void getAllAudits() throws Exception {
+    // Initialize the database
+    auditEventRepository.save(auditEvent);
 
-    @Test
-    public void getAllAudits() throws Exception {
-        // Initialize the database
-        auditEventRepository.save(auditEvent);
+    // Get all the audits
+    restAuditMockMvc.perform(get("/api/audits"))
+      .andExpect(status().isOk())
+      // .andDo(print())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.[*].principal").value(hasItem(SAMPLE_PRINCIPAL)));
+  }
 
-        // Get all the audits
-        restAuditMockMvc.perform(get("/api/audits"))
-                .andExpect(status().isOk())
-                // .andDo(print())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.[*].principal").value(hasItem(SAMPLE_PRINCIPAL)));
-    }
+  @Test
+  public void getAudit() throws Exception {
+    // Initialize the database
+    auditEventRepository.save(auditEvent);
 
-    @Test
-    public void getAudit() throws Exception {
-        // Initialize the database
-        auditEventRepository.save(auditEvent);
+    // Get the audit
+    restAuditMockMvc.perform(get("/api/audits/{id}", auditEvent.getId()))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.principal").value(SAMPLE_PRINCIPAL));
+  }
 
-        // Get the audit
-        restAuditMockMvc.perform(get("/api/audits/{id}", auditEvent.getId()))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.principal").value(SAMPLE_PRINCIPAL));
-    }
+  @Test
+  public void getNonExistingAudit() throws Exception {
+    // Get the audit
+    restAuditMockMvc.perform(get("/api/audits/{id}", Long.MAX_VALUE))
+      .andExpect(status().isNotFound());
+  }
 
-    @Test
-    public void getNonExistingAudit() throws Exception {
-        // Get the audit
-        restAuditMockMvc.perform(get("/api/audits/{id}", Long.MAX_VALUE))
-                .andExpect(status().isNotFound());
-    }
+  @Test
+  public void testGetByDates() throws Exception {
+    // Arrange
+    this.auditEventRepository.save(this.auditEvent);
+
+    // Act
+    MvcResult mvcResultEmpty =
+        this.restAuditMockMvc.perform(get("/api/audits?fromDate=2000-01-01&toDate=2001-12-31"))
+          .andExpect(status().isOk())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+          .andReturn();
+
+    MvcResult mvcResultNotEmpty =
+        this.restAuditMockMvc.perform(get("/api/audits?fromDate=2014-01-01&toDate=2016-12-31"))
+          .andExpect(status().isOk())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+          .andReturn();
+    JSONArray jsonArrayEmpty = new JSONArray(mvcResultEmpty.getResponse()
+      .getContentAsString());
+    JSONArray jsonArrayNotEmpty = new JSONArray(mvcResultNotEmpty.getResponse()
+      .getContentAsString());
+
+    // Assert
+    assertThat(mvcResultEmpty.getResponse()
+      .getContentAsString(), not(nullValue()));
+    assertThat(mvcResultNotEmpty.getResponse()
+      .getContentAsString(), not(nullValue()));
+    assertThat(jsonArrayEmpty.length(), is(0));
+    assertThat(jsonArrayNotEmpty.length(), is(1));
+  }
 
 }
--- src/test/java/eu/dzhw/fdz/metadatamanagement/web/rest/UserResourceTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/web/rest/UserResourceTest.java
@@ -12,6 +12,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.inject.Inject;
@@ -32,12 +36,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import eu.dzhw.fdz.metadatamanagement.Application;
 import eu.dzhw.fdz.metadatamanagement.domain.User;
 import eu.dzhw.fdz.metadatamanagement.domain.builders.UserBuilder;
 import eu.dzhw.fdz.metadatamanagement.repository.AuthorityRepository;
 import eu.dzhw.fdz.metadatamanagement.repository.UserRepository;
 import eu.dzhw.fdz.metadatamanagement.service.UserService;
+import eu.dzhw.fdz.metadatamanagement.util.UnitTestReflectionUtils;
 import eu.dzhw.fdz.metadatamanagement.web.rest.dto.ManagedUserDTO;
 
 /**
@@ -53,118 +60,131 @@ import eu.dzhw.fdz.metadatamanagement.web.rest.dto.ManagedUserDTO;
 @IntegrationTest
 public class UserResourceTest {
 
-	@Inject
-	private UserRepository userRepository;
-	
-	@Inject
-	private AuthorityRepository authorityRepository;
+  @Inject
+  private UserRepository userRepository;
 
-	@Inject
-	private UserService userService;
+  @Inject
+  private AuthorityRepository authorityRepository;
 
-	@Inject
-	private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+  @Inject
+  private UserService userService;
 
-	private MockMvc restUserMockMvc;
+  @Inject
+  private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
 
-	@Before
-	public void setup() {
-		UserResource userResource = new UserResource();
-		ReflectionTestUtils.setField(userResource, "userRepository", this.userRepository);
-		ReflectionTestUtils.setField(userResource, "userService", this.userService);
-		ReflectionTestUtils.setField(userResource, "authorityRepository", this.authorityRepository);
-		this.restUserMockMvc = MockMvcBuilders.standaloneSetup(userResource)
-				.setCustomArgumentResolvers(pageableArgumentResolver).build();
-	}
+  private MockMvc restUserMockMvc;
 
-	@Test
-	public void testGetExistingUser() throws Exception {
-		// Arrange
+  @Before
+  public void setup() {
+    UserResource userResource = new UserResource();
+    ReflectionTestUtils.setField(userResource, "userRepository", this.userRepository);
+    ReflectionTestUtils.setField(userResource, "userService", this.userService);
+    ReflectionTestUtils.setField(userResource, "authorityRepository", this.authorityRepository);
+    this.restUserMockMvc = MockMvcBuilders.standaloneSetup(userResource)
+      .setCustomArgumentResolvers(pageableArgumentResolver)
+      .build();
+  }
 
-		// Act
+  @Test
+  public void testGetExistingUser() throws Exception {
+    // Arrange
 
-		// Assert
-		restUserMockMvc.perform(get("/api/users/admin").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(content().contentType("application/json"))
-				.andExpect(jsonPath("$.lastName").value("Administrator"));
-	}
+    // Act
 
-	@Test
-	public void testGetUnknownUser() throws Exception {
-		// Arrange
+    // Assert
+    restUserMockMvc.perform(get("/api/users/admin").accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType("application/json"))
+      .andExpect(jsonPath("$.lastName").value("Administrator"));
+  }
 
-		// Act
+  @Test
+  public void testGetUnknownUser() throws Exception {
+    // Arrange
 
-		// Assert
-		restUserMockMvc.perform(get("/api/users/unknown").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isNotFound());
-	}
+    // Act
 
-	@Test
-	public void testCreateUserWithID() throws Exception {
-		// Arrange
-		User user = new UserBuilder().withId("testGetAllUser_ID").build();
+    // Assert
+    restUserMockMvc.perform(get("/api/users/unknown").accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isNotFound());
+  }
 
-		// Act
+  @Test
+  public void testCreateUserWithID() throws Exception {
+    // Arrange
+    User user = new UserBuilder().withId("testGetAllUser_ID")
+      .build();
 
-		// Assert
-		restUserMockMvc.perform(post("/api/users").contentType(TestUtil.APPLICATION_JSON_UTF8)
-				.content(TestUtil.convertObjectToJsonBytes(user))).andExpect(status().is4xxClientError());
-	}
+    // Act
 
-	@Test
-	public void testCreateUser() throws Exception {
-		// Arrange
-		String jsonCreateUser = "{\"login\":\"logintest\",\"password\":\"xwqyUOk2LCQ7IsuhhzVvxwqyUOk2LCQ7IsuhhzVvxwqyUOk2LCQ7IsuhhzVv\",\"email\":\"test@test.test\",\"activated\":false,\"langKey\":\"de\"}";
+    // Assert
+    restUserMockMvc.perform(post("/api/users").contentType(TestUtil.APPLICATION_JSON_UTF8)
+      .content(TestUtil.convertObjectToJsonBytes(user)))
+      .andExpect(status().is4xxClientError());
+  }
 
-		// Act
+  @Test
+  public void testCreateUser() throws Exception {
+    // Arrange
+    String jsonCreateUser =
+        "{\"login\":\"logintest\",\"password\":\"xwqyUOk2LCQ7IsuhhzVvxwqyUOk2LCQ7IsuhhzVvxwqyUOk2LCQ7IsuhhzVv\",\"email\":\"test@test.test\",\"activated\":false,\"langKey\":\"de\"}";
 
-		// Assert
-		restUserMockMvc.perform(post("/api/users").contentType(TestUtil.APPLICATION_JSON_UTF8).content(jsonCreateUser))
-				.andExpect(status().isCreated());
-	}
+    // Act
+    //With reflection, push the passwort.
+    Field field = User.class.getDeclaredField("password");
+    final JsonIgnore jsonIgnoreAnnotation = field.getAnnotation(JsonIgnore.class);
+    UnitTestReflectionUtils.changeAnnotationValue(jsonIgnoreAnnotation, "value", false);
+    
+    // Assert
+    restUserMockMvc.perform(post("/api/users").contentType(TestUtil.APPLICATION_JSON_UTF8)
+      .content(jsonCreateUser))
+      .andExpect(status().isCreated());
+    
+    UnitTestReflectionUtils.changeAnnotationValue(jsonIgnoreAnnotation, "value", true);
+  }
 
-	@Test
-	public void testGetAllUser() throws Exception {
-		// Arrange
+  @Test
+  public void testGetAllUser() throws Exception {
+    // Arrange
 
-		// Act
-		MvcResult mvcResult = restUserMockMvc.perform(get("/api/users"))
-				.andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().is2xxSuccessful())
-				.andReturn();
-		String content = mvcResult.getResponse().getContentAsString();
-		JSONArray jsonArray = new JSONArray(content);
-		System.out.println(content);
-		
-		// Assert
-		assertThat(content, not(nullValue()));
-		assertThat(jsonArray.length(), is(8));
-	}
-	
-	@Test
-	public void testUpdateUser() throws IOException, Exception {
-		
-		assertThat(this.authorityRepository, not(nullValue()));
-		
-		//Arrange
-		Optional<User> userO = this.userRepository.findOneByLogin("user");
-		User user = userO.get();
-		user.setEmail("userMod@localhost");
-		ManagedUserDTO dto = new ManagedUserDTO(user);
-		
-		//Act
-		MvcResult mvcResult = restUserMockMvc.perform(put("/api/users")
-                .contentType(TestUtil.APPLICATION_JSON_UTF8)
-                .content(TestUtil.convertObjectToJsonBytes(dto)))
-                .andExpect(status().isOk())
-                .andReturn();
-		String content = mvcResult.getResponse().getContentAsString();
-		JSONObject jsonObject = new JSONObject(content);
-				
-		//Assert
-		assertThat(content, not(nullValue()));
-		assertThat(jsonObject.getString("email"), is("userMod@localhost"));		
-	}
+    // Act
+    MvcResult mvcResult = restUserMockMvc.perform(get("/api/users"))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().is2xxSuccessful())
+      .andReturn();
+    String content = mvcResult.getResponse()
+      .getContentAsString();
+    JSONArray jsonArray = new JSONArray(content);
+
+    // Assert
+    assertThat(content, not(nullValue()));
+    assertThat(jsonArray.length(), is(8));
+  }
+
+  @Test
+  public void testUpdateUser() throws IOException, Exception {
+
+    assertThat(this.authorityRepository, not(nullValue()));
+
+    // Arrange
+    Optional<User> userO = this.userRepository.findOneByLogin("user");
+    User user = userO.get();
+    user.setEmail("userMod@localhost");
+    ManagedUserDTO dto = new ManagedUserDTO(user);
+
+    // Act
+    MvcResult mvcResult =
+        restUserMockMvc.perform(put("/api/users").contentType(TestUtil.APPLICATION_JSON_UTF8)
+          .content(TestUtil.convertObjectToJsonBytes(dto)))
+          .andExpect(status().isOk())
+          .andReturn();
+    String content = mvcResult.getResponse()
+      .getContentAsString();
+    JSONObject jsonObject = new JSONObject(content);
+
+    // Assert
+    assertThat(content, not(nullValue()));
+    assertThat(jsonObject.getString("email"), is("userMod@localhost"));
+  }
 }
