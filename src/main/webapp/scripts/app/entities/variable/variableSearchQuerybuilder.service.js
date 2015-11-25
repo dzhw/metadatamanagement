76c19317a913eb043e6a02ee54865fc0e93bf813
Amine limouri <limouri@dzhw.eu>
2015-11-25 15:02:45 +0100

    #76 added search query vor variables

--- src/main/webapp/scripts/app/entities/variable/variableSearchQuerybuilder.service.js
+++ src/main/webapp/scripts/app/entities/variable/variableSearchQuerybuilder.service.js
@@ -2,26 +2,46 @@
 
 angular.module('metadatamanagementApp').service('VariableSearchQuerybuilder',
     function($rootScope) {
-  return {
-    Query: function(queryterm, page) {
-      var query = {
-          'index': 'metadata_' + $rootScope.currentLanguage,
-          'type': 'variables',
-          'body': {
-            'query': {
-              'match_phrase_prefix': {
-                'label': queryterm
-              }
-            },
-            'from': page,
-            'size': 3
-          }
-        };
-      if (!queryterm) {
-        query.body.query = {
-            'match_all': {}
+      return {
+        Query: function(queryterm, page) {
+          var query = {
+            'index': 'metadata_' + $rootScope.currentLanguage,
+            'type': 'variables',
+            'body': {
+              'query': {
+                'bool': {
+                  'should': [{
+                    'match': {
+                      '_all': {
+                        'query': queryterm,
+                        'type': 'boolean',
+                        'operator': 'AND',
+                        'zero_terms_query': 'NONE'
+                      }
+                    }
+                  }, {
+                    'match': {
+                      'allStringsAsNgrams': {
+                        'query': queryterm,
+                        'type': 'boolean',
+                        'operator': 'AND',
+                        'minimum_should_match': '100%',
+                        'zero_terms_query': 'NONE'
+                      }
+                    }
+                  }]
+                }
+              },
+              'from': page,
+              'size': 3
+            }
           };
-      }
-      return query;
-    }};
-});
+          if (!queryterm) {
+            query.body.query = {
+              'match_all': {}
+            };
+          }
+          return query;
+        }
+      };
+    });
