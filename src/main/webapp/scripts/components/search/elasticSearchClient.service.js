53a2c39f6cbea9d9e400a3f28c3932fd97763e75
Amine limouri <limouri@dzhw.eu>
2015-12-01 14:06:24 +0100

    #76 removed unnecessary changes

--- src/main/webapp/scripts/components/search/elasticSearchClient.service.js
+++ src/main/webapp/scripts/components/search/elasticSearchClient.service.js
@@ -1,11 +1,9 @@
 'use strict';
 /*global jQuery:false */
 angular.module('metadatamanagementApp').service('ElasticSearchClient',
-    function() {
-  var url1 = 'https://public:bx6hbibdskm7j1ag4v6pyvvl';
-  var url2 = 'sdxknfhu@dwalin-us-east-1.searchly.com';
+    function($rootScope) {
   var client = new jQuery.es.Client({
-    hosts: url1 + url2 //$rootScope.elasticSearchProperties.url
+    hosts: $rootScope.elasticSearchProperties.url
   });
   return client;
 });
