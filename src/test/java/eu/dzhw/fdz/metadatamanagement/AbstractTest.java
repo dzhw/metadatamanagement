b6d5ecd622288b322ca8a007154fd9390d20adff
Daniel Katzberg <katzberg@dzhw.eu>
2015-12-10 10:16:44 +0100

    updated in travis.yml the node version to 5.2.0

--- src/test/java/eu/dzhw/fdz/metadatamanagement/AbstractTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/AbstractTest.java
@@ -2,7 +2,6 @@ package eu.dzhw.fdz.metadatamanagement;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
