630dee646e96538541babe2dc88ab590d40fd075
René Reitmann <reitmann@dzhw.eu>
2016-01-08 12:24:40 +0100

    #131 removed unrequired code

--- src/main/webapp/scripts/app/entities/variable/variable-detail.controller.js
+++ src/main/webapp/scripts/app/entities/variable/variable-detail.controller.js
@@ -2,17 +2,10 @@
 
 angular.module('metadatamanagementApp')
   .controller('VariableDetailController',
-    function($scope, $rootScope, $stateParams, entity, Variable,
-      VariableExportService) {
+    function($scope, entity, VariableExportService) {
       $scope.variable = entity;
 
       $scope.exportToODT = function() {
         VariableExportService.exportToODT($scope.variable);
       };
-      var unsubscribe = $rootScope.$on('metadatamanagementApp:variableUpdate',
-      function(event, result) {
-        $scope.variable = result;
-      });
-      $scope.$on('$destroy', unsubscribe);
-
     });
