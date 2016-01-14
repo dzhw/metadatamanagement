75271a0f7c19f97de6c7324ac2f03529521be680
Amine limouri <limouri@dzhw.eu>
2016-01-14 16:57:38 +0100

    #123 javascript tests

---
+++ src/test/javascript/spec/components/interceptor/auth.interceptor.spec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+describe('Factory Tests ', function () {
+  var authInterceptor, authExpiredInterceptor, localStorageService, response, $q, $scope;
+    describe('authInterceptor', function() {
+        beforeEach(inject(function($injector) {
+            authInterceptor = $injector.get('authInterceptor');
+            $q = $injector.get('$q');
+            $scope = $injector.get('$rootScope').$new();
+          localStorageService = (function() {
+            return {
+              get: function(key) {
+                return {
+                  'expires_at':12-12-3001
+                };
+              }
+            };
+          })();
+          Object.defineProperty(window, 'localStorageService', { value: localStorageService, configurable: true, enumerable: true, writable: true });
+          spyOn(localStorageService, 'get').and.callThrough()
+        }));
+        it('authInterceptor should be defined', function() {
+          authInterceptor.request({});
+          expect(authInterceptor).toBeDefined();
+        });
+      });
+    describe('authExpiredInterceptor', function() {
+      beforeEach(inject(function($injector) {
+        authExpiredInterceptor = $injector.get('authExpiredInterceptor');
+        $q = $injector.get('$q');
+        $scope = $injector.get('$rootScope').$new();
+        response = {
+          'data':{
+            'path':'/test',
+            'error': 'invalid_token'
+          },
+          'status':401
+        };
+        spyOn($q,'reject').and.callThrough();
+      }));
+      it('should call $q.reject', function() {
+        authExpiredInterceptor.responseError(response);
+        expect($q.reject).toHaveBeenCalled();
+      });
+    });
+  });
