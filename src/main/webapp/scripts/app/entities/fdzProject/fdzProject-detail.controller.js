dcedde81b8ba352786f021a10d1d4fde8895c277
Daniel Katzberg <katzberg@dzhw.eu>
2016-01-20 16:38:22 +0100

    #158 rewrited fdzproject detail controller for export. button is still disabled.

--- src/main/webapp/scripts/app/entities/fdzProject/fdzProject-detail.controller.js
+++ src/main/webapp/scripts/app/entities/fdzProject/fdzProject-detail.controller.js
@@ -3,10 +3,19 @@
 angular.module('metadatamanagementApp')
     .controller('FdzProjectDetailController',
       function($scope, $rootScope, $stateParams,
-        entity, FdzProjectExportService) {
+        entity, FdzProjectExportService, VariableCollection) {
         $scope.fdzProject = entity;
 
+        $scope.loadAll = function() {
+          VariableCollection.query($scope.fdzProject.id,
+            function(result) {
+              $scope.totalItems = result.page.totalElements;
+              $scope.variables = result._embedded.variables;
+            });
+        };
+        $scope.loadAll();
+
         $scope.exportToODT = function() {
-        FdzProjectExportService.exportToODT($scope.variable);
-      };
+          FdzProjectExportService.exportToODT($scope.variables);
+        };
       });
