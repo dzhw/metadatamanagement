5bd1d9bc142c2b8d5a0b837494dbedaefc7dade3
Daniel Katzberg <katzberg@dzhw.eu>
2016-01-26 15:00:53 +0100

    #179 changed browser from chrome to phantomjs

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,12 +2,8 @@
 'use strict';
 exports.config = {
   seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar',
-  chromeDriver: './node_modules/protractor/selenium/chromedriver',
+  capabilities: {
+    browserName: 'phantomjs'
+  },
   specs: ['src/test/protractor/title.js']
-    /*capabilities: {
-      browserName: 'chrome',
-      version: '',
-      platform: 'ANY',
-      'webdriver.chrome.driver': require('chromedriver').path
-    }*/
 };
