a777520b24f828b29c48589d211c65dcaf3852e3
Daniel Katzberg <katzberg@dzhw.eu>
2016-02-15 10:31:02 +0100

    #45 fixed issue with async problems with values from the backend. changed bar color to corporite color.

--- src/main/webapp/scripts/app/entities/variable/variable-detail.controller.js
+++ src/main/webapp/scripts/app/entities/variable/variable-detail.controller.js
@@ -3,78 +3,74 @@
 'use strict';
 
 angular.module('metadatamanagementApp')
-  .controller('VariableDetailController', ['$scope', 'entity', '$translate',
-    function($scope, entity, $translate) {
+  .controller('VariableDetailController', ['$scope', 'entity', 'Language',
+    function($scope, entity, Language) {
       $scope.variable = entity;
 
-      var usedLanguage = $translate.use();
-
-      //options for charts
-      $scope.options = {
-        chart: {
-
-          //CSS information
-          type: 'discreteBarChart',
-          height: 350,
-          showValues: true,
-          valueFormat: function(d) {
-            return d3.format(',.2f')(d); //.2f means two numbers after a commata
-          },
-          transitionDuration: 500,
-
-          //Point for data extraction to visualisation
-          x: function(d) {
-
-            //if en, return en lables
-            if (usedLanguage === 'en') {
-              return d.label.en;
-            }
-
-            //de is default. return de labels.
-            return d.label.de;
-          },
-          y: function(d) {
-            return d.absoluteFrequency;
-          }
-        },
-
-        // title options
-        title: {
-          enable: true,
-          text: $scope.variable.name
-        },
-
-        // subtitle options
-        subtitle: {
-          enable: true,
-          text: 'Absolute Häufigkeit von Werten',
-          css: {
-            'text-align': 'center',
-            'margin': '10px 13px 0px 7px'
-          }
-        },
-
-        // caption options
-        caption: {
-          enable: true,
-          html: 'Figure 1: ' + $scope.variable.name +
-            ', Absolute Häufigkeit von Werten',
-          css: {
-            'text-align': 'center',
-            'margin': '-30px 0px 0px 0px'
-          }
-        }
-      };
-
       //The data object is for the display
-      $scope.data = [{
-        values: []
-      }];
+      $scope.variable.$promise.then(function() {
 
-      //TODO problem after language change ... no direct set...
-      console.log('Test');
-      console.log($scope);
-      $scope.data[0].values = $scope.variable.values;
-      console.log($scope);
+        //options for charts
+        $scope.options = {
+          chart: {
+
+            //CSS information
+            type: 'discreteBarChart',
+            height: 350,
+            showValues: true,
+            color : ['#0069B4'],
+            valueFormat: function(d) {
+              return d3.format(',.2f')(d); //.2f means two numbers after a commata
+            },
+            transitionDuration: 500,
+
+            //Point for data extraction to visualisation
+            x: function(d) {
+
+              //if en, return en lables
+              if (Language.getCurrentInstantly() === 'en') {
+                return d.label.en;
+              }
+
+              //de is default. return de labels.
+              return d.label.de;
+            },
+            y: function(d) {
+              return d.absoluteFrequency;
+            }
+          },
+
+          // title options
+          title: {
+            enable: true,
+            text: $scope.variable.name
+          },
+
+          // subtitle options
+          subtitle: {
+            enable: true,
+            text: 'Absolute Häufigkeit von Werten',
+            css: {
+              'text-align': 'center',
+              'margin': '10px 13px 0px 7px'
+            }
+          },
+
+          // caption options
+          caption: {
+            enable: true,
+            html: 'Figure 1: ' + $scope.variable.name +
+              ', Absolute Häufigkeit von Werten',
+            css: {
+              'text-align': 'center',
+              'margin': '-30px 0px 0px 0px'
+            }
+          }
+        };
+
+        $scope.data = [{
+          values: $scope.variable.values
+        }];
+      });
     }
   ]);
