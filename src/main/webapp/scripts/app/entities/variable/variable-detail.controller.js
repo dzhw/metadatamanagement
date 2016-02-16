221f0a2d6fffe091e836db30039ce151c0789b19
Daniel Katzberg <katzberg@dzhw.eu>
2016-02-16 16:07:49 +0100

    #45 added box plot. added relative plot

--- src/main/webapp/scripts/app/entities/variable/variable-detail.controller.js
+++ src/main/webapp/scripts/app/entities/variable/variable-detail.controller.js
@@ -10,11 +10,68 @@ angular.module('metadatamanagementApp')
       $scope.variable = entity;
 
       //options for charts
-      $scope.options = {
+      $scope.optionsRelative = {
         chart: {
           //CSS information
           type: 'discreteBarChart',
-          height: 350,
+          height: 250,
+          showValues: true,
+          color: ['#0069B4'],
+          valueFormat: function(d) {
+            //.2f means two numbers after a commata
+            return d3.format(',.2f')(d);
+          },
+
+          transitionDuration: 500,
+
+          //Point for data extraction to visualisation
+          x: function(d) {
+
+            //if en, return en lables
+            if (Language.getCurrentInstantly() === 'en') {
+              if (d.label.en) {
+                return d.label.en;
+                //return the code, if label is not set
+              } else {
+                return d.code;
+              }
+            }
+
+            //de is default. return de labels.
+            if (d.label.de) {
+              return d.label.de;
+              //return code, if label is not set
+            } else {
+              return d.code;
+            }
+          },
+
+          y: function(d) {
+            return d.relativeFrequency;
+          },
+        },
+
+        // title options
+        title: {
+          enable: true,
+          text: '',
+        },
+        // subtitle options
+        subtitle: {
+          enable: true,
+          text: '',
+          css: {
+            'text-align': 'center',
+            margin: '10px 13px 0px 7px',
+          },
+        }
+      };
+
+      $scope.optionsAbsolute = {
+        chart: {
+          //CSS information
+          type: 'discreteBarChart',
+          height: 250,
           showValues: true,
           color: ['#0069B4'],
           valueFormat: function(d) {
@@ -65,17 +122,7 @@ angular.module('metadatamanagementApp')
             'text-align': 'center',
             margin: '10px 13px 0px 7px',
           },
-        },
-
-        // caption options
-        caption: {
-          enable: true,
-          html: '',
-          css: {
-            'text-align': 'center',
-            margin: '-30px 0px 0px 0px',
-          },
-        },
+        }
       };
 
       //Wait for promise object
@@ -84,19 +131,53 @@ angular.module('metadatamanagementApp')
         $scope.data = [{
           values: $scope.variable.values
         }];
-        $scope.options.title.text = $scope.variable.name;
 
         $translate(
           'metadatamanagementApp.variable.chart.absoluteFrequency').then(
           function(translation) {
-            $scope.options.subtitle.text = translation;
+            $scope.optionsAbsolute.subtitle.text = translation;
           });
 
-        $translate('metadatamanagementApp.variable.chart.figure').then(
+        $translate(
+          'metadatamanagementApp.variable.chart.relativeFrequency').then(
           function(translation) {
-            $scope.options.caption.html = translation + ': ' + $scope.variable
-              .name + ', ' + $scope.options.subtitle.text;
+            $scope.optionsRelative.subtitle.text = translation;
           });
       });
+
+      // Box Plot Chart
+      //----------------------------------------------------------------------
+      $scope.options = {
+        chart: {
+          type: 'boxPlotChart',
+          height: 550,
+          color: ['#0069B4'],
+          x: function(d) {
+            return d.label;
+          },
+          y: function(d) {
+            return d.values.Q3;
+          },
+          maxBoxWidth: 55,
+          yDomain: [0, 150]
+        }
+      };
+
+      // Code here will be linted with JSCS.
+      // jscs:disable
+      $scope.data2 = [{
+        label: 'Sample A',
+        values: {
+          Q1: 70,
+          Q2: 90,
+          Q3: 110,
+          whisker_low: 30,
+          whisker_high: 130,
+          outliers: [5, 15, 140]
+        }
+      }];
+      // Code here will be linted with JSCS.
+      // jscs:enable
+
     }
   ]);
