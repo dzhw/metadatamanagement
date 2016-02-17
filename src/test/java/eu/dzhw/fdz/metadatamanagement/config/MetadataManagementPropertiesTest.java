64979222e219a001978a8da4d693ecd3a95beac6
Amine Limouri <limouri@dzhw.eu>
2016-02-17 11:02:24 +0100

    #184 fixed test error

--- src/test/java/eu/dzhw/fdz/metadatamanagement/config/MetadataManagementPropertiesTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/config/MetadataManagementPropertiesTest.java
@@ -45,12 +45,12 @@ public class MetadataManagementPropertiesTest {
     ElasticsearchAngularClient angularClient = managementProperties.getElasticsearchAngularClient();
     angularClient.setLogLevel("info");
     angularClient.setUrl("http://localhost:1234");
-    angularClient.setApiVersion("1.7");
+    angularClient.setApiVersion("2.1");
 
     // Assert
     assertThat(angularClient, not(nullValue()));
     assertThat(angularClient.getLogLevel(), is("info"));
-    assertThat(angularClient.getApiVersion(), is("1.7"));
+    assertThat(angularClient.getApiVersion(), is("2.1"));
     assertThat(angularClient.getUrl(), is("http://localhost:1234"));
   }
 }
