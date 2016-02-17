65b1210fc48695ac381a8287b73c285c480d747c
Daniel Katzberg <katzberg@dzhw.eu>
2016-02-17 12:02:15 +0100

    #45 added jasmine tests

--- src/test/javascript/spec/app/entities/variable/variable-detail.controller.spec.js
+++ src/test/javascript/spec/app/entities/variable/variable-detail.controller.spec.js
@@ -1,29 +1,117 @@
+/* global describe */
+/* global beforeEach */
+/* global it */
+/* global inject */
+/* global expect */
 'use strict';
 
 describe('Variable Detail Controller', function() {
-    var $scope, $rootScope, MockEntity, createController, Language;
+  var $scope;
+  var $rootScope;
+  var MockEntity;
+  var createController;
+  var Language;
+  var $translate;
 
-    beforeEach(inject(function($injector) {
-        $rootScope = $injector.get('$rootScope');
-        Language = $injector.get('Language');
-        $scope = $rootScope.$new();
-        MockEntity = {
-          $promise: {
-            then: function() {}            
-          }
-        };
+  beforeEach(inject(function($injector) {
+    $rootScope = $injector.get('$rootScope');
+    Language = $injector.get('Language');
+    $translate = $injector.get('$translate');
+    $scope = $rootScope.$new();
+    MockEntity = {
+      $promise: {
+        then: function(callback) {
+          callback();
+        }
+      }
+    };
 
-        var locals = {
-            '$scope': $scope,
-            'entity': MockEntity ,
-            'Language' : Language
-        };
-        createController = function() {
-            $injector.get('$controller')('VariableDetailController', locals);
-        };
-    }));
-    it('should set $scope.variable', function() {
-      createController();
-      expect($scope.variable).toBeDefined();
-    });
+    var locals = {
+      '$scope': $scope,
+      'entity': MockEntity,
+      'Language': Language,
+      '$translate': $translate
+    };
+    createController = function() {
+      $injector.get('$controller')('VariableDetailController',
+        locals);
+    };
+  }));
+
+  it('should set $scope.variable', function() {
+    createController();
+    expect($scope.variable).toBeDefined();
+
+  });
+
+  it('check options without language', function() {
+    //define variables
+    createController();
+    var d = {};
+    d.relativeFrequency = 86.76;
+    d.absoluteFrequency = 345;
+
+    //relative
+    expect($scope.optionsRelative.chart.valueFormat(123.456)).toBe(
+      '123.46');
+    expect($scope.optionsRelative.chart.y(d)).toBe(86.76);
+
+    //absolute
+    expect($scope.optionsAbsolute.chart.valueFormat(123.456)).toBe(
+      '123.46');
+    expect($scope.optionsAbsolute.chart.y(d)).toBe(345);
+  });
+
+  it('check options for box plot', function() {
+    //define variables
+    createController();
+    var d = {};
+    d.label = 'A Lable';
+    d.values = {};
+    d.values.Q3 = 110;
+
+    //Assert
+    expect($scope.options.chart.x(d)).toBe('A Lable');
+    expect($scope.options.chart.y(d)).toBe(110);
+  });
+
+  it('check options with german', function() {
+    createController();
+    var currentLanguage = Language.getCurrentInstantly();
+    Language.setCurrent('de');
+    var d = {};
+    d.label = {};
+
+    //check for code
+    d.code = 'A Code';
+    expect($scope.optionsRelative.chart.x(d)).toBe('A Code');
+    expect($scope.optionsAbsolute.chart.x(d)).toBe('A Code');
+
+    //check for label
+    d.label.de = 'german';
+    expect($scope.optionsRelative.chart.x(d)).toBe('german');
+    expect($scope.optionsAbsolute.chart.x(d)).toBe('german');
+
+    Language.setCurrent(currentLanguage);
+  });
+
+  it('check options with english', function() {
+    createController();
+    var currentLanguage = Language.getCurrentInstantly();
+    Language.setCurrent('en');
+    var d = {};
+    d.label = {};
+
+    //check for code
+    d.code = 'Another Code';
+    expect($scope.optionsRelative.chart.x(d)).toBe('Another Code');
+    expect($scope.optionsAbsolute.chart.x(d)).toBe('Another Code');
+
+    //check for label
+    d.label.en = 'english';
+    expect($scope.optionsRelative.chart.x(d)).toBe('english');
+    expect($scope.optionsAbsolute.chart.x(d)).toBe('english');
+
+    Language.setCurrent(currentLanguage);
+  });
 });
--- src/test/javascript/spec/app/entities/variable/variable-dialog.controller.spec.js
+++ src/test/javascript/spec/app/entities/variable/variable-dialog.controller.spec.js
@@ -1,13 +1,14 @@
 'use strict';
 
-describe('Controllers Tests ', function () {
-  var $scope, $controller, createController, $uibModalInstance, MockEntity, FdzProjectCollection, SurveyCollection, $q, isCreateMode;
+describe('Controllers Tests ', function() {
+  var $scope, $controller, createController, $uibModalInstance, MockEntity,
+    FdzProjectCollection, SurveyCollection, $q, isCreateMode;
   var result = {
-    'page' : {
-      'totalElements':2
+    'page': {
+      'totalElements': 2
     },
-    '_embedded' : {
-      'fdzProjects':[]
+    '_embedded': {
+      'fdzProjects': []
     }
   };
   beforeEach(mockApiAccountCall);
@@ -18,14 +19,13 @@ describe('Controllers Tests ', function () {
       $q = _$q_;
       $controller = _$controller_;
       MockEntity = {
-        $save: function(success, error){
+        $save: function(success, error) {
           error();
-          return {
-          };
+          return {};
         }
       };
       FdzProjectCollection = {
-        query: function(callback){
+        query: function(callback) {
           var deferred = $q.defer();
           deferred.resolve(result);
           callback(result);
@@ -33,7 +33,7 @@ describe('Controllers Tests ', function () {
         }
       };
       SurveyCollection = {
-        query: function(par,callback){
+        query: function(par, callback) {
           var deferred = $q.defer();
           deferred.resolve(result);
           callback(result);
@@ -51,65 +51,67 @@ describe('Controllers Tests ', function () {
       spyOn(SurveyCollection, 'query').and.callThrough();
       spyOn($scope, '$broadcast');
     });
-   });
-   describe('VariableDialogController with createMode',function(){
-     beforeEach(function(){
-       isCreateMode = true;
-       var locals = {
-         '$scope' : $scope,
-         'entity': MockEntity ,
-         '$uibModalInstance': $uibModalInstance,
-         'isCreateMode': isCreateMode,
-         'FdzProjectCollection' : FdzProjectCollection,
-         'SurveyCollection' : SurveyCollection
-       };
-       createController = function() {
-         return $controller('VariableDialogController', locals);
-       };
-         createController();
-     });
-     it('should call $scope.$broadcast with variable.created',function(){
-       $scope.save();
-       expect($scope.$broadcast).toHaveBeenCalledWith('variable.created');
-     });
-     it('$scope.isSurveyEmpty should be true',function(){
-       var returnValue = $scope.isSurveyEmpty();
-       expect(returnValue).toEqual(true);
-     });
-   });
-   describe('VariableDialogController without createMode',function(){
-     beforeEach(function(){
-       isCreateMode = false;
-       var locals = {
-         '$scope' : $scope,
-         'entity': MockEntity ,
-         '$uibModalInstance': $uibModalInstance,
-         'isCreateMode': isCreateMode,
-         'FdzProjectCollection' : FdzProjectCollection,
-         'SurveyCollection' : SurveyCollection
-       };
-       createController = function() {
-         return $controller('VariableDialogController', locals);
-       };
-         createController();
-     });
-     it('should call $scope.$broadcast with variable.updated',function(){
-       $scope.save();
-       expect($scope.$broadcast).toHaveBeenCalledWith('variable.updated');
-     });
-     it('$scope.isSurveyEmpty should be true',function(){
-       $scope.allSurveysByFdzProjectId = [];
-       var returnValue = $scope.isSurveyEmpty();
-       expect(returnValue).toEqual(true);
-     });
-     it('$scope.isSurveyEmptyg should be false',function(){
-       $scope.allSurveysByFdzProjectId = [{}];
-       var returnValue = $scope.isSurveyEmpty();
-       expect(returnValue).toEqual(false);
-     });
-     it('should call $uibModalInstance.dismiss', function() {
-           $scope.clear();
-           expect($uibModalInstance.dismiss).toHaveBeenCalled();
-         });
-   });
+  });
+  describe('VariableDialogController with createMode', function() {
+    beforeEach(function() {
+      isCreateMode = true;
+      var locals = {
+        '$scope': $scope,
+        'entity': MockEntity,
+        '$uibModalInstance': $uibModalInstance,
+        'isCreateMode': isCreateMode,
+        'FdzProjectCollection': FdzProjectCollection,
+        'SurveyCollection': SurveyCollection
+      };
+      createController = function() {
+        return $controller('VariableDialogController', locals);
+      };
+      createController();
+    });
+    it('should call $scope.$broadcast with variable.created', function() {
+      $scope.save();
+      expect($scope.$broadcast).toHaveBeenCalledWith(
+        'variable.created');
+    });
+    it('$scope.isSurveyEmpty should be true', function() {
+      var returnValue = $scope.isSurveyEmpty();
+      expect(returnValue).toEqual(true);
+    });
+  });
+  describe('VariableDialogController without createMode', function() {
+    beforeEach(function() {
+      isCreateMode = false;
+      var locals = {
+        '$scope': $scope,
+        'entity': MockEntity,
+        '$uibModalInstance': $uibModalInstance,
+        'isCreateMode': isCreateMode,
+        'FdzProjectCollection': FdzProjectCollection,
+        'SurveyCollection': SurveyCollection
+      };
+      createController = function() {
+        return $controller('VariableDialogController', locals);
+      };
+      createController();
+    });
+    it('should call $scope.$broadcast with variable.updated', function() {
+      $scope.save();
+      expect($scope.$broadcast).toHaveBeenCalledWith(
+        'variable.updated');
+    });
+    it('$scope.isSurveyEmpty should be true', function() {
+      $scope.allSurveysByFdzProjectId = [];
+      var returnValue = $scope.isSurveyEmpty();
+      expect(returnValue).toEqual(true);
+    });
+    it('$scope.isSurveyEmptyg should be false', function() {
+      $scope.allSurveysByFdzProjectId = [{}];
+      var returnValue = $scope.isSurveyEmpty();
+      expect(returnValue).toEqual(false);
+    });
+    it('should call $uibModalInstance.dismiss', function() {
+      $scope.clear();
+      expect($uibModalInstance.dismiss).toHaveBeenCalled();
+    });
+  });
 });
