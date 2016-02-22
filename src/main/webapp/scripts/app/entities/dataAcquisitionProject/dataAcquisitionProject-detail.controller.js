b51be6501e8ae7d4853edd7c2bf2fe185424cb17
René Reitmann <reitmann@dzhw.eu>
2016-02-22 16:50:58 +0100

    #165 refactored error handling

--- src/main/webapp/scripts/app/entities/dataAcquisitionProject/dataAcquisitionProject-detail.controller.js
+++ src/main/webapp/scripts/app/entities/dataAcquisitionProject/dataAcquisitionProject-detail.controller.js
@@ -2,63 +2,105 @@
 'use strict';
 
 angular.module('metadatamanagementApp')
-    .controller('DataAcquisitionProjectDetailController',
-      function($scope, entity, DataAcquisitionProjectExportService,
-        ExcelParser, Survey) {
-        $scope.dataAcquisitionProject = entity;
-        $scope.show = false;
-        $scope.progress = 0;
-        $scope.isOpen = false;
-        $scope.errorMessages = [];
-        var saveSurveys = function(surveys) {
-          var length = surveys.length;
-          $scope.maxProgress = length;
-          $scope.uploadResult = 'success';
-          for (var i = 0; i < length; i++) {
-            var data = surveys[i];
-            console.log(data.questionnaireId);
-            var surveyObj = {
-              id: data.id,
-              dataAcquisitionProjectId: $scope.dataAcquisitionProject.id,
-              questionnaireId: data.questionnaireId,
-              title: {
-                en: data['title.en'],
-                de: data['title.de']
-              },
-              fieldPeriod: {
-                start: data['fieldPeriod.start'],
-                end: data['fieldPeriod.end']
-              }
-            };
-            var survey = new Survey(surveyObj);
-            survey.$save().then(function() {
-              $scope.progress = $scope.progress + 1;
-              $scope.state = $scope.progress + '/' + $scope.maxProgress;
-            }).catch(function(errors) {
-              $scope.show = true;
-              $scope.isOpen = true;
-              $scope.progress = $scope.progress + 1;
-              $scope.uploadResult = 'danger';
-              $scope.errorMessages.push(errors);
-              $scope.state = $scope.progress + '/' + $scope.maxProgress;
+  .controller('DataAcquisitionProjectDetailController',
+    function($scope, $translate, entity, DataAcquisitionProjectExportService,
+      ExcelParser, Survey) {
+      $scope.dataAcquisitionProject = entity;
+      $scope.initUpload = function(itemsToUpload) {
+        $scope.uploadStatus = {
+          itemsToUpload: itemsToUpload,
+          errors: 0,
+          successes: 0,
+          logMessages: [{
+            message: $translate.instant('metadatamanagementApp.' +
+            'dataAcquisitionProject.detail.logMessages.intro') + '\n'
+          }],
+          getProgress: function() {
+            return this.errors + this.successes;
+          },
+          getResult: function() {
+            if (this.errors > 0) {
+              return 'danger';
+            }
+            return 'success';
+          },
+          getProgressState: function() {
+            return this.getProgress() + '/' + this.itemsToUpload;
+          },
+          pushError: function(error) {
+            //add an empty line
+            $scope.uploadStatus.logMessages.push({
+              message: '\n'
+            });
 
-            });
+            // log the dataset id
+            if (error.config.data.id) {
+              this.logMessages.push({
+                message: $translate.instant('metadatamanagementApp' +
+                '.dataAcquisitionProject.detail.logMessages.dataSetNotSaved',
+                {id: error.config.data.id}) + '\n',
+                type: 'error'
+              });
+            }
+
+            //create additional information for the unsaved dataset
+            if (error.data && error.data.errors) {
+              error.data.errors.forEach(function(error) {
+                $scope.uploadStatus.logMessages.push({
+                  message: error.message + '\n',
+                  type: 'error'
+                });
+              });
+            } else if (error.data && error.data.status === 500) {
+              $scope.uploadStatus.logMessages.push({
+                message: $translate.instant('metadatamanagementApp' +
+                '.dataAcquisitionProject.detail.logMessages.' +
+                'internalServerError') + '\n',
+                type: 'error'
+              });
+            }
+            this.errors++;
+          },
+          pushSuccess: function() {
+            this.successes++;
           }
         };
-        $scope.onSurveyUpload = function(file) {
-          if (file !== null) {
-            $scope.show = false;
-            $scope.isOpen = false;
-            $scope.progress = 0;
-            $scope.uploadResult = 'success';
-            $scope.errorMessages = [];
-            ExcelParser.readFileAsync(file)
-            .then(function(fileInputContent) {
-              saveSurveys(fileInputContent);
-            });
-          }
-        };
-        $scope.exportToODT = function() {
-          DataAcquisitionProjectExportService.exportToODT($scope.rdcProject);
-        };
-      });
+      };
+
+      $scope.initUpload(0);
+
+      var saveSurveys = function(surveys) {
+        $scope.initUpload(surveys.length);
+        for (var i = 0; i < surveys.length; i++) {
+          var data = surveys[i];
+          var surveyObj = {
+            id: data.id,
+            dataAcquisitionProjectId: $scope.dataAcquisitionProject.id,
+            questionnaireId: data.questionnaireId,
+            title: {
+              en: data['title.en'],
+              de: data['title.de']
+            },
+            fieldPeriod: {
+              start: data['fieldPeriod.start'],
+              end: data['fieldPeriod.end']
+            }
+          };
+          var survey = new Survey(surveyObj);
+          survey.$save().then(function() {
+            $scope.uploadStatus.pushSuccess();
+          }).catch(function(error) {
+            $scope.uploadStatus.pushError(error);
+          });
+        }
+      };
+      $scope.onSurveyUpload = function(file) {
+        if (file !== null) {
+          ExcelParser.readFileAsync(file)
+            .then(saveSurveys);
+        }
+      };
+      $scope.exportToODT = function() {
+        DataAcquisitionProjectExportService.exportToODT($scope.rdcProject);
+      };
+    });
