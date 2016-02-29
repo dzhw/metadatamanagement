4513d6fa1c9b4cc546c835747b5fa177a49cfdb9
Daniel Katzberg <katzberg@dzhw.eu>
2016-02-29 12:33:21 +0100

    #256 added public german broken links test

--- src/test/protractor/brokenLinks/publicBrokenLinksGerman.spec.js
+++ src/test/protractor/brokenLinks/publicBrokenLinksGerman.spec.js
@@ -8,16 +8,17 @@
 
 'use strict';
 var brokenLinks = require('../utils/brokenLinks');
+var utilMissingTranslations = require('../utils/findMissingTranslations');
 var cacheHelper = require('../utils/cacheHelper');
 
-describe('Check broken links in GERMAN language for ... ', function() {
+describe('Check broken links in GERMAN language for ', function() {
 
   beforeEach(function() {
-    return browser.ignoreSynchronization = true;
+    browser.ignoreSynchronization = true;
   });
 
   afterEach(function() {
-    return browser.ignoreSynchronization = false;
+    browser.ignoreSynchronization = false;
   });
 
   afterAll(function() {
@@ -26,6 +27,31 @@ describe('Check broken links in GERMAN language for ... ', function() {
 
   it('... welcome page', function() {
     var pages = ['/'];
-    brokenLinks.checkLinks('#/de', pages);
+    brokenLinks.checkLinks(
+      utilMissingTranslations.germanLanguage, pages);
+  });
+
+  it('... login page', function() {
+    var pages = ['/login'];
+    brokenLinks.checkLinks(
+      utilMissingTranslations.germanLanguage, pages);
+  });
+
+  it('... register page', function() {
+    var pages = ['/register'];
+    brokenLinks.checkLinks(
+      utilMissingTranslations.germanLanguage, pages);
+  });
+
+  it('... request/reset page', function() {
+    var pages = ['/reset/request'];
+    brokenLinks.checkLinks(
+      utilMissingTranslations.germanLanguage, pages);
+  });
+
+  it('... disclosure page', function() {
+    var pages = ['/disclosure'];
+    brokenLinks.checkLinks(
+      utilMissingTranslations.germanLanguage, pages);
   });
 });
