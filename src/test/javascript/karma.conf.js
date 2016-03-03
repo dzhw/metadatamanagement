0d4fdc59a4f2eb104fdfe1982db0e9e31e08a762
Daniel Katzberg <katzberg@dzhw.eu>
2016-03-03 14:29:06 +0100

    #279 removed code for tracker websockets. removed bower packages for websockets

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -33,8 +33,6 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/json3/lib/json3.js',
             'src/main/webapp/bower_components/ng-file-upload/ng-file-upload.js',
             'src/main/webapp/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
-            'src/main/webapp/bower_components/sockjs-client/dist/sockjs.js',
-            'src/main/webapp/bower_components/stomp-websocket/lib/stomp.min.js',
             'src/main/webapp/bower_components/elasticsearch/elasticsearch.js',
             'src/main/webapp/bower_components/jszip/dist/jszip.js',
             'src/main/webapp/bower_components/file-saver.js/FileSaver.js',
