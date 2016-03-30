43763fc3da5f5aea2713896ae40dbef8071b700e
Amine Limouri <limouri@dzhw.eu>
2016-03-30 16:33:30 +0200

    #303 changed VariablesParser service

--- src/main/webapp/scripts/components/import/parser/variablesParser.service.js
+++ src/main/webapp/scripts/components/import/parser/variablesParser.service.js
@@ -7,89 +7,79 @@ function(Variable, ArrayParser) {
     var jsonContent = {};
     var variablesValues = {};
     var variablesObjArray = [];
-    Object.keys(data.files).forEach(function(key) {
-      var file = data.files[key];
-      if (!file.dir) {
-        if (file.name.indexOf('.json') > -1) {
-          var jsonKey = file.name.replace('values/','').replace('.json','');
-          variablesValues[jsonKey] = JSON.parse(file.asBinary());
-        }
-        if (file.name.indexOf('.xlsx') > -1) {
-          var content = XLSX.read(file.asBinary(), {type: 'binary'});
-          var sheetList = content.SheetNames;
-          var worksheet = content.Sheets[sheetList[0]];
-          // jscs:disable
-          jsonContent = XLSX.utils.sheet_to_json(worksheet);
-          // jscs:enable
-          for (var i = 0; i < jsonContent.length; i++) {
-            var variableObj = {
-              id: jsonContent[i].id,
-              accessWays: ArrayParser.getParsedArray(jsonContent[i].accessWays),
-              dataAcquisitionProjectId: dataAcquisitionProjectId,
-              dataType: {
-                en: jsonContent[i]['dataType.en'],
-                de: jsonContent[i]['dataType.de']
-              },
-              label: {
-                en: jsonContent[i]['label.en'],
-                de: jsonContent[i]['label.de']
-              },
-              description: {
-                en: jsonContent[i]['description.en'],
-                de: jsonContent[i]['description.de']
-              },
-              filterDetaills: {
-                filterExpression:
-                jsonContent[i]['filterDetaills.filterExpression'],
-                filterDescription: {
-                  en: jsonContent[i]['filterDetaills.filterDescription.de'],
-                  de: jsonContent[i]['filterDetaills.filterDescription.en']
-                },
-                filterExpressionLanguage:
-                jsonContent[i]['filterDetaills.filterExpressionLanguage']
-              },
-              sameVariablesInPanel: ArrayParser.
-              getParsedArray(jsonContent[i].sameVariablesInPanel),
-              generationDetails: {
-                rule: jsonContent[i]['generationDetails.rule'],
-                ruleExpressionLanguage: jsonContent[i]
-                ['generationDetails.ruleExpressionLanguage'],
-                description: {
-                  en: jsonContent[i]['generationDetails.description.en'],
-                  de: jsonContent[i]['generationDetails.description.de']
-                }
-              },
-              name: jsonContent[i].name,
-              scaleLevel: {
-                en: jsonContent[i]['scaleLevel.en'],
-                de: jsonContent[i]['scaleLevel.de']
-              },
-              statistics: {
-                firstQuartile: jsonContent[i]['statistics.firstQuartile'],
-                highWhisker: jsonContent[i]['statistics.  highWhisker'],
-                kurtosis: jsonContent[i]['statistics.kurtosis'],
-                lowWhisker: jsonContent[i]['statistics.lowWhisker'],
-                maximum: jsonContent[i]['statistics.maximum'],
-                meanValue: jsonContent[i]['statistics.meanValue'],
-                median: jsonContent[i]['statistics.median'],
-                minimum: jsonContent[i]['statistics.minimum'],
-                skewness: jsonContent[i]['statistics.skewness'],
-                standardDeviation: jsonContent[i]
-                ['statistics.standardDeviation'],
-                thirdQuartile: jsonContent[i]['statistics.thirdQuartile']
-              },
-              values: variablesValues[jsonContent[i].id] ?
-                variablesValues[jsonContent[i].id].values :
-                variablesValues[jsonContent[i].id],
-              surveyId: jsonContent[i].surveyId,
-              conceptId: jsonContent[i].conceptId,
-              dataSetIds: ArrayParser.getParsedArray(jsonContent[i].dataSetIds),
-            };
-            variablesObjArray.push(new Variable(variableObj));
+    var excelFile = data.files['variables.xlsx'];
+    var content = XLSX.read(excelFile.asBinary(), {type: 'binary'});
+    var sheetList = content.SheetNames;
+    var worksheet = content.Sheets[sheetList[0]];
+    // jscs:disable
+    jsonContent = XLSX.utils.sheet_to_json(worksheet);
+    // jscs:enable
+    for (var i = 0; i < jsonContent.length; i++) {
+      var variableObj = {
+        id: jsonContent[i].id,
+        accessWays: ArrayParser.getParsedArray(jsonContent[i].accessWays),
+        dataAcquisitionProjectId: dataAcquisitionProjectId,
+        dataType: {
+          en: jsonContent[i]['dataType.en'],
+          de: jsonContent[i]['dataType.de']
+        },
+        label: {
+          en: jsonContent[i]['label.en'],
+          de: jsonContent[i]['label.de']
+        },
+        description: {
+          en: jsonContent[i]['description.en'],
+          de: jsonContent[i]['description.de']
+        },
+        filterDetaills: {
+          filterExpression:
+          jsonContent[i]['filterDetaills.filterExpression'],
+          filterDescription: {
+            en: jsonContent[i]['filterDetaills.filterDescription.de'],
+            de: jsonContent[i]['filterDetaills.filterDescription.en']
+          },
+          filterExpressionLanguage:
+          jsonContent[i]['filterDetaills.filterExpressionLanguage']
+        },
+        sameVariablesInPanel: ArrayParser.
+        getParsedArray(jsonContent[i].sameVariablesInPanel),
+        generationDetails: {
+          rule: jsonContent[i]['generationDetails.rule'],
+          ruleExpressionLanguage: jsonContent[i]
+          ['generationDetails.ruleExpressionLanguage'],
+          description: {
+            en: jsonContent[i]['generationDetails.description.en'],
+            de: jsonContent[i]['generationDetails.description.de']
           }
-        }
-      }
-    });
+        },
+        name: jsonContent[i].name,
+        scaleLevel: {
+          en: jsonContent[i]['scaleLevel.en'],
+          de: jsonContent[i]['scaleLevel.de']
+        },
+        statistics: {
+          firstQuartile: jsonContent[i]['statistics.firstQuartile'],
+          highWhisker: jsonContent[i]['statistics.  highWhisker'],
+          kurtosis: jsonContent[i]['statistics.kurtosis'],
+          lowWhisker: jsonContent[i]['statistics.lowWhisker'],
+          maximum: jsonContent[i]['statistics.maximum'],
+          meanValue: jsonContent[i]['statistics.meanValue'],
+          median: jsonContent[i]['statistics.median'],
+          minimum: jsonContent[i]['statistics.minimum'],
+          skewness: jsonContent[i]['statistics.skewness'],
+          standardDeviation: jsonContent[i]
+          ['statistics.standardDeviation'],
+          thirdQuartile: jsonContent[i]['statistics.thirdQuartile']
+        },
+        values: data.files['values/'+jsonContent[i].id+'.json'] ?
+          data.files['values/'+jsonContent[i].id+'.json'].values :
+          variablesValues[jsonContent[i].id],
+        surveyId: jsonContent[i].surveyId,
+        conceptId: jsonContent[i].conceptId,
+        dataSetIds: ArrayParser.getParsedArray(jsonContent[i].dataSetIds),
+      };
+      variablesObjArray.push(new Variable(variableObj));
+    }
     console.log(variablesObjArray);
     return variablesObjArray;
   };
