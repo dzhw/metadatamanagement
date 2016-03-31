194f7064f664a6eee114902e098c6625480b4f9c
René Reitmann <reitmann@dzhw.eu>
2016-03-31 12:24:48 +0200

    #267 configured jackson not to serialize null values anymore

--- src/main/java/eu/dzhw/fdz/metadatamanagement/config/JacksonConfiguration.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/config/JacksonConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
@@ -16,6 +17,7 @@ public class JacksonConfiguration {
   Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
     // enable custom data bindings like JSR 310 (e.g. LocalDate)
     return new Jackson2ObjectMapperBuilder()
-      .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+      .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+      .serializationInclusion(Include.NON_NULL);
   }
 }
