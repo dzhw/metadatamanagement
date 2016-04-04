34e9f1fa9412db5831dfb6e230b9b9d22d31d3e4
Amine Limouri <limouri@dzhw.eu>
2016-04-04 16:53:39 +0200

    #303 added support for fileReader api for ie 11

--- src/main/webapp/scripts/components/import/log/jobLoggingService.service.js
+++ src/main/webapp/scripts/components/import/log/jobLoggingService.service.js
@@ -2,88 +2,87 @@
 
 angular.module('metadatamanagementApp').service('JobLog',
 function($translate) {
-  var uploadState = {};
-  var initUploadState = function() {
-    uploadState.errors = 0;
-    uploadState.successes = 0;
-    uploadState.hasFinished = false;
-    uploadState.itemsToUpload = 0;
-    uploadState.progress = 0;
-    uploadState.uploadedDomainObject = '';
-    uploadState.disableButton = false;
-    uploadState.logMessages = [{
+  var state = {};
+  var hasFinished = function() {
+    if (state.progress === state.itemsToUpload) {
+      state.hasFinished = true;
+      state.disableButton = false;
+    }
+  };
+  var start = function(jobName, numberOfSteps, domainObjectName) {
+    state.jobName = jobName;
+    state.numberOfSteps = numberOfSteps;
+    state.currentProgressStep = 100 / numberOfSteps;
+    state.domainObjectName = domainObjectName;
+    state.errors = 0;
+    state.successes = 0;
+    state.progress = 0;
+    state.disableButton = false;
+    state.logMessages = [{
             message: $translate.instant('metadatamanagementApp.' +
                 'dataAcquisitionProject.detail.logMessages.intro') +
               '\n'
           }];
-    uploadState.checkState = function() {
-      if (uploadState.progress >= uploadState.itemsToUpload) {
-        uploadState.hasFinished = true;
-        uploadState.disableButton = false;
-      }
-    };
-    uploadState.getResult = function() {
-      if (uploadState.errors > 0) {
-        return 'danger';
-      }
-      return 'success';
-    };
-    uploadState.getProgressState = function() {
-      return uploadState.progress + '/' + uploadState.itemsToUpload;
-    };
-    uploadState.pushSuccess = function() {
-      uploadState.progress++;
-      uploadState.successes++;
-    };
-    uploadState.pushError = function(error) {
-      //add an empty line
-      uploadState.logMessages.push({
-         message: '\n'
+  };
+  var error = function(error) {
+    state.errors++;
+    state.progress++;
+    hasFinished();
+    state.logMessages.push({
+       message: '\n'
+     });
+    if (typeof error === 'string' || error instanceof String) {
+      state.logMessages.push({
+         message: error + '\n',
+         type: 'error'
        });
-      // if the error is already a string simply display it
-      if (typeof error === 'string' || error instanceof String) {
-        uploadState.logMessages.push({
-           message: error + '\n',
+    }
+    if (error.config && error.config.data && error.config.data.id) {
+      state.logMessages.push({
+         message: $translate.instant('metadatamanagementApp' +
+           '.dataAcquisitionProject.detail.' +
+           'logMessages.dataSetNotSaved', {
+             id: error.config.data.id
+           }) + '\n',
+         type: 'error'
+       });
+    }
+    if (error.data && error.data.errors) {
+      error.data.errors.forEach(function(error) {
+         state.logMessages.push({
+           message: error.message + '\n',
            type: 'error'
          });
-      }
-      // log the dataset id
-      if (error.config && error.config.data && error.config.data.id) {
-        uploadState.logMessages.push({
-           message: $translate.instant('metadatamanagementApp' +
-             '.dataAcquisitionProject.detail.' +
-             'logMessages.dataSetNotSaved', {
-               id: error.config.data.id
-             }) + '\n',
-           type: 'error'
-         });
-      }
-      //create additional information for the unsaved dataset
-      if (error.data && error.data.errors) {
-        error.data.errors.forEach(function(error) {
-           uploadState.logMessages.push({
-             message: error.message + '\n',
-             type: 'error'
-           });
-         });
-      } else if (error.data && error.data.status === 500) {
-        uploadState.logMessages.push({
-           message: $translate.instant('metadatamanagementApp' +
-             '.dataAcquisitionProject.detail.logMessages.' +
-             'internalServerError') + '\n',
-           type: 'error'
-         });
-      } else if (error.data && error.data.message) {
-        uploadState.logMessages.push({
-           message: error.data.message +
-             '\n',
-           type: 'error'
-         });
-      }
-    };
+       });
+    } else if (error.data && error.data.status === 500) {
+      state.logMessages.push({
+         message: $translate.instant('metadatamanagementApp' +
+           '.dataAcquisitionProject.detail.logMessages.' +
+           'internalServerError') + '\n',
+         type: 'error'
+       });
+    } else if (error.data && error.data.message) {
+      state.logMessages.push({
+         message: error.data.message +
+           '\n',
+         type: 'error'
+       });
+    }
+    return state;
+  };
+  var success = function() {
+    state.successes++;
+    state.progress++;
+    hasFinished();
+    return state;
+  };
+  var cancel = function() {
+
   };
   return {
-    initUploadState: initUploadState,
-    uploadState: uploadState
+    start: start,
+    error: error,
+    success: success,
+    cancel: cancel
   };
 });
--- src/main/webapp/scripts/components/import/reader/excelReader.service.js
+++ src/main/webapp/scripts/components/import/reader/excelReader.service.js
@@ -1,10 +1,29 @@
-/* global XLSX, FileReader */
+/* global XLSX, FileReader, jQuery */
 'use strict';
 
 angular.module('metadatamanagementApp').service('ExcelReader', function($q) {
   this.readFileAsync = function(file) {
     var deferred = $q.defer();
     var fileReader = new FileReader();
+    if (FileReader.prototype.readAsBinaryString === undefined) {
+      FileReader.prototype.readAsBinaryString = function(fileData) {
+        var binary = '';
+        var pt = this;
+        var reader = new FileReader();
+        //jshint unused:true
+        reader.onload = function(e) {
+          //jshint unused:false
+          var bytes = new Uint8Array(reader.result);
+          var length = bytes.byteLength;
+          for (var i = 0; i < length; i++) {
+            binary += String.fromCharCode(bytes[i]);
+          }
+          pt.content = binary;
+          jQuery(pt).trigger('onload');
+        };
+        reader.readAsArrayBuffer(fileData);
+      };
+    }
     fileReader.readAsBinaryString(file);
     fileReader.onload = function(e) {
       var data = e.target.result;
