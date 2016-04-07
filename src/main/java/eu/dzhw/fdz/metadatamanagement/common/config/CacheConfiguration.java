c45022b0049f15e29a209c5a1bd50178493af061
René Reitmann <reitmann@dzhw.eu>
2016-04-07 15:36:18 +0200

    #367 simplified mongoDb configuration

--- src/main/java/eu/dzhw/fdz/metadatamanagement/common/config/CacheConfiguration.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/common/config/CacheConfiguration.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Profile;
  */
 @Configuration
 @EnableCaching
-@AutoConfigureAfter(value = {MetricsConfiguration.class, DatabaseConfiguration.class})
+@AutoConfigureAfter(value = {MetricsConfiguration.class, MongoDbConfiguration.class})
 @Profile("!" + Constants.SPRING_PROFILE_FAST)
 public class CacheConfiguration {
 
--- src/main/java/eu/dzhw/fdz/metadatamanagement/common/config/MongoDbConfiguration.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/common/config/MongoDbConfiguration.java
@@ -8,20 +8,18 @@ import javax.inject.Inject;
 import org.mongeez.Mongeez;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -30,17 +28,14 @@ import com.mongodb.Mongo;
 import eu.dzhw.fdz.metadatamanagement.usermanagement.repository.OAuth2AuthenticationReadConverter;
 
 /**
- * Configure the mongo db client instance when not running in the cloud.
+ * Configure the mongo db client instance.
  */
-// TODO ensure that the mongo client configuration is the same as CloudMongoDbConfiguration
 @Configuration
-@Profile("!" + Constants.SPRING_PROFILE_CLOUD)
 @EnableMongoRepositories("eu.dzhw.fdz.metadatamanagement.**.repository")
-@Import(value = MongoAutoConfiguration.class)
 @EnableMongoAuditing(auditorAwareRef = "springSecurityAuditorAware")
-public class DatabaseConfiguration extends AbstractMongoConfiguration {
+public class MongoDbConfiguration {
 
-  private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+  private final Logger log = LoggerFactory.getLogger(MongoDbConfiguration.class);
 
   @Inject
   private Mongo mongo;
@@ -56,6 +51,16 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration {
       LocalValidatorFactoryBean validator) {
     return new ValidatingMongoEventListener(validator);
   }
+  
+  /**
+   * Switch off cloud service discovery in the cloud, since we use vcap
+   * variables in application-prod.yml.
+   */
+  @Profile(Constants.SPRING_PROFILE_CLOUD)
+  @Bean
+  public Cloud cloud() {
+    return new CloudFactory().getCloud();
+  }
 
   /**
    * We need to set springs standard message source for the JSR-303 validation errors.
@@ -67,20 +72,9 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration {
     return validator;
   }
 
-  @Override
-  protected String getDatabaseName() {
-    return mongoProperties.getDatabase();
-  }
-
-  @Override
-  public Mongo mongo() throws Exception {
-    return mongo;
-  }
-
   /**
    * Register custom converters for mongo access.
    */
-  @Override
   @Bean
   public CustomConversions customConversions() {
     List<Converter<?, ?>> converters = new ArrayList<>();
@@ -92,7 +86,7 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration {
    * Configure Mongeez for schema management.
    */
   @Bean
-  @Profile("!" + Constants.SPRING_PROFILE_FAST)
+  @Profile("!" + Constants.SPRING_PROFILE_CLOUD)
   public Mongeez mongeez() {
     log.debug("Configuring Mongeez");
     Mongeez mongeez = new Mongeez();
@@ -101,10 +95,5 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration {
     mongeez.setDbName(mongoProperties.getDatabase());
     mongeez.process();
     return mongeez;
-  }
-
-  @Bean
-  public GridFsTemplate gridFsTemplate() throws Exception {
-    return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
   }
 }
