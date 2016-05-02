ed9a6eba2e6d1c7456c19e63d4270fa47ab51ca7
René Reitmann <reitmann@dzhw.eu>
2016-05-02 21:37:00 +0200

    Releasing v0.1.19 (closes #417)

--- src/main/webapp/scripts/app/app.constants.js
+++ src/main/webapp/scripts/app/app.constants.js
@@ -4,7 +4,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'dev')
 
-.constant('VERSION', '0.1.19-SNAPSHOT')
+.constant('VERSION', '0.1.19')
 
 .constant('ElasticSearchProperties', {apiVersion:'_default',logLevel:'error',pageSize:10})
 
