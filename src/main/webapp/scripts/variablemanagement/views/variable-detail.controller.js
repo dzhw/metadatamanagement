b7631a444e1e952b11e86d313c31dd017bf2bb7a
Daniel Katzberg <katzberg@dzhw.eu>
2016-05-09 11:09:15 +0200

    #410 removed questionnaire pdf. removed histogram with relative values. removed depending js test. fixed bug with box plots, which werent painted if one value had a zero.

--- src/main/webapp/scripts/variablemanagement/views/variable-detail.controller.js
+++ src/main/webapp/scripts/variablemanagement/views/variable-detail.controller.js
@@ -4,66 +4,11 @@
 
 angular.module('metadatamanagementApp')
   .controller('VariableDetailController', ['$scope', 'entity', 'Language',
-    '$translate',
+    '$translate', 'CleanJSObjectService',
 
-    function($scope, entity, Language, $translate) {
+    function($scope, entity, Language, $translate, CleanJSObjectService) {
       $scope.variable = entity;
       //options for charts
-      $scope.optionsRelativeFrequencyChart = {
-        chart: {
-          //CSS information
-          type: 'discreteBarChart',
-          height: 250,
-          showValues: true,
-          color: ['#0069B4'],
-          valueFormat: function(d) {
-            //.2f means two numbers after a commata
-            return d3.format(',.2f')(d);
-          },
-
-          transitionDuration: 500,
-
-          //Point for data extraction to visualisation
-          x: function(d) {
-            //if en, return en lables
-            if (Language.getCurrentInstantly() === 'en') {
-              if (d.label && d.label.en) {
-                return d.label.en;
-                //return the code, if label is not set
-              } else {
-                return d.value;
-              }
-            }
-
-            //de is default. return de labels.
-            if (d.label && d.label.de) {
-              return d.label.de;
-              //return code, if label is not set
-            } else {
-              return d.value;
-            }
-          },
-
-          y: function(d) {
-            return d.relativeFrequency;
-          },
-        },
-
-        // title options
-        title: {
-          enable: true,
-          text: '',
-        },
-        // subtitle options
-        subtitle: {
-          enable: true,
-          text: '',
-          css: {
-            'text-align': 'center',
-            margin: '10px 13px 0px 7px',
-          },
-        }
-      };
 
       $scope.optionsAbsoluteFrequencyChart = {
         chart: {
@@ -133,35 +78,37 @@ angular.module('metadatamanagementApp')
 
         // Box Plot Chart
         //----------------------------------------------------------------------
-        if (statistics.firstQuartile && statistics.median &&
-          statistics.thirdQuartile && statistics.lowWhisker &&
-          statistics.highWhisker) {
+        if (!CleanJSObjectService.isNullOrEmpty(statistics.firstQuartile) &&
+          !CleanJSObjectService.isNullOrEmpty(statistics.median) &&
+          !CleanJSObjectService.isNullOrEmpty(statistics.thirdQuartile) &&
+          !CleanJSObjectService.isNullOrEmpty(statistics.lowWhisker) &&
+          !CleanJSObjectService.isNullOrEmpty(statistics.highWhisker)) {
           $scope.optionsBoxPlot = {
-          chart: {
-            type: 'boxPlotChart',
-            height: 550,
-            color: ['#0069B4'],
-            x: function(d) {
-              return d.label;
-            },
-            y: function(d) {
-              return d.values.Q3;
-            },
-            yDomain: [statistics.lowWhisker, statistics.highWhisker]
-          }
-        };
+            chart: {
+              type: 'boxPlotChart',
+              height: 550,
+              color: ['#0069B4'],
+              x: function(d) {
+                return d.label;
+              },
+              y: function(d) {
+                return d.values.Q3;
+              },
+              yDomain: [statistics.lowWhisker, statistics.highWhisker]
+            }
+          };
           // Code here will not be linted with JSCS.
           // jscs:disable
           $scope.dataBoxPlot = [{
-          label: $scope.variable.name,
-          values: {
-            Q1: statistics.firstQuartile,
-            Q2: statistics.median,
-            Q3: statistics.thirdQuartile,
-            whisker_low: statistics.lowWhisker,
-            whisker_high: statistics.highWhisker
-          }
-        }];
+            label: $scope.variable.name,
+            values: {
+              Q1: statistics.firstQuartile,
+              Q2: statistics.median,
+              Q3: statistics.thirdQuartile,
+              whisker_low: statistics.lowWhisker,
+              whisker_high: statistics.highWhisker
+            }
+          }];
           // Code here will be linted with JSCS.
           // jscs:enable
         }
@@ -169,13 +116,6 @@ angular.module('metadatamanagementApp')
           'metadatamanagementApp.variable.chart.absoluteFrequency').then(
           function(translation) {
             $scope.optionsAbsoluteFrequencyChart.subtitle.text =
-              translation;
-          });
-
-        $translate(
-          'metadatamanagementApp.variable.chart.relativeFrequency').then(
-          function(translation) {
-            $scope.optionsRelativeFrequencyChart.subtitle.text =
               translation;
           });
       });
