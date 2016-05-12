956a3129b852a9877f7d81401decb5893dc79cb6
Daniel Katzberg <katzberg@dzhw.eu>
2016-05-12 11:01:44 +0200

    #379 added check for null and empty at rule expression language validator

--- src/main/java/eu/dzhw/fdz/metadatamanagement/variablemanagement/domain/validation/ValidRuleExpressionLanguageValidator.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/variablemanagement/domain/validation/ValidRuleExpressionLanguageValidator.java
@@ -3,6 +3,8 @@ package eu.dzhw.fdz.metadatamanagement.variablemanagement.domain.validation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.google.common.base.Strings;
+
 import eu.dzhw.fdz.metadatamanagement.variablemanagement.domain.RuleExpressionLanguages;
 
 /**
@@ -31,6 +33,11 @@ public class ValidRuleExpressionLanguageValidator
    */
   @Override
   public boolean isValid(String ruleExpressionLanguage, ConstraintValidatorContext context) {
+
+    // It is allowed, that the rule expression language is empty.
+    if (Strings.isNullOrEmpty(ruleExpressionLanguage)) {
+      return true;
+    }
 
     // check for rule expression languages
     return RuleExpressionLanguages.ALL.contains(ruleExpressionLanguage);
