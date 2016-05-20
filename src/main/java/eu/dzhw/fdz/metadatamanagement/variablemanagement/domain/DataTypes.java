f57cac77482357c0bc2504bc5785e65c91ca7e23
Daniel Katzberg <katzberg@dzhw.eu>
2016-05-20 12:08:04 +0200

    #458 added new data type date

--- src/main/java/eu/dzhw/fdz/metadatamanagement/variablemanagement/domain/DataTypes.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/variablemanagement/domain/DataTypes.java
@@ -14,6 +14,7 @@ public class DataTypes {
 
   public static final I18nString STRING = new I18nString("string", "string");
   public static final I18nString NUMERIC = new I18nString("numerisch", "numeric");
+  public static final I18nString DATE = new I18nString("datum", "date");
   public static final Set<I18nString> ALL =
-      Collections.unmodifiableSet(new HashSet<I18nString>(Arrays.asList(STRING, NUMERIC)));
+      Collections.unmodifiableSet(new HashSet<I18nString>(Arrays.asList(STRING, NUMERIC, DATE)));
 }
