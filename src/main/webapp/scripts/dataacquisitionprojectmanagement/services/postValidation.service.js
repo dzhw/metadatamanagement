71200f26e8535adc758505dd935a17b45ee7f2d7
Daniel Katzberg <katzberg@dzhw.eu>
2016-05-23 15:58:24 +0200

    #347 added connection between post validation service and job logging service

--- src/main/webapp/scripts/dataacquisitionprojectmanagement/services/postValidation.service.js
+++ src/main/webapp/scripts/dataacquisitionprojectmanagement/services/postValidation.service.js
@@ -3,16 +3,28 @@
 angular.module('metadatamanagementApp').service(
   'DataAcquisitionProjectPostValidationService',
   function($translate, JobLoggingService,
-    DataAcquisitionProjectPostValidationResource,
-    ErrorMessageResolverService) {
+    DataAcquisitionProjectPostValidationResource) {
 
     var postValidate = function(id) {
       // To be removed
-      console.log(ErrorMessageResolverService);
+      JobLoggingService.start('postValidation');
       DataAcquisitionProjectPostValidationResource.postValidate({
         id: id
+      }, function(result) {
+        // everything is okay.
+        if (result.errors.length > 0) {
+          for (var i = 0; i < result.errors.length; i++) {
+            JobLoggingService.error(result.errors[i]);
+          }
+        } else {
+          JobLoggingService.success();
+        }
+        JobLoggingService.finish($translate.instant(
+          'metadatamanagementApp.dataAcquisitionProject.detail.' +
+          'logMessages.postValidationTerminated', {}));
       });
     };
+
     return {
       postValidate: postValidate
     };
