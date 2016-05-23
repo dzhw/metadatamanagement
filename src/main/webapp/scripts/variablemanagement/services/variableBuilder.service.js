7ef66f7157ec98a2c38ea5736c4b50c10f88962e
René Reitmann <reitmann@dzhw.eu>
2016-05-23 12:08:29 +0200

    #368 changed import source of variable attributes

--- src/main/webapp/scripts/variablemanagement/services/variableBuilder.service.js
+++ src/main/webapp/scripts/variablemanagement/services/variableBuilder.service.js
@@ -1,82 +1,73 @@
 'use strict';
 
 angular.module('metadatamanagementApp').service('VariableBuilderService',
-function(VariableResource, CleanJSObjectService, $translate) {
-  var parseErrors = [];
-  var getVariables = function(variables, zip, dataAcquisitionProjectId) {
-    var variablesObjArray = [];
-    for (var i = 0; i < variables.length; i++) {
-      var distribution = {};
-      try {
-        distribution = zip.files['values/' + variables[i].id + '.json'] ?
+  function(VariableResource, CleanJSObjectService, $translate) {
+    var parseErrors = [];
+    var getVariables = function(variables, zip, dataAcquisitionProjectId) {
+      var variablesObjArray = [];
+      for (var i = 0; i < variables.length; i++) {
+        var generatedVariable = {};
+        try {
+          generatedVariable = zip.files['variables/' + variables[i].id +
+              '.json'] ?
             JSON.
-            parse(zip.files['values/' + variables[i].id + '.json'].asText())
-            .distribution : undefined;
-      }catch (e) {
-        parseErrors.push('\n' + $translate.instant(
-          'metadatamanagementApp.dataAcquisitionProject.detail.' +
-          'logMessages.malformedJsonFile', {id: variables[i].id}));
-        continue;
-      }
-      var variableObj = {
-        id: variables[i].id,
-        dataType: {
-          en: variables[i]['dataType.en'],
-          de: variables[i]['dataType.de']
-        },
-        scaleLevel: {
-          en: variables[i]['scaleLevel.en'],
-          de: variables[i]['scaleLevel.de']
-        },
-        name: variables[i].name,
-        label: {
-          en: variables[i]['label.en'],
-          de: variables[i]['label.de']
-        },
-        description: {
-          en: variables[i]['description.en'],
-          de: variables[i]['description.de']
-        },
-        accessWays: CleanJSObjectService.
-        removeWhiteSpace(variables[i].accessWays),
-
-        filterDetails: {
-          expression:
-          variables[i]['filterDetails.expression'],
+          parse(zip.files['variables/' + variables[i].id + '.json'].asText()) :
+            undefined;
+        } catch (e) {
+          parseErrors.push('\n' + $translate.instant(
+            'metadatamanagementApp.dataAcquisitionProject.detail.' +
+            'logMessages.malformedJsonFile', {
+              id: variables[i].id
+            }));
+          continue;
+        }
+        var variableObj = {
+          id: variables[i].id,
+          dataType: generatedVariable.dataType,
+          scaleLevel: generatedVariable.scaleLevel,
+          name: generatedVariable.name,
+          label: generatedVariable.label,
           description: {
-            en: variables[i]['filterDetails.description.de'],
-            de: variables[i]['filterDetails.description.en']
+            en: variables[i]['description.en'],
+            de: variables[i]['description.de']
           },
-          expressionLanguage:
-          variables[i]['filterDetails.expressionLanguage']
-        },
-        sameVariablesInPanel: CleanJSObjectService.
-        removeWhiteSpace(variables[i].sameVariablesInPanel),
-        surveyIds: CleanJSObjectService.
-        removeWhiteSpace(variables[i].surveyIds),
-        conceptId: variables[i].conceptId,
-        dataSetIds: CleanJSObjectService.
-        removeWhiteSpace(variables[i].dataSetIds),
-        atomicQuestionId: variables[i].atomicQuestionId,
-        dataAcquisitionProjectId: dataAcquisitionProjectId,
-        generationDetails: {
-          rule: variables[i]['generationDetails.rule'],
-          ruleExpressionLanguage: variables[i]
-          ['generationDetails.ruleExpressionLanguage'],
-          description: {
-            en: variables[i]['generationDetails.description.en'],
-            de: variables[i]['generationDetails.description.de']
-          }
-        },
-        distribution: distribution,
-      };
-      CleanJSObjectService.removeEmptyJsonObjects(variableObj);
-      variablesObjArray.push(new VariableResource(variableObj));
-    }
-    return variablesObjArray;
-  };
-  return {
+          accessWays: CleanJSObjectService.
+          removeWhiteSpace(variables[i].accessWays),
+
+          filterDetails: {
+            expression: variables[i]['filterDetails.expression'],
+            description: {
+              en: variables[i]['filterDetails.description.de'],
+              de: variables[i]['filterDetails.description.en']
+            },
+            expressionLanguage: variables[i][
+              'filterDetails.expressionLanguage'
+            ]
+          },
+          sameVariablesInPanel: generatedVariable.sameVariablesInPanel,
+          surveyIds: generatedVariable.surveyIds,
+          conceptId: variables[i].conceptId,
+          dataSetIds: generatedVariable.dataSetIds,
+          atomicQuestionId: generatedVariable.atomicQuestionId,
+          dataAcquisitionProjectId: dataAcquisitionProjectId,
+          generationDetails: {
+            rule: variables[i]['generationDetails.rule'],
+            ruleExpressionLanguage: variables[i]
+              ['generationDetails.ruleExpressionLanguage'],
+            description: {
+              en: variables[i]['generationDetails.description.en'],
+              de: variables[i]['generationDetails.description.de']
+            }
+          },
+          distribution: generatedVariable.distribution,
+        };
+        CleanJSObjectService.removeEmptyJsonObjects(variableObj);
+        variablesObjArray.push(new VariableResource(variableObj));
+      }
+      return variablesObjArray;
+    };
+    return {
       getVariables: getVariables,
       getParseErrors: parseErrors
     };
-});
+  });
