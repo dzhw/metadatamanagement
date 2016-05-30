1ce7bb563c758e697b66222157ab9e33a6a523b5
Daniel Katzberg <katzberg@dzhw.eu>
2016-05-30 15:04:24 +0200

    #473 fixed issue with get request for the navbar

--- src/test/javascript/spec/helpers/httpBackend.js
+++ src/test/javascript/spec/helpers/httpBackend.js
@@ -4,6 +4,8 @@ function mockApis() {
   inject(function($httpBackend) {
     $httpBackend.whenGET(/api\/account.*/).respond({});
     $httpBackend.whenGET(/i18n\/de\/.+\.json/).respond({});
+    $httpBackend.whenGET('scripts/common/navbar/views/navbar.html.tmpl').
+    respond({});
     $httpBackend.whenGET('scripts/home/home.html.tmpl').respond({});
   });
 }
