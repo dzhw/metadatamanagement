26c802ac63b076474b909a63191d1314fc653e98
René Reitmann <reitmann@dzhw.eu>
2016-06-02 21:10:17 +0200

    #407 moved build number to sauceBuild property

--- src/test/protractor/protractor.conf.js
+++ src/test/protractor/protractor.conf.js
@@ -12,6 +12,7 @@ exports.config = {
   //disable these if you want to run tests locally
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
+  sauceBuild: process.env.TRAVIS_BUILD_NUMBER,
 
   baseUrl: 'https://metadatamanagement-dev.cfapps.io/',
   specs: [
@@ -29,19 +30,16 @@ exports.config = {
   multiCapabilities: [{
     'name': 'Win8.1/Chrome',
     'platform': 'Windows 8.1',
-    'browserName': 'chrome',
-    'build': process.env.TRAVIS_BUILD_NUMBER
+    'browserName': 'chrome'
   }, {
     'name': 'Win8.1/Firefox',
     'platform': 'Windows 8.1',
-    'browserName': 'firefox',
-    'build': process.env.TRAVIS_BUILD_NUMBER
+    'browserName': 'firefox'
   }, {
     'name': 'Win8.1/IE11',
     'platform': 'Windows 8.1',
     'browserName': 'internet explorer',
-    'version': '11.0',
-    'build': process.env.TRAVIS_BUILD_NUMBER
+    'version': '11.0'
   }],
 
   //enable this for local tests without selenium
