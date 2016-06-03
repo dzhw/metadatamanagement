8c355978b3f95d6162aa91a40085943bf767b84d
Amine Limouri <limouri@dzhw.eu>
2016-06-03 11:47:30 +0200

    #421 added tests

--- src/test/protractor/disclosure/disclosure.spec.js
+++ src/test/protractor/disclosure/disclosure.spec.js
@@ -10,7 +10,6 @@
 
 var findNotTranslationedStrings =
 require('../utils/findNotTranslationedStrings');
-var findBrockenUrls = require('../utils/findBrockenUrls');
 
 describe('Disclosure page', function() {
   beforeAll(function() {
@@ -22,31 +21,6 @@ describe('Disclosure page', function() {
       findNotTranslationedStrings.checkHTMLContent(el, '#/de/disclosure')
       .then(function(result) {
         expect(result.length).toBe(0, result.message);
-      });
-    });
-  });
-
-  describe('URLs', function() {
-    var htmlContent;
-    var currentUrl;
-    beforeAll(function() {
-      htmlContent = element.all(by.css('.container')).get(1);
-      browser.getCurrentUrl().then(function(url) {
-        currentUrl = url;
-      });
-    });
-    it('should check DZHW external URL', function(done) {
-      htmlContent.all(by.css('a')).then(function(items) {
-        items[0].getAttribute('href').then(function(href) {
-          return href;
-        }).then(function(url) {
-          items[0].click().then(function() {
-            findBrockenUrls.checkHREFs(url, currentUrl).then(function(result) {
-              done();
-              expect(result.isValidUrl).toBe(true, result.message);
-            });
-          });
-        });
       });
     });
   });
