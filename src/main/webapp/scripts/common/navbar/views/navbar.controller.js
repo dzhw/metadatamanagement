cd1a8858890e6d3650e1757dc90e8b1faf1a7799
Daniel Katzberg <katzberg@dzhw.eu>
2016-06-06 14:33:10 +0200

    #473 non working first version of a toggle

--- src/main/webapp/scripts/common/navbar/views/navbar.controller.js
+++ src/main/webapp/scripts/common/navbar/views/navbar.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('metadatamanagementApp').controller('NavbarController',
-  function($scope, $state, Auth, Principal, ENV) {
+angular.module('metadatamanagementApp').controller(
+  'NavbarController',
+  function($scope, $state, Auth, Principal, ENV, $filter, $mdSidenav) {
     $scope.isAuthenticated = Principal.isAuthenticated;
     $scope.$state = $state;
     $scope.inProductionOrDev = ENV === 'prod' || ENV === 'dev';
@@ -9,5 +10,30 @@ angular.module('metadatamanagementApp').controller('NavbarController',
     $scope.logout = function() {
       Auth.logout();
       $state.go('home');
+    };
+
+    var toggleMenues = [{
+      name: 'menu-entities',
+      toggle: false
+    }];
+
+    $scope.toggleMenues = toggleMenues;
+
+    $scope.isOpen = function() {
+      return true;
+    };
+
+    $scope.toggle = function($event) {
+      console.log($event);
+      var toggleElement = $event.currentTarget.attributes[4].value;
+      var found = $filter('filter')($scope.toggleMenues, {
+        name: toggleElement
+      }, true);
+      if (found.length) {
+        $event.currentTarget.attributes[5].value = !$event.currentTarget
+          .attributes[5].value;
+        found[0].toggle = !found[0].toggle;
+        $mdSidenav('menu-entities').toggle();
+      }
     };
   });
