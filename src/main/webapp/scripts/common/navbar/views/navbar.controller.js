ed6644b6f0582bf472513638a1116090eaddb2db
Daniel Katzberg <katzberg@dzhw.eu>
2016-06-07 11:59:52 +0200

    #473 removed toggle feature from md-buttons in side nav

--- src/main/webapp/scripts/common/navbar/views/navbar.controller.js
+++ src/main/webapp/scripts/common/navbar/views/navbar.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('metadatamanagementApp').controller(
   'NavbarController',
-  function($scope, $state, Auth, Principal, ENV, $filter, $mdSidenav) {
+  function($scope, $state, Auth, Principal, ENV, $filter) {
     $scope.isAuthenticated = Principal.isAuthenticated;
     $scope.$state = $state;
     $scope.inProductionOrDev = ENV === 'prod' || ENV === 'dev';
@@ -10,30 +10,5 @@ angular.module('metadatamanagementApp').controller(
     $scope.logout = function() {
       Auth.logout();
       $state.go('home');
-    };
-
-    var toggleMenues = [{
-      name: 'menu-entities',
-      toggle: false
-    }];
-
-    $scope.toggleMenues = toggleMenues;
-
-    $scope.isOpen = function() {
-      return true;
-    };
-
-    $scope.toggle = function($event) {
-      console.log($event);
-      var toggleElement = $event.currentTarget.attributes[4].value;
-      var found = $filter('filter')($scope.toggleMenues, {
-        name: toggleElement
-      }, true);
-      if (found.length) {
-        $event.currentTarget.attributes[5].value = !$event.currentTarget
-          .attributes[5].value;
-        found[0].toggle = !found[0].toggle;
-        $mdSidenav('menu-entities').toggle();
-      }
     };
   });
