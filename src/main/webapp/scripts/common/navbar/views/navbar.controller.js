c0c33aba938a1b0ee5f4320214c308be3ccb151a
Daniel Katzberg <katzberg@dzhw.eu>
2016-06-08 10:48:31 +0200

    #473 removed not needed js code for sidenavbar

--- src/main/webapp/scripts/common/navbar/views/navbar.controller.js
+++ src/main/webapp/scripts/common/navbar/views/navbar.controller.js
@@ -2,48 +2,19 @@
 
 angular.module('metadatamanagementApp').controller(
   'NavbarController',
-  function($scope, $state, Auth, Principal, ENV, $mdSidenav, $timeout) {
+  function($scope, $state, Auth, Principal, ENV, $mdSidenav) {
     $scope.isAuthenticated = Principal.isAuthenticated;
     $scope.$state = $state;
     $scope.inProductionOrDev = ENV === 'prod' || ENV === 'dev';
 
+    //Logout function
     $scope.logout = function() {
       Auth.logout();
       $state.go('home');
     };
 
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait) {
-      var timer;
-      return function debounced() {
-        var args = Array.prototype.slice.call(arguments);
-        console.log('HALLO');
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply($scope, args);
-        }, wait || 10);
-      };
-    }
-
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        // Component lookup should always be
-        //available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function() {
-            console.log('toggle ' + navID + ' is done');
-          });
-      }, 200);
-    }
-
-    $scope.toggleLeft = buildDelayedToggler('left');
+    //Toggle Function
+    $scope.toggleLeft = function() {
+      $mdSidenav('SideNavBar').toggle();
+    };
   });
