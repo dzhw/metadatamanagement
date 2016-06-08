92b577059986226880e91aacf6dd0919b7ed4ff7
Daniel Katzberg <katzberg@dzhw.eu>
2016-06-08 10:38:49 +0200

    #473 added first functional toggle button for side nav

--- src/main/webapp/scripts/common/navbar/views/navbar.controller.js
+++ src/main/webapp/scripts/common/navbar/views/navbar.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('metadatamanagementApp').controller(
   'NavbarController',
-  function($scope, $state, Auth, Principal, ENV) {
+  function($scope, $state, Auth, Principal, ENV, $mdSidenav, $timeout) {
     $scope.isAuthenticated = Principal.isAuthenticated;
     $scope.$state = $state;
     $scope.inProductionOrDev = ENV === 'prod' || ENV === 'dev';
@@ -11,4 +11,39 @@ angular.module('metadatamanagementApp').controller(
       Auth.logout();
       $state.go('home');
     };
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait) {
+      var timer;
+      return function debounced() {
+        var args = Array.prototype.slice.call(arguments);
+        console.log('HALLO');
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply($scope, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        // Component lookup should always be
+        //available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function() {
+            console.log('toggle ' + navID + ' is done');
+          });
+      }, 200);
+    }
+
+    $scope.toggleLeft = buildDelayedToggler('left');
   });
