b7b6426f00c815ecfdb85fd1b1d5fc3e2913d9a5
Daniel Katzberg <katzberg@dzhw.eu>
2016-06-10 06:56:59 +0200

    #480 added small elements to the search controller

--- src/main/webapp/scripts/variablemanagement/views/variable.controller.js
+++ src/main/webapp/scripts/variablemanagement/views/variable.controller.js
@@ -4,7 +4,7 @@
 angular.module('metadatamanagementApp')
   .controller('VariableController', function($scope,
     VariableResource, $location, $filter, VariableSearchDao, AlertService,
-    ElasticSearchProperties) {
+    ElasticSearchProperties, $timeout) {
     // TODO create global page class which is returned from search daos
     $scope.page = {
       size: ElasticSearchProperties.pageSize,
@@ -120,4 +120,40 @@ angular.module('metadatamanagementApp')
           $('#deleteVariableConfirmation').modal('hide');
         });
     };
+    // --------------------------------------------------------------
+    // In this example, we set up our model using a plain object.
+    // Using a class works too. All that matters is that we implement
+    // getItemAtIndex and getLength.
+    this.infiniteItems = {
+      numLoaded_: 0,
+      toLoad_: 0,
+      // Required.
+      getItemAtIndex: function(index) {
+        if (index > this.numLoaded_) {
+          this.fetchMoreItems_(index);
+          return null;
+        }
+        return index;
+      },
+      // Required.
+      // For infinite scroll behavior, we always return a slightly higher
+      // number than the previously loaded items.
+      getLength: function() {
+        return this.numLoaded_ + 5;
+      },
+      fetchMoreItems_: function(index) {
+        // For demo purposes, we simulate loading more items with a timed
+        // promise. In real code, this function would likely contain an
+        // $http request.
+        if (this.toLoad_ < index) {
+          this.toLoad_ += 20;
+          $timeout(angular.noop, 300).then(angular.bind(this,
+            function() {
+              this.numLoaded_ = this.toLoad_;
+            }));
+        }
+      }
+    };
+
+    $scope.infiniteItems = this.infiniteItems;
   });
