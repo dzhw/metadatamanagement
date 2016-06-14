527ebde40d09553c70c7f1f31525146d7cf8e93f
René Reitmann <reitmann@dzhw.eu>
2016-06-14 13:56:19 +0200

    #421 added dedicated protractor user

--- src/test/protractor/utils/loginHelper.js
+++ src/test/protractor/utils/loginHelper.js
@@ -7,11 +7,12 @@
 
 function login() {
   browser.get('#/de/login');
-  element(by.id('username')).sendKeys('admin');
-  element(by.id('password')).sendKeys('admin');
+  element(by.id('username')).sendKeys('protractor');
+  element(by.id('password')).sendKeys('protractor');
   element(by.css('.form button[type="submit"]')).click();
   expect(element(by.css('[ng-click="logout()"]')).isPresent()).toBe(true);
 }
+
 function logout() {
   element(by.css('[ng-click="logout()"]')).click();
   expect(element(by.css('[ng-click="logout()"]')).isPresent()).toBe(false);
