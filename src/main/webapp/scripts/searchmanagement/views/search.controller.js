b0921a27206620ecc2e87c2e87d3241ca9a84b30
Daniel Katzberg <katzberg@dzhw.eu>
2016-06-15 11:52:27 +0200

    #480 changed pager/pagination. that save the currentPage for every tab

--- src/main/webapp/scripts/searchmanagement/views/search.controller.js
+++ src/main/webapp/scripts/searchmanagement/views/search.controller.js
@@ -1,14 +1,25 @@
 /* Author: Daniel Katzberg */
 'use strict';
 
+/* The Controller for the search. It differs between tabs and a tab represent
+a result of a type like variable or dataSet and so on. */
 angular.module('metadatamanagementApp').controller('SearchController',
   function($scope, Principal, ElasticSearchProperties, $location,
     AlertService, VariableSearchDao, $timeout) {
+
+    //Check the login status
     Principal.identity().then(function(account) {
       $scope.account = account;
       $scope.isAuthenticated = Principal.isAuthenticated;
     });
 
+    //Max number of button at a pager / pagination
+    $scope.pagerMaxSize = 7;
+
+    //Need for interpretation of the query element in the url.
+    $scope.query = $location.search().query;
+
+    //The current index of the active tab
     $scope.selectedTabIndex = 0;
 
     //Information for the different tabs
@@ -16,12 +27,14 @@ angular.module('metadatamanagementApp').controller('SearchController',
       title: 'global.menu.search.all',
       elasticSearchType: '',
       totalHits: 0,
-      searchResult: []
+      searchResult: [],
+      currentPageNumber: 1
     }, {
       title: 'global.menu.search.variables',
       elasticSearchType: 'variables',
       totalHits: 0,
-      searchResult: []
+      searchResult: [],
+      currentPageNumber: 1
     }];
 
     //Watch the tabs, set index of current active tab
@@ -29,30 +42,28 @@ angular.module('metadatamanagementApp').controller('SearchController',
       $scope.selectedTabIndex = current;
     });
 
-    //Information for the pager / pagination
-    $scope.pager = {
-      currentPageNumber: 1,
-      maxSize: 7,
-    };
-
-    //Need for interpretation of the query element in the url.
-    $scope.query = $location.search().query;
-
     //Search function
     $scope.search = function(pageNumber) {
+      //Iterate the tabs. The search results have to be
+      //different between the tabs
       $scope.tabs.forEach(function(tab) {
+        var tabIndex = $scope.tabs.indexOf(tab);
 
-        if (pageNumber) {
-          $scope.pager.currentPageNumber = pageNumber;
+        //save actual page for every pagination of the tab
+        if (pageNumber && tabIndex === $scope.selectedTabIndex) {
+          tab.currentPageNumber = pageNumber;
         }
 
+        //Search with different types, binded on every tab
         $location.search('query', $scope.query);
-        $location.search('page', $scope.pager.currentPageNumber);
-        VariableSearchDao.search($scope.query, $scope.pager.currentPageNumber,
+        $location.search('page', tab.currentPageNumber);
+        VariableSearchDao.search($scope.query, tab.currentPageNumber,
             tab.elasticSearchType)
           .then(function(data) {
             tab.searchResult = data.hits.hits;
             tab.totalHits = data.hits.total;
+
+            //If something going wrong: send an alert
           }, function(error) {
             AlertService.error(error.message);
             console.trace(error);
