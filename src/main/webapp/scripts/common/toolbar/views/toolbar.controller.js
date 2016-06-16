86bee765a475189ba85834a3e4d61131b77f9d08
Daniel Katzberg <katzberg@dzhw.eu>
2016-06-16 09:18:20 +0200

    #473 added some comments. beautify the index.html

--- src/main/webapp/scripts/common/toolbar/views/toolbar.controller.js
+++ src/main/webapp/scripts/common/toolbar/views/toolbar.controller.js
@@ -5,22 +5,25 @@ angular.module('metadatamanagementApp').controller(
   function($scope, $state, Auth, Principal, $mdSidenav, Language, $mdMedia) {
     $scope.isAuthenticated = Principal.isAuthenticated;
 
+    //Set Languages
     $scope.changeLanguage = function(languageKey) {
       Language.setCurrent(languageKey);
     };
 
     $scope.$mdMedia = $mdMedia;
 
-    //Logout function
+    //Goto Logout Page
     $scope.logout = function() {
       Auth.logout();
       $state.go('search');
     };
 
+    //Goto Login Page
     $scope.login = function() {
       $state.go('login');
     };
 
+    //Register function
     $scope.register = function() {
       $state.go('register');
     };
