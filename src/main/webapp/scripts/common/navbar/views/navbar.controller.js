ed0990226ceaced1f8bbeb825ed20c54c1472fed
Daniel Katzberg <katzberg@dzhw.eu>
2016-06-16 09:04:53 +0200

    #473 added comments. removed arialabel warning

--- src/main/webapp/scripts/common/navbar/views/navbar.controller.js
+++ src/main/webapp/scripts/common/navbar/views/navbar.controller.js
@@ -1,14 +1,15 @@
 'use strict';
 
-angular.module('metadatamanagementApp').controller(
-  'NavbarController',
+angular.module('metadatamanagementApp').controller('NavbarController',
   function($scope, Principal) {
     $scope.isAuthenticated = Principal.isAuthenticated;
 
+    //For toggle buttons
     $scope.isAdminMenuOpen = false;
     $scope.isEntityMenuOpen = false;
     $scope.isAccountMenuOpen = false;
 
+    //Functions for toggling buttons.
     $scope.toggleEntityMenu = function() {
       $scope.isEntityMenuOpen = !$scope.isEntityMenuOpen;
     };
