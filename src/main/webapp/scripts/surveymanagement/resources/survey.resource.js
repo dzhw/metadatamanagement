6e53adfe1f67094b243a2f328aa3c4ebd677f76d
Daniel Katzberg <katzberg@dzhw.eu>
2016-07-12 15:10:52 +0200

    #479 removed date util

--- src/main/webapp/scripts/surveymanagement/resources/survey.resource.js
+++ src/main/webapp/scripts/surveymanagement/resources/survey.resource.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('metadatamanagementApp')
-  .factory('SurveyResource', function($resource, DateUtils, $state) {
+  .factory('SurveyResource', function($resource, $state) {
     return $resource('/api/surveys/:id', {
       id: '@id'
     }, {
@@ -15,29 +15,11 @@ angular.module('metadatamanagementApp')
             if (response.status === 404) {
               $state.go('error');
             }
-          },
-          transformResponse: function(data) {
-            // data might be empty if 404
-            if (data) {
-              data = angular.fromJson(data);
-              data.fieldPeriod.start =
-                DateUtils.convertLocaleDateFromServer(data.fieldPeriod.start);
-              data.fieldPeriod.end =
-                DateUtils.convertLocaleDateFromServer(data.fieldPeriod.end);
-              return data;
-            }
           }
         },
       },
       'save': {
-        method: 'PUT',
-        transformRequest: function(data) {
-          data.fieldPeriod.start =
-            DateUtils.convertLocaleDateToServer(data.fieldPeriod.start);
-          data.fieldPeriod.end =
-            DateUtils.convertLocaleDateToServer(data.fieldPeriod.end);
-          return angular.toJson(data);
-        }
+        method: 'PUT'
       },
       'delete': {
         method: 'DELETE'
