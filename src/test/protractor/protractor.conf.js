52644d64a419e05c03179a7997ec814d92917d44
Amine Limouri <limouri@dzhw.eu>
2016-07-12 14:56:03 +0200

    #421 added configuration for protractor

--- src/test/protractor/protractor.conf.js
+++ src/test/protractor/protractor.conf.js
@@ -1,6 +1,5 @@
-
 /* global browser */
-// conf.js
+
 'use strict';
 
 exports.config = {
@@ -8,13 +7,37 @@ exports.config = {
     require('./utils/locators.js');
     browser.driver.manage().window().maximize();
   },
-  baseUrl: 'http://localhost:8080/',
+  //disable these if you want to run tests locally
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  sauceBuild: process.env.TRAVIS_BUILD_NUMBER,
+
+  baseUrl: 'https://metadatamanagement-dev.cfapps.io/',
   specs: [
     'home/home.spec.js',
     'disclosure/disclosure.spec.js',
     'common/navbar.spec.js',
     'common/toolbar.spec.js',
     ],
+  multiCapabilities: [{
+     'name': 'Win8.1/Chrome',
+     'platform': 'Windows 8.1',
+     'browserName': 'chrome',
+     'screenResolution': '1280x1024'
+   }, {
+      'name': 'Win8.1/Firefox',
+      'platform': 'Windows 8.1',
+      'browserName': 'firefox',
+      'screenResolution': '1280x1024'
+    }, {
+      'name': 'Win8.1/IE11',
+      'platform': 'Windows 8.1',
+      'browserName': 'internet explorer',
+      'version': '11.0',
+      'screenResolution': '1280x1024'
+    }],
+  //enable this for local tests without selenium
+  //directConnect: true,
   jasmineNodeOpts: {
       defaultTimeoutInterval: 120000
     },
