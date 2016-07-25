b29458b18fcad82f153df1696752f0b5d7fab68f
Amine Limouri <limouri@dzhw.eu>
2016-07-25 15:23:44 +0200

    #421 fixed protractor test

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -106,12 +106,7 @@ angular
         resolve: {
           authorize: ['Auth', function(Auth) {
             return Auth.authorize();
-          }],
-          translatePartialLoader: ['$translate', '$translatePartialLoader',
-            function($translate, $translatePartialLoader) {
-              $translatePartialLoader.addPart('global');
-            }
-          ]
+          }]
         }
       });
       /*
