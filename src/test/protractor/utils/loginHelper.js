d8168356e45514d3312f055da39bb04564c67df7
Amine Limouri <limouri@dzhw.eu>
2016-08-03 11:58:36 +0200

    #421 fixed ie protractor timeout

--- src/test/protractor/utils/loginHelper.js
+++ src/test/protractor/utils/loginHelper.js
@@ -1,7 +1,6 @@
 /* global element */
 /* global browser */
 /* global by */
-/* global expect */
 
 'use strict';
 
@@ -11,12 +10,11 @@ function login() {
   element(by.id('username')).sendKeys('protractor');
   element(by.id('password')).sendKeys('protractor');
   element(by.css('.form button[type="submit"]')).click();
-  expect(element(by.css('[ng-click="logout()"]')).isPresent()).toBe(true);
 }
 
 function logout() {
   element(by.css('[ng-click="logout()"]')).click();
-  expect(element(by.css('[ng-click="logout()"]')).isPresent()).toBe(false);
+  browser.waitForAngular();
 }
 module.exports.login = login;
 module.exports.logout = logout;
