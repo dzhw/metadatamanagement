05c441de0e5a728908c5d79b1b60f9a17f28dd5b
Daniel Katzberg <katzberg@dzhw.eu>
2016-08-24 12:44:05 +0200

    #619 added fields to Study Domain Object

--- src/main/java/eu/dzhw/fdz/metadatamanagement/studymanagement/domain/Study.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/studymanagement/domain/Study.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import com.google.common.base.MoreObjects;
+
 import eu.dzhw.fdz.metadatamanagement.common.domain.I18nString;
 import eu.dzhw.fdz.metadatamanagement.common.domain.validation.I18nStringSize;
 import eu.dzhw.fdz.metadatamanagement.common.domain.validation.StringLengths;
@@ -21,16 +23,61 @@ import net.karneim.pojobuilder.GeneratePojoBuilder;
      intoPackage = "eu.dzhw.fdz.metadatamanagement.studymanagement.domain.builders")
 public class Study {
   
+  private I18nString title;
+  
+  private I18nString descripion;
+  
+  private I18nString institution;
+  
   @I18nStringSize(max = StringLengths.MEDIUM,
       message = "data-acquisition-project."
           + "error.data-acquisition-project.survey-series.i18n-string-size")
   private I18nString surveySeries;
+  
+  private I18nString sponsor;
+  
+  private I18nString citationHint;
+  
+  private String authors;
+  
+  private List<String> accessWays;
 
   /* Nested Objects */
   @Valid
   private List<Release> releases;
   
+  /* Foreign Keys */
+  private List<String> dataSetIds;
   
+  private List<String> surveyIds;
+  
+  private List<String> instrumentIds;
+  
+  private List<String> relatedPublicationIds;
+  
+  /*
+   * (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+      .add("title", title)
+      .add("descripion", descripion)
+      .add("institution", institution)
+      .add("surveySeries", surveySeries)
+      .add("sponsor", sponsor)
+      .add("citationHint", citationHint)
+      .add("authors", authors)
+      .add("accessWays", accessWays)
+      .add("releases", releases)
+      .add("dataSetIds", dataSetIds)
+      .add("surveyIds", surveyIds)
+      .add("instrumentIds", instrumentIds)
+      .add("relatedPublicationIds", relatedPublicationIds)
+      .toString();
+  }
+
   /* GETTER / SETTER */
   public List<Release> getReleases() {
     return releases;
@@ -46,6 +93,94 @@ public class Study {
 
   public void setSurveySeries(I18nString surveySeries) {
     this.surveySeries = surveySeries;
+  }
+
+  public I18nString getTitle() {
+    return title;
+  }
+
+  public void setTitle(I18nString title) {
+    this.title = title;
+  }
+
+  public I18nString getDescripion() {
+    return descripion;
+  }
+
+  public void setDescripion(I18nString descripion) {
+    this.descripion = descripion;
+  }
+
+  public I18nString getInstitution() {
+    return institution;
+  }
+
+  public void setInstitution(I18nString institution) {
+    this.institution = institution;
+  }
+
+  public I18nString getSponsor() {
+    return sponsor;
+  }
+
+  public void setSponsor(I18nString sponsor) {
+    this.sponsor = sponsor;
+  }
+
+  public I18nString getCitationHint() {
+    return citationHint;
+  }
+
+  public void setCitationHint(I18nString citationHint) {
+    this.citationHint = citationHint;
+  }
+
+  public String getAuthors() {
+    return authors;
+  }
+
+  public void setAuthors(String authors) {
+    this.authors = authors;
+  }
+
+  public List<String> getAccessWays() {
+    return accessWays;
+  }
+
+  public void setAccessWays(List<String> accessWays) {
+    this.accessWays = accessWays;
+  }
+
+  public List<String> getDataSetIds() {
+    return dataSetIds;
+  }
+
+  public void setDataSetIds(List<String> dataSetIds) {
+    this.dataSetIds = dataSetIds;
+  }
+
+  public List<String> getSurveyIds() {
+    return surveyIds;
+  }
+
+  public void setSurveyIds(List<String> surveyIds) {
+    this.surveyIds = surveyIds;
+  }
+
+  public List<String> getInstrumentIds() {
+    return instrumentIds;
+  }
+
+  public void setInstrumentIds(List<String> instrumentIds) {
+    this.instrumentIds = instrumentIds;
+  }
+
+  public List<String> getRelatedPublicationIds() {
+    return relatedPublicationIds;
+  }
+
+  public void setRelatedPublicationIds(List<String> relatedPublicationIds) {
+    this.relatedPublicationIds = relatedPublicationIds;
   }
   
 }
