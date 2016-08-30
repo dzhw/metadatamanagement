a036ce8c1088486b28707ba24a06f14c573cd3ac
Daniel Katzberg <katzberg@dzhw.eu>
2016-08-30 14:03:54 +0200

    #618 updated study builder. added new js files to index.html

--- src/main/webapp/scripts/studymanagement/services/studyBuilder.service.js
+++ src/main/webapp/scripts/studymanagement/services/studyBuilder.service.js
@@ -9,7 +9,42 @@ angular.module('metadatamanagementApp').service('StudyBuilderService',
         var data = studies[i];
         var studyObj = {
           id: data.id,
-          dataAcquisitionProjectId: projectId
+          title: {
+            en: data['title.en'],
+            de: data['title.de']
+          },
+          description: {
+            en: data['description.en'],
+            de: data['description.de']
+          },
+          institution: {
+            en: data['institution.en'],
+            de: data['institution.de']
+          },
+          surveySeries: {
+            en: data['surveySeries.en'],
+            de: data['surveySeries.de']
+          },
+          sponsor: {
+            en: data['sponsor.en'],
+            de: data['sponsor.de']
+          },
+          citationHint: {
+            en: data['citationHint.en'],
+            de: data['citationHint.de']
+          },
+          authors: data.authors,
+          accessWays: CleanJSObjectService.
+          removeWhiteSpace(data.accessWays),
+          dataAcquisitionProjectId: projectId,
+          releases: CleanJSObjectService.
+          removeWhiteSpace(data.releases),
+          studyIds: CleanJSObjectService.
+          removeWhiteSpace(data.studyIds),
+          instrumentIds: CleanJSObjectService.
+          removeWhiteSpace(data.instrumentIds),
+          relatedPublicationIds: CleanJSObjectService.
+          removeWhiteSpace(data.relatedPublicationIds)
         };
         var cleanedStudyObject = CleanJSObjectService
           .removeEmptyJsonObjects(studyObj);
