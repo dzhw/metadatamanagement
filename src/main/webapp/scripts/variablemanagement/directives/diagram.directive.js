8f658dc453d6eed488126c6d37b738e508449389
Amine Limouri <limouri@dzhw.eu>
2016-09-16 14:10:31 +0200

    #616 optimized plotly layout

--- src/main/webapp/scripts/variablemanagement/directives/diagram.directive.js
+++ src/main/webapp/scripts/variablemanagement/directives/diagram.directive.js
@@ -8,22 +8,47 @@ angular.module('metadatamanagementApp').directive('diagram',
       scope.isNotAvailable = false;
       var data = [{
         marker: {
-          color: 'rgb(49,130,189)',
+          color: '#006AB2',
           opacity: 0.7
         }}];
+      var layout = {
+        xaxis: {
+          type: 'category',
+          autotick: true,
+          showticklabels: true,
+          tickangle: 45,
+          tickcolor: '#000'
+        },
+        yaxis: {
+          tickangle: 45,
+        }
+      };
       var drawDiagram = function() {
         $timeout(function() {
           scope.isNotLoaded = false;
-          Plotly.newPlot('diagram', data);
+          Plotly.newPlot('diagram', data, layout);
         }, 1000);
       };
+      function createString(object) {
+        if (object === null) {
+          return '';
+        }
+        return String(object);
+      }
+      function truncate(str, length) {
+        str = createString(str);
+        return str.length > length ? str.slice(0, length) + '...' : str;
+      }
       if ((scope.type === 'nominal') || (scope.type === 'ordinal')) {
         data[0].x = [];
+        data[0].text = [];
         data[0].y = [];
         data[0].type = 'bar';
         if (scope.statistics.validResponses) {
           scope.statistics.validResponses.forEach(function(obj) {
-            data[0].x.push('-' + obj.label[scope.language] + '-');
+            var tempValue = truncate(obj.label[scope.language], 10);
+            data[0].x.push(tempValue);
+            data[0].text.push(obj.label[scope.language]);
             data[0].y.push(obj.absoluteFrequency);
           });
         } else {
