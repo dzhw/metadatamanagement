9be98b5681be5ccc015fed265f9ee3a8e52ff7fd
Daniel Katzberg <katzberg@dzhw.eu>
2016-10-04 14:50:24 +0200

    #679 implented projection

---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/relatedpublicationmanagement/domain/projections/CompleteRelatedPublicationProjection.java
@@ -0,0 +1,41 @@
+package eu.dzhw.fdz.metadatamanagement.relatedpublicationmanagement.domain.projections;
+
+import java.util.List;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import eu.dzhw.fdz.metadatamanagement.common.domain.projections.AbstractRdcDomainObjectProjection;
+import eu.dzhw.fdz.metadatamanagement.relatedpublicationmanagement.domain.RelatedPublication;
+
+/**
+ * The complete projection of a related publication domain object. It return all fields.
+ * 
+ * @author Daniel Katzberg
+ *
+ */
+@Projection(name = "complete", types = RelatedPublication.class)
+interface CompleteRelatedPublicationProjection extends AbstractRdcDomainObjectProjection {
+
+  String getSourceReference();
+
+  String getPublicationAbstract();
+
+  String getDoi();
+
+  String getSourceLink();
+
+  String getTitle();
+
+  List<String> getQuestionIds();
+
+  List<String> getSurveyIds();
+
+  List<String> getVariableIds();
+
+  List<String> getDataSetIds();
+
+  List<String> getStudyIds();
+
+  List<String> getInstrumentIds();
+  
+}
