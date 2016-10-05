4134c2ade1e60514885a44868556bd46bb2b8950
Daniel Katzberg <katzberg@dzhw.eu>
2016-10-05 15:09:26 +0200

    #679 added repository for related publication

---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/relatedpublicationmanagement/repository/RelatedPublicationRepository.java
@@ -0,0 +1,20 @@
+package eu.dzhw.fdz.metadatamanagement.relatedpublicationmanagement.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import eu.dzhw.fdz.metadatamanagement.instrumentmanagement.domain.Instrument;
+import eu.dzhw.fdz.metadatamanagement.relatedpublicationmanagement.domain.RelatedPublication;
+
+/**
+ * Related Publication Repository.
+ * 
+ * @author Daniel Katzberg
+ *
+ */
+@RepositoryRestResource(path = "/related-publications")
+public interface RelatedPublicationRepository extends MongoRepository<Instrument, String>, 
+    QueryDslPredicateExecutor<RelatedPublication> {
+
+}
