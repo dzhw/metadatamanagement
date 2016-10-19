1d52bee017e3d25a9514115b27eb4609a3a2a585
Amine Limouri <limouri@dzhw.eu>
2016-10-19 10:55:48 +0200

    #679 removed regEx for url validation

--- src/main/java/eu/dzhw/fdz/metadatamanagement/common/domain/util/Patterns.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/common/domain/util/Patterns.java
@@ -13,14 +13,4 @@ public interface Patterns {
   String GERMAN_ALPHANUMERIC = "^[A-Za-z0-9äöüÄÖÜß]*$";
   String GERMAN_ALPHANUMERIC_WITH_UNDERSCORE_AND_MINUS = "^[_A-Za-z0-9äöüÄÖÜß\\-]*$";  
   String GERMAN_ALPHANUMERIC_WITH_UNDERSCORE_AND_MINUS_AND_DOT = "^[_A-Za-z0-9äöüÄÖÜß\\-\\.]*$";
-  
-  //The Pattern ist from the URL: https://mathiasbynens.be/demo/url-regex and from @diegoperini 
-  //The Pattern was aftwards escaped special signs for java.
-  String VALID_URL = "_^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127"
-      + "(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\."
-      + "(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?"
-      + "\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z"
-      + "\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}"
-      + "0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5"
-      + "})?(?:/[^\\s]*)?$_iuS";
 }
