f530773d585089972cfcad21c879d4e138b97bfd
René Reitmann <reitmann@dzhw.eu>
2016-11-01 10:19:14 +0100

    Released version v0.1.32 (closes #762)

--- src/main/webapp/scripts/app.constants.js
+++ src/main/webapp/scripts/app.constants.js
@@ -6,7 +6,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'local')
 
-.constant('VERSION', '0.1.32-SNAPSHOT')
+.constant('VERSION', '0.1.32')
 
 .constant('ElasticSearchProperties', {apiVersion:'_default',logLevel:'error',pageSize:10})
 
