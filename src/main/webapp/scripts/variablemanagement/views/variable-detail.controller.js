70cc7927ed928f73acdb7ea88cf1481bfac99f33
Amine Limouri <limouri@dzhw.eu>
2016-11-08 15:15:34 +0100

    #766 reworked related informations

--- src/main/webapp/scripts/variablemanagement/views/variable-detail.controller.js
+++ src/main/webapp/scripts/variablemanagement/views/variable-detail.controller.js
@@ -1,13 +1,38 @@
 'use strict';
 
 angular.module('metadatamanagementApp')
-  .controller('VariableDetailController', function(SurveySearchDialogService,
-    DataSetSearchDialogService, $scope, entity, $state, ShoppingCartService,
-    RelatedPublicationSearchDialogService) {
+  .controller('VariableDetailController', function($scope, entity, $state,
+    SurveySearchDialogService, DataSetSearchDialogService,
+    RelatedPublicationSearchDialogService, QuestionSearchDialogService,
+    SurveySearchResource, DataSetSearchResource, QuestionSearchResource,
+    RelatedPublicationSearchResource, StudySearchResource) {
     $scope.generationCodeToggleFlag = true;
     $scope.notAllRowsVisible = true;
+    $scope.counts = {};
     entity.$promise.then(function(variable) {
       $scope.variable = variable;
+      StudySearchResource
+      .findStudy($scope.variable.dataAcquisitionProjectId)
+      .then(function(study) {
+        $scope.study = study.hits.hits[0]._source;
+      });
+      QuestionSearchResource
+      .findQuestion($scope.variable.questionId)
+      .then(function(question) {
+        console.log(question);
+        $scope.question = question.hits.hits[0]._source;
+      });
+      DataSetSearchResource
+      .getCounts('variableIds', $scope.variable.id)
+      .then(function(dataSetsCount) {
+        $scope.counts.dataSetsCount = dataSetsCount.count;
+      });
+      RelatedPublicationSearchResource
+      .getCounts('variableIds', $scope.variable.id)
+      .then(function(publicationsCount) {
+        $scope.counts.publicationsCount = publicationsCount.count;
+      });
+      $scope.counts.surveysCount =  $scope.variable.surveyIds.length;
     });
     $scope.isRowHidden = function(index) {
       if (index <= 4 || index >= $scope
@@ -23,22 +48,17 @@ angular.module('metadatamanagementApp')
     $scope.toggleGenerationCode = function() {
       $scope.generationCodeToggleFlag = !$scope.generationCodeToggleFlag;
     };
-    $scope.showSurveys = function() {
-      SurveySearchDialogService.findSurveys($scope.variable.surveyIds);
+    $scope.showRelatedSurveys = function() {
+      SurveySearchDialogService.findSurveys('findSurveys',
+      $scope.variable.surveyIds, $scope.counts.surveysCount);
     };
-    $scope.showDataSets = function() {
-      DataSetSearchDialogService.findByVariableId($scope.variable.id);
-    };
-    $scope.showStudy = function() {
-      $state.go('studyDetail', {id: $scope.variable.dataAcquisitionProjectId});
+    $scope.showRelatedDataSets = function() {
+      DataSetSearchDialogService.findDataSets('findByVariableId',
+      $scope.variable.id, $scope.counts.dataSetsCount);
     };
     $scope.showRelatedPublications = function() {
-      RelatedPublicationSearchDialogService
-      .findByVariableId($scope.variable.id);
-    };
-    /* add new  item to localStorage */
-    $scope.addToNotepad = function() {
-      ShoppingCartService
-      .addToShoppingCart($scope.variable.dataAcquisitionProjectId);
+      RelatedPublicationSearchDialogService.
+      findRelatedPublications('findByVariableId', $scope.variable.id,
+      $scope.counts.publicationsCount);
     };
   });
