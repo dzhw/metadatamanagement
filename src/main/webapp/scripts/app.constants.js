14cdd8dc26e6ad12c7c8bd631cbc93daa95954db
René Reitmann <reitmann@dzhw.eu>
2016-11-14 22:02:34 +0100

    Prepared Sprint 34 (closes #799)

--- src/main/webapp/scripts/app.constants.js
+++ src/main/webapp/scripts/app.constants.js
@@ -6,7 +6,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'local')
 
-.constant('VERSION', '0.1.33')
+.constant('VERSION', '0.1.34-SNAPSHOT')
 
 .constant('ElasticSearchProperties', {apiVersion:'_default',logLevel:'error',pageSize:10})
 
