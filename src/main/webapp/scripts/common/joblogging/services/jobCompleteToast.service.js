aac5b371767c77b008e6abb3bae7b0d8c942e216
Daniel Katzberg <katzberg@dzhw.eu>
2016-11-30 09:26:30 +0100

    #828 added comment to job complete toast

--- src/main/webapp/scripts/common/joblogging/services/jobCompleteToast.service.js
+++ src/main/webapp/scripts/common/joblogging/services/jobCompleteToast.service.js
@@ -7,6 +7,10 @@ angular.module('metadatamanagementApp').service('JobCompleteToastService',
 
     //The Toast for the upload complete
     function openJobCompleteToast(resultMessage, translationParams) {
+      //The reason for the time out is the problem with server errors.
+      //The simple toast and the job complete / cancel will be stated on the
+      //same time. This is just a workaround, until stacked toast are supported
+      //by the angular material team.
       $timeout(function() {
         $mdToast.show({
           controller: 'SimpleMessageToastController',
