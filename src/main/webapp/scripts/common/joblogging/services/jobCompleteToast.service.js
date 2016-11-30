b1a6240404a6c45f43ddab61354954f656054b50
Daniel Katzberg <katzberg@dzhw.eu>
2016-11-30 09:24:30 +0100

    #828 moved timeout to jobcomplete. clean up toast services

--- src/main/webapp/scripts/common/joblogging/services/jobCompleteToast.service.js
+++ src/main/webapp/scripts/common/joblogging/services/jobCompleteToast.service.js
@@ -2,25 +2,24 @@
 'use strict';
 
 angular.module('metadatamanagementApp').service('JobCompleteToastService',
-  function($mdToast) {
+  function($mdToast, $timeout) {
     var toastParent = angular.element('#toast-container');
 
     //The Toast for the upload complete
     function openJobCompleteToast(resultMessage, translationParams) {
-      console.log('job complete');
-      $mdToast.hide();
-      console.log($mdToast);
-      $mdToast.show({
-        controller: 'SimpleMessageToastController',
-        templateUrl: 'scripts/common/toast/simple-message-toast.html.tmpl',
-        hideDelay: 0,
-        position: 'top right',
-        parent: toastParent,
-        locals: {
-          messageId: resultMessage,
-          messageParams: translationParams
-        }
-      });
+      $timeout(function() {
+        $mdToast.show({
+          controller: 'SimpleMessageToastController',
+          templateUrl: 'scripts/common/toast/simple-message-toast.html.tmpl',
+          hideDelay: 0,
+          position: 'top right',
+          parent: toastParent,
+          locals: {
+            messageId: resultMessage,
+            messageParams: translationParams
+          }
+        });
+      }, 10);
     }
 
     return {
--- src/main/webapp/scripts/common/toast/simpleMessageToast.service.js
+++ src/main/webapp/scripts/common/toast/simpleMessageToast.service.js
@@ -2,28 +2,22 @@
 'use strict';
 
 angular.module('metadatamanagementApp').service('SimpleMessageToastService',
-  function($mdToast, $timeout) {
+  function($mdToast) {
     var toastParent = angular.element('#toast-container');
 
     //The Toast for the upload complete
     function openSimpleMessageToast(messageId, messageParams) {
-      console.log(toastParent);
-      console.log('simple');
-      console.log($mdToast);
-
-      $timeout(function() {
-        $mdToast.show({
-          controller: 'SimpleMessageToastController',
-          templateUrl: 'scripts/common/toast/simple-message-toast.html.tmpl',
-          hideDelay: 0,
-          position: 'top right',
-          parent: toastParent,
-          locals: {
-            messageId: messageId,
-            messageParams: messageParams
-          }
-        });
-      }, 1000);
+      $mdToast.show({
+        controller: 'SimpleMessageToastController',
+        templateUrl: 'scripts/common/toast/simple-message-toast.html.tmpl',
+        hideDelay: 0,
+        position: 'top right',
+        parent: toastParent,
+        locals: {
+          messageId: messageId,
+          messageParams: messageParams
+        }
+      });
     }
 
     return {
