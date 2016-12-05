ab2016266280dfc1336cf98e0954fcfcffc634ae
Daniel Katzberg <katzberg@dzhw.eu>
2016-12-05 12:44:40 +0100

    #741 activated question post validation for related publications

--- src/main/java/eu/dzhw/fdz/metadatamanagement/relatedpublicationmanagement/service/RelatedPublicationPostValidationService.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/relatedpublicationmanagement/service/RelatedPublicationPostValidationService.java
@@ -1,11 +1,18 @@
 package eu.dzhw.fdz.metadatamanagement.relatedpublicationmanagement.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
 import eu.dzhw.fdz.metadatamanagement.projectmanagement.rest.dto.PostValidationMessageDto;
+import eu.dzhw.fdz.metadatamanagement.questionmanagement.domain.Question;
+import eu.dzhw.fdz.metadatamanagement.questionmanagement.repository.QuestionRepository;
+import eu.dzhw.fdz.metadatamanagement.relatedpublicationmanagement.domain.RelatedPublication;
+import eu.dzhw.fdz.metadatamanagement.relatedpublicationmanagement.repository.RelatedPublicationRepository;
 
 /**
  * This service handels the post-validation of related publications. It checks the foreign keys and 
@@ -19,6 +26,8 @@ import eu.dzhw.fdz.metadatamanagement.projectmanagement.rest.dto.PostValidationM
 public class RelatedPublicationPostValidationService {
 
   /* Repositories for loading data from the repository */
+  @Inject
+  private RelatedPublicationRepository relatedPublicationRepository;
 //  @Inject
 //  private VariableRepository variableRepository;
 //
@@ -30,9 +39,9 @@ public class RelatedPublicationPostValidationService {
 //
 //  @Inject
 //  private InstrumentRepository instrumentRepository;
-//
-//  @Inject
-//  private QuestionRepository questionRepository;
+
+  @Inject
+  private QuestionRepository questionRepository;
 //  
 //  @Inject
 //  private StudyRepository studyRepository;
@@ -45,8 +54,45 @@ public class RelatedPublicationPostValidationService {
   public List<PostValidationMessageDto> postValidate() {
 
     List<PostValidationMessageDto> errors = new ArrayList<>();
+    List<RelatedPublication> relatedPublications = this.relatedPublicationRepository.findAll();
+    
+    for (RelatedPublication relatedPublication : relatedPublications) {
+      errors = this.postValidateQuestions(relatedPublication.getQuestionIds(), 
+          relatedPublication, errors);
+    }
+    
+    return errors;
+  }
 
-    //TODO DKatzberg Implement the post validation for the related publications. 
+  /**
+   * 
+   * @param questionIds A list of ids of all referenced questions.
+   * @param relatedPublication The actual related publication.
+   * @param errors The list with all recognized errors.    
+   * @return The updated list of errors.
+   */
+  private List<PostValidationMessageDto> postValidateQuestions(List<String> questionIds,
+      RelatedPublication relatedPublication, List<PostValidationMessageDto> errors) {
+    
+    //check all referenced question ids
+    for (String questionId : questionIds) {
+      Question question = this.questionRepository.findOne(questionId);
+      
+      //check for exting referenced
+      if (question == null) {
+        String[] information = {questionId, relatedPublication.getId()};
+        errors.add(new PostValidationMessageDto("question-management.error."
+            + "post-validation.question-unknown", Arrays.asList(information)));
+      } else { //All other checks, where question is != null
+        //is the same study id referenced to the related publication
+        if (!relatedPublication.getStudyIds().contains(question.getDataAcquisitionProjectId())) {
+          String[] information = {questionId, relatedPublication.getId(), 
+              question.getDataAcquisitionProjectId()};
+          errors.add(new PostValidationMessageDto("question-management.error."
+              + "post-validation.question-has-not-a-referenced-study", Arrays.asList(information)));
+        }
+      }      
+    }
     
     return errors;
   }
