09c54eabb570d4a64d2c5d0e24d9c42875979b01
Daniel Katzberg <katzberg@dzhw.eu>
2016-12-12 15:40:23 +0100

    #855 updated protrator firefox version to 46.0

--- src/test/protractor/protractor.conf.js
+++ src/test/protractor/protractor.conf.js
@@ -30,7 +30,7 @@ exports.config = {
     'platform': 'Windows 10',
     'browserName': 'firefox',
     'screenResolution': '1280x1024',
-    'version': 'latest'
+    'version': '46.0'
   }, {
     'name': 'Win10/IE',
     'platform': 'Windows 10',
