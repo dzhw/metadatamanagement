3d4786471093d1f174e2d792924a4b47b312b159
René Reitmann <reitmann@dzhw.eu>
2017-01-18 12:56:24 +0100

    #925 removed call to getInnerHtml

--- src/test/protractor/utils/htmlContentHelper.js
+++ src/test/protractor/utils/htmlContentHelper.js
@@ -4,13 +4,13 @@
 function findNotTranslationedStrings(htmlContent, pageUrl) {
   var exp = /\{\{\s*\S*\s*\}\}/g;
   var deferred = protractor.promise.defer();
-  htmlContent.getInnerHtml().then(function(content) {
+  htmlContent.getAttribute('innerHTML').then(function(content) {
     var results = content.match(exp);
     var length = results ? results.length : 0;
     var result = {
       length: length,
       message: 'On [' + pageUrl + '] is/are ' + length +
-      ' not translated string(s)\n' + results
+        ' not translated string(s)\n' + results
     };
     deferred.fulfill(result);
   });
