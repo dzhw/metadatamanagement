e79a3b060ecf7829cf888be6e4585ee55bd40068
Amine Limouri <limouri@dzhw.eu>
2017-01-25 17:08:57 +0100

    #938 added changes

--- src/main/webapp/scripts/common/breadcrumb/directives/breadCrumb.directive.js
+++ src/main/webapp/scripts/common/breadcrumb/directives/breadCrumb.directive.js
@@ -2,11 +2,53 @@
 
 angular.module('metadatamanagementApp').directive('breadCrumb',
   function() {
+    var link = function(scope) {
+      scope.stateNames = {
+        'search': {
+          'de': 'Suche',
+          'en': 'Search'
+        },
+        'data-setDetail': {
+          'de': 'Datensätze',
+          'en': 'Data Sets'
+        },
+        'disclosure': {
+          'de': 'Impressum',
+          'en': 'Disclosure'
+        },
+        'instrumentDetail': {
+          'de': 'Instrumente',
+          'en': 'Instruments'
+        },
+        'questionDetail': {
+          'de': 'Fragen',
+          'en': 'Questions'
+        },
+        'relatedPublicationDetail': {
+          'de': 'Publikationen',
+          'en': 'Publications'
+        },
+        'studyDetail': {
+          'de': 'Studien',
+          'en': 'Studies'
+        },
+        'surveyDetail': {
+          'de': 'Erhebungen',
+          'en': 'Surveys'
+        },
+        'variableDetail': {
+          'de': 'Variablen',
+          'en': 'Variables'
+        }
+      };
+    };
     return {
       restrict: 'E',
+      link: link,
       templateUrl: 'scripts/common/breadcrumb/directives/breadCrumb.html.tmpl',
       scope: {
-        items: '='
+        items: '=',
+        language: '='
       }
     };
   });
