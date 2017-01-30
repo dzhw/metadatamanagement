b0746294fbe4c45a5fd41ac74ee995d11d503f52
Amine Limouri <limouri@dzhw.eu>
2017-01-30 10:07:26 +0100

    #938 fixed protractor

--- src/test/protractor/protractor.conf.js
+++ src/test/protractor/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
     sauceBuild: process.env.TRAVIS_BUILD_NUMBER,
-    baseUrl: 'https://metadatamanagement dev.cfapps.io/',
+    baseUrl: 'https://metadatamanagement-dev.cfapps.io/',
     specs: [
       'home/home.spec.js',
       'disclosure/disclosure.spec.js',
