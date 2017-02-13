6d0c20ce4f27a81dd214020605d7c5b3877db763
Daniel Katzberg <katzberg@dzhw.eu>
2017-02-13 08:41:48 +0100

    Upgraded js-xlsx to 0.8.3 and js-beautify to 1.6.9 (bower.json) #965

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
       'src/main/webapp/bower_components/d3/d3.js',
       'src/main/webapp/bower_components/nvd3/build/nv.d3.js',
       'src/main/webapp/bower_components/angular-nvd3/dist/angular-nvd3.js',
-      'src/main/webapp/bower_components/js-xlsx/dist/xlsx.js',
+      'src/main/webapp/bower_components/js-xlsx/dist/xlsx.core.min.js',
       'src/main/webapp/bower_components/cheet.js/cheet.js',
       'src/main/webapp/bower_components/angular-animate/angular-animate.js',
       'src/main/webapp/bower_components/angular-messages/angular-messages.js',
