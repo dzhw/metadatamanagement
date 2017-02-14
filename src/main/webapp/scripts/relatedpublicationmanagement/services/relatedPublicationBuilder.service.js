356e5e12a7fb069ba7a8af26521e7a3b427cdd59
Daniel Katzberg <katzberg@dzhw.eu>
2017-02-14 10:04:49 +0100

    #743 added id builder for related publication

--- src/main/webapp/scripts/relatedpublicationmanagement/services/relatedPublicationBuilder.service.js
+++ src/main/webapp/scripts/relatedpublicationmanagement/services/relatedPublicationBuilder.service.js
@@ -3,40 +3,42 @@
 
 angular.module('metadatamanagementApp')
   .service('RelatedPublicationBuilderService',
-  function(RelatedPublicationResource, CleanJSObjectService) {
-    var getRelatedPublications = function(relatedPublications) {
-      var relatedPublicationsObjArray = [];
-      for (var i = 0; i < relatedPublications.length; i++) {
-        var data = relatedPublications[i];
-        var relatedPublicationObj = {
-          id: data.id,
-          sourceReference: data.sourceReference,
-          publicationAbstract: data.publicationAbstract,
-          doi: data.doi,
-          sourceLink: data.sourceLink,
-          title: data.title,
-          questionIds: CleanJSObjectService.removeWhiteSpace(data.questionIds),
-          surveyIds: CleanJSObjectService.removeWhiteSpace(data.surveyIds),
-          variableIds: CleanJSObjectService.removeWhiteSpace(data.variableIds),
-          dataSetIds: CleanJSObjectService.removeWhiteSpace(data.dataSetIds),
-          studyIds: CleanJSObjectService.removeWhiteSpace(data.studyIds),
-          instrumentIds: CleanJSObjectService
-          .removeWhiteSpace(data.instrumentIds),
-          authors: data.authors,
-          year: parseInt(data.year),
-          abstractSource: {
-            en: data['abstractSource.en'],
-            de: data['abstractSource.de']
-          }
-        };
-        var cleanedRelatedPublicationObject = CleanJSObjectService
-          .removeEmptyJsonObjects(relatedPublicationObj);
-        relatedPublicationsObjArray[i] =
-          new RelatedPublicationResource(cleanedRelatedPublicationObject);
-      }
-      return relatedPublicationsObjArray;
-    };
-    return {
-      getRelatedPublications: getRelatedPublications
-    };
-  });
+    function(RelatedPublicationResource, CleanJSObjectService,
+      RelatedPublicationIdBuilderService) {
+      var getRelatedPublications = function(relatedPublications) {
+        var relatedPublicationsObjArray = [];
+        for (var i = 0; i < relatedPublications.length; i++) {
+          var data = relatedPublications[i];
+          var relatedPublicationObj = {
+            id: RelatedPublicationIdBuilderService.buildRelatedPublicationId(
+              data.id),
+            sourceReference: data.sourceReference,
+            publicationAbstract: data.publicationAbstract,
+            doi: data.doi,
+            sourceLink: data.sourceLink,
+            title: data.title,
+            questionIds: CleanJSObjectService.removeWhiteSpace(data.questionIds),
+            surveyIds: CleanJSObjectService.removeWhiteSpace(data.surveyIds),
+            variableIds: CleanJSObjectService.removeWhiteSpace(data.variableIds),
+            dataSetIds: CleanJSObjectService.removeWhiteSpace(data.dataSetIds),
+            studyIds: CleanJSObjectService.removeWhiteSpace(data.studyIds),
+            instrumentIds: CleanJSObjectService
+              .removeWhiteSpace(data.instrumentIds),
+            authors: data.authors,
+            year: parseInt(data.year),
+            abstractSource: {
+              en: data['abstractSource.en'],
+              de: data['abstractSource.de']
+            }
+          };
+          var cleanedRelatedPublicationObject = CleanJSObjectService
+            .removeEmptyJsonObjects(relatedPublicationObj);
+          relatedPublicationsObjArray[i] =
+            new RelatedPublicationResource(cleanedRelatedPublicationObject);
+        }
+        return relatedPublicationsObjArray;
+      };
+      return {
+        getRelatedPublications: getRelatedPublications
+      };
+    });
---
+++ src/main/webapp/scripts/relatedpublicationmanagement/services/relatedPublicationIdBuilder.service.js
@@ -0,0 +1,13 @@
+/* Author: Daniel Katzberg */
+'use strict';
+
+angular.module('metadatamanagementApp').service(
+  'RelatedPublicationIdBuilderService',
+  function() {
+    var buildRelatedPublicationId = function(id) {
+      return 'pub-' + id + '!';
+    };
+    return {
+      buildRelatedPublicationId: buildRelatedPublicationId
+    };
+  });
