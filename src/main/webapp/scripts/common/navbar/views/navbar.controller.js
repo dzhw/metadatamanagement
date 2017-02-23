6c53c2db7c53f5d84618e3bae98a304fcbab8537
René Reitmann <reitmann@dzhw.eu>
2017-02-23 10:54:55 +0100

    #668 changed skip navigation to focus content

--- src/main/webapp/scripts/common/navbar/views/navbar.controller.js
+++ src/main/webapp/scripts/common/navbar/views/navbar.controller.js
@@ -25,30 +25,7 @@ angular.module('metadatamanagementApp').controller('NavbarController',
       }
     };
 
-    var findFirstFocusableElement = function(element) {
-      var focusableChild;
-      if (element.tabIndex != null && element.tabIndex > -1 &&
-        // IE gives ALL elements a tabindex === 0 therefore we limit
-        // to the following list of tags
-        (element.nodeName === 'A' || element.nodeName === 'BUTTON' ||
-        element.nodeName === 'INPUT' || element.nodeName === 'SELECT' ||
-        element.nodeName === 'AREA' || element.nodeName === 'TEXTAREA')) {
-        return element;
-      } else {
-        for (var i = 0; i < element.children.length; i++) {
-          focusableChild = findFirstFocusableElement(element.children[i]);
-          if (focusableChild) {
-            return focusableChild;
-          }
-        }
-      }
-    };
-
     $scope.focusContent = function() {
-      var content = $document.find('#content')[0];
-      var element = findFirstFocusableElement(content);
-      if (element) {
-        element.focus();
-      }
+      $document.find('.fdz-content')[0].focus();
     };
   });
