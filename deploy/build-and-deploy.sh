e8c253217b27bf74358f9c962dd3bd6b4d63797e
René Reitmann <reitmann@dzhw.eu>
2017-03-10 15:11:37 +0100

    #916 deploy script now takes username and password as params

--- deploy/build-and-deploy.sh
+++ deploy/build-and-deploy.sh
@@ -6,19 +6,25 @@ if [[ $0 != ./deploy/* ]]; then
   exit -1
 fi
 PROFILE="$1"
+USERNAME="$2"
+PASSWORD="$3"
 if [ -z ${PROFILE} ]; then
   echo "Please provide a valid profile e.g.: $0 dev"
   exit -1
 fi
 echo "Going to run maven build and cloudfoundry deployment with profile: ${PROFILE}"
+if [ -z ${USERNAME} ] || [ -z ${PASSWORD} ]; then
+  cf login -o DZHW -s ${PROFILE}
+else
+  cf login -o DZHW -s ${PROFILE} -u ${USERNAME} -p ${PASSWORD}
+fi
+if [ $? -ne 0 ]; then
+    echo "cf login failed!"
+    exit -1
+fi
 mvn -P${PROFILE} clean package
 if [ $? -ne 0 ]; then
     echo "Maven build failed!"
-    exit -1
-fi
-cf login -o DZHW -s ${PROFILE}
-if [ $? -ne 0 ]; then
-    echo "cf login failed!"
     exit -1
 fi
 cf push -f ./deploy/manifest-${PROFILE}.yml
