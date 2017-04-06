23d29bb229eb79559f7bdf220d9e7dfd9d6614d5
Amine Limouri <limouri@dzhw.eu>
2017-04-06 13:12:39 +0200

    #981 renamed i18n directive

---
+++ src/main/webapp/scripts/common/i18n/directives/display-i18n.directive.js
@@ -0,0 +1,36 @@
+/* global _*/
+'use strict';
+
+angular.module('metadatamanagementApp').directive('displayI18nString',
+    function(LanguageService) {
+        var link = function(scope, element) {
+            var toBeDisplayed;
+            var currentLanguage = LanguageService.getCurrentInstantly();
+            if (_.isObject(scope.displayI18nString) &&
+                !_.isArray(scope.displayI18nString)) {
+              if (scope.displayI18nString[currentLanguage]) {
+                toBeDisplayed = scope.displayI18nString[currentLanguage];
+                element.attr('lang', currentLanguage);
+              } else {
+                var secondLanguage = currentLanguage === 'de' ? 'en' : 'de';
+                toBeDisplayed = scope.displayI18nString[secondLanguage];
+                element.attr('lang', secondLanguage);
+              }
+            }
+            if (!toBeDisplayed) {
+              toBeDisplayed = '';
+            }
+            if (element[0].tagName === 'IMG') {
+              element.attr('alt', toBeDisplayed);
+            } else {
+              element.empty().append(toBeDisplayed + '&nbsp;');
+            }
+          };
+        return {
+          restrict: 'A',
+          link: link,
+          scope: {
+              displayI18nString: '='
+            }
+        };
+      });
