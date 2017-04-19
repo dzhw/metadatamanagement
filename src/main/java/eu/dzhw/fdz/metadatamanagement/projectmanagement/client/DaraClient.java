8918baca701398fa83aaa681e8e14395f0a0bee9
Daniel Katzberg <katzberg@dzhw.eu>
2017-04-19 14:53:13 +0200

    #1035 added empty dara client

---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/projectmanagement/client/DaraClient.java
@@ -0,0 +1,13 @@
+package eu.dzhw.fdz.metadatamanagement.projectmanagement.client;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * The client for the dara registration and unregistration.
+ * @author Daniel Katzberg
+ *
+ */
+@Component
+public class DaraClient {
+
+}
---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/projectmanagement/client/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Metadatamanagement Clients for external apis and programs.
+ * @author Daniel Katzberg
+ *
+ */
+package eu.dzhw.fdz.metadatamanagement.projectmanagement.client;
