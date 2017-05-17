be322af0446328678b8dfcb02b2a18488e44231d
Amine Limouri <limouri@dzhw.eu>
2017-05-17 16:57:18 +0200

    #1084 added changes [removed directive, removed button, added srvice, added template, added checkbox]

--- src/main/webapp/scripts/common/fdzwelcomedialog/directives/fdz-welcome-dialog.controller.js
+++ src/main/webapp/scripts/common/fdzwelcomedialog/directives/fdz-welcome-dialog.controller.js
@@ -2,11 +2,12 @@
 angular.module('metadatamanagementApp').controller('FdzWelcomeDialogController',
   function($scope, $mdDialog, localStorageService, bowser) {
     $scope.bowser = bowser;
-    $scope.closeDialogForEver = function() {
-      localStorageService.set('closeWelcomeDialogForever', true);
-      $scope.closeDialog();
-    };
+    $scope.closeWelcomeDialogForever = false;
+
     $scope.closeDialog = function() {
+      if ($scope.closeWelcomeDialogForever) {
+        localStorageService.set('closeWelcomeDialogForever', true);
+      }
       $mdDialog.hide();
     };
   });
