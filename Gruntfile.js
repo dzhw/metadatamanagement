4af4e7e19190c7f62a08bc86dab0060168630ec4
René Reitmann <reitmann@dzhw.eu>
2017-05-22 10:56:20 +0200

    #1174 reenabled w3c validation

--- Gruntfile.js
+++ Gruntfile.js
@@ -621,21 +621,21 @@ module.exports = function(grunt) {
     'jshint', 'jscs'
   ]);
 
-  grunt.registerTask('builddev', ['test', /*'htmlangular',*/ 'clean:dist',
+  grunt.registerTask('builddev', ['test', 'htmlangular', 'clean:dist',
     'wiredep:app', 'ngconstant:dev',
     'useminPrepare', 'ngtemplates', 'imagemin', 'svgmin',
     'concat', 'copy:fonts', 'copy:dist', 'ngAnnotate', 'cssmin',
     'autoprefixer', 'uglify', 'rev', 'usemin', 'htmlmin'
   ]);
 
-  grunt.registerTask('buildtest', ['test', /*'htmlangular',*/ 'clean:dist',
+  grunt.registerTask('buildtest', ['test', 'htmlangular', 'clean:dist',
     'wiredep:app', 'ngconstant:test',
     'useminPrepare', 'ngtemplates', 'imagemin', 'svgmin',
     'concat', 'copy:fonts', 'copy:dist', 'ngAnnotate', 'cssmin',
     'autoprefixer', 'uglify', 'rev', 'usemin', 'htmlmin'
   ]);
 
-  grunt.registerTask('buildprod', ['test', /*'htmlangular',*/ 'clean:dist',
+  grunt.registerTask('buildprod', ['test', 'htmlangular', 'clean:dist',
     'wiredep:app', 'ngconstant:prod',
     'useminPrepare', 'ngtemplates', 'imagemin', 'svgmin',
     'concat', 'copy:fonts', 'copy:dist', 'ngAnnotate', 'cssmin',
