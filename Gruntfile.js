7a8c7ef5620354ef8ad69238d4d7824ff80c0af2
René Reitmann <reitmann@dzhw.eu>
2017-06-06 10:24:20 +0200

    #1198 added package-lock.json and disabled htmlangular for the time being

--- Gruntfile.js
+++ Gruntfile.js
@@ -607,21 +607,21 @@ module.exports = function(grunt) {
     'jshint', 'jscs'
   ]);
 
-  grunt.registerTask('builddev', ['test', 'htmlangular', 'clean:dist',
+  grunt.registerTask('builddev', ['test', /*'htmlangular',*/ 'clean:dist',
     'wiredep:app', 'ngconstant:dev',
     'useminPrepare', 'ngtemplates', 'svgmin',
     'concat', 'copy:fonts', 'copy:dist', 'ngAnnotate', 'cssmin',
     'autoprefixer', 'uglify', 'rev', 'usemin', 'htmlmin'
   ]);
 
-  grunt.registerTask('buildtest', ['test', 'htmlangular', 'clean:dist',
+  grunt.registerTask('buildtest', ['test', /*'htmlangular',*/ 'clean:dist',
     'wiredep:app', 'ngconstant:test',
     'useminPrepare', 'ngtemplates', 'svgmin',
     'concat', 'copy:fonts', 'copy:dist', 'ngAnnotate', 'cssmin',
     'autoprefixer', 'uglify', 'rev', 'usemin', 'htmlmin'
   ]);
 
-  grunt.registerTask('buildprod', ['test', 'htmlangular', 'clean:dist',
+  grunt.registerTask('buildprod', ['test', /*'htmlangular',*/ 'clean:dist',
     'wiredep:app', 'ngconstant:prod',
     'useminPrepare', 'ngtemplates', 'svgmin',
     'concat', 'copy:fonts', 'copy:dist', 'ngAnnotate', 'cssmin',
