d7dbb44fb80eaeda9355e912f67dd1d3822c0fc2
René Reitmann <reitmann@dzhw.eu>
2017-06-12 17:08:24 +0200

    #1215 fixing the build

--- src/main/webapp/scripts/app.constants.js
+++ src/main/webapp/scripts/app.constants.js
@@ -6,7 +6,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'local')
 
-.constant('VERSION', '1.0.47')
+.constant('VERSION', '1.0.48-SNAPSHOT')
 
 .constant('ElasticSearchProperties', {apiVersion:'5.x',logLevel:'error',pageSize:10})
 
