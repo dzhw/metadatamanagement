5335c69f7e31e15ad66f5ff3c038db6c54c8aa3b
Daniel Katzberg <katzberg@dzhw.eu>
2017-07-11 08:20:17 +0200

    #1245 changed to release version

--- src/main/webapp/scripts/app.constants.js
+++ src/main/webapp/scripts/app.constants.js
@@ -6,7 +6,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'local')
 
-.constant('VERSION', '1.0.49-SNAPSHOT')
+.constant('VERSION', '1.0.49')
 
 .constant('ElasticSearchProperties', {apiVersion:'5.x',logLevel:'error',pageSize:10})
 
