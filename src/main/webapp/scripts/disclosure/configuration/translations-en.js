dead9ce14e842312514cf23d1e1c1f80af58b179
Daniel Katzberg <katzberg@dzhw.eu>
2017-07-11 12:48:33 +0200

    #1272 changed disclosure page

--- src/main/webapp/scripts/disclosure/configuration/translations-en.js
+++ src/main/webapp/scripts/disclosure/configuration/translations-en.js
@@ -17,7 +17,7 @@ angular.module('metadatamanagementApp').config(
         'executive-director-male': 'Executive Director: ',
         'register-court': 'Register Court: Municipal Court',
         'identification-number': 'VAT-Identification Number: ',
-        'responsiblepress-law': 'Person responsible with regard to the press law for the editorial content: ',
+        'responsiblepress-law': 'Person responsible for the editorial content: ',
         'copyright': 'Copyright: ',
         'copyright-general': 'Unless otherwise stated, copyright and all other rights for the internet pages of the Deutsches Zentrum für Hochschul- und Wissenschaftsforschung under <a id="externalLink" href="#!/en/">https://metadata.fdz.dzhw.eu/</a> lie with the DZHW. Further use and passing on of content, even in excerpts, for pedagogical, scientific or private use is allowed within the scope of the proper citation of the source. Permission must first be obtained for the use of copyright material in commercial business.',
         'copyright-employees': 'For the case that inapplicable information is published on our website or that our programmes or data bases contain errors, liability will only be accepted in instances of gross negligence on the part of the DZHW GmbH or its employees.',
