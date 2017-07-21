f3de7798ff8f650edcbdc391fcbee3a7501b5d9a
Daniel Katzberg <katzberg@dzhw.eu>
2017-07-21 07:50:23 +0200

    #1265 merged dynamic github urls feature with the actual implementation

--- src/main/webapp/scripts/common/fdzwelcomedialog/directives/fdz-welcome-dialog.controller.js
+++ src/main/webapp/scripts/common/fdzwelcomedialog/directives/fdz-welcome-dialog.controller.js
@@ -1,8 +1,51 @@
+/* global window */
+/* Author Daniel Katzberg */
 'use strict';
 angular.module('metadatamanagementApp').controller('FdzWelcomeDialogController',
-  function($scope, $mdDialog, localStorageService, bowser) {
+  function($scope, $mdDialog, localStorageService, bowser, $location,
+      $translate) {
     $scope.bowser = bowser;
     $scope.closeWelcomeDialogForever = false;
+
+    $scope.currentUrl = function() {
+
+      var domainManagement = 'crosscutting';
+      var encodedUrl = window.encodeURIComponent($location.absUrl());
+      if (encodedUrl.includes('studies')) {
+        domainManagement = 'studymanagement';
+      }
+      if (encodedUrl.includes('surveys')) {
+        domainManagement = 'surveymanagement';
+      }
+      if (encodedUrl.includes('instruments')) {
+        domainManagement = 'instrumentmanagement';
+      }
+      if (encodedUrl.includes('questions')) {
+        domainManagement = 'questionmanagement';
+      }
+      if (encodedUrl.includes('data-sets') ||
+        encodedUrl.includes('data_sets')) {
+        domainManagement = 'datasetmanagement';
+      }
+      if (encodedUrl.includes('variables')) {
+        domainManagement = 'variablemanagement';
+      }
+      if (encodedUrl.includes('publications')) {
+        domainManagement = 'relatedpublicationmanagement';
+      }
+
+      var sourceLang = $translate
+        .instant('global.navbar-feedback.source');
+      var feedbackUrl = 'https://github.com/dzhw/' +
+      'metadatamanagement/issues/new?' +
+      'body=%0A---------------------------------%0A' + sourceLang + '%3A%0A' +
+      encodedUrl +
+      '%0A---------------------------------' +
+      '&labels[]=type:bug' +
+      '&labels[]=category:' + domainManagement +
+      '&labels[]=prio:?';
+      return feedbackUrl;
+    };
 
     $scope.closeDialog = function() {
       if ($scope.closeWelcomeDialogForever) {
--- src/main/webapp/scripts/common/fdzwelcomedialog/services/fdz-welcome-dialog.service.js
+++ src/main/webapp/scripts/common/fdzwelcomedialog/services/fdz-welcome-dialog.service.js
@@ -2,23 +2,23 @@
 
 angular.module('metadatamanagementApp').service('FdzWelcomeDialogService',
     function($mdDialog, localStorageService, $rootScope) {
-        var showDialog = function(openByNavbarFeedbackButton) {
-          if (!localStorageService.get('closeWelcomeDialogForever') ||
-            openByNavbarFeedbackButton) {
-            $mdDialog.show({
-              templateUrl: 'scripts/common/fdzwelcomedialog/directives/' +
-              'fdz-welcome-dialog.html.tmpl',
-              controller: 'FdzWelcomeDialogController',
-              locals: {
-                bowser: $rootScope.bowser,
-                showCheckbox: !openByNavbarFeedbackButton
-              },
-              clickOutsideToClose: true,
-              closeTo: '#feedBackButton'
-            });
-          }
-        };
-        return {
-          showDialog: showDialog
-        };
-      });
+      var showDialog = function(openByNavbarFeedbackButton) {
+        if (!localStorageService.get('closeWelcomeDialogForever') ||
+          openByNavbarFeedbackButton) {
+          $mdDialog.show({
+            templateUrl: 'scripts/common/fdzwelcomedialog/directives/' +
+            'fdz-welcome-dialog.html.tmpl',
+            controller: 'FdzWelcomeDialogController',
+            locals: {
+              bowser: $rootScope.bowser,
+              showCheckbox: !openByNavbarFeedbackButton
+            },
+            clickOutsideToClose: true,
+            closeTo: '#feedBackButton'
+          });
+        }
+      };
+      return {
+        showDialog: showDialog
+      };
+    });
