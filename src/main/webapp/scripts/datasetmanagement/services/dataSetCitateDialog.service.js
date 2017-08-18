3e5343dfbad7bf1518baf05222271afeac90ada9
Daniel Katzberg <katzberg@dzhw.eu>
2017-08-18 16:00:28 +0200

    #1150 moved dialog to view from directive

--- src/main/webapp/scripts/datasetmanagement/services/dataSetCitateDialog.service.js
+++ src/main/webapp/scripts/datasetmanagement/services/dataSetCitateDialog.service.js
@@ -5,7 +5,7 @@ angular.module('metadatamanagementApp').service('DataSetCitateDialogService',
     function($mdDialog) {
       var showDialog = function(citationHint) {
         $mdDialog.show({
-          templateUrl: 'scripts/datasetmanagement/directives/' +
+          templateUrl: 'scripts/datasetmanagement/views/' +
           'data-set-citate-dialog.html.tmpl',
           controller: 'DataSetCitateDialogController',
           controllerAs: 'ctrl',
