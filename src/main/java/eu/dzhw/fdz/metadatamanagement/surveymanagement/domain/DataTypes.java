db867bec725cd82b2a54eb240b2c189fff8deada
Daniel Katzberg <katzberg@dzhw.eu>
2017-08-28 14:42:18 +0200

    #1258 changed data type string

--- src/main/java/eu/dzhw/fdz/metadatamanagement/surveymanagement/domain/DataTypes.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/surveymanagement/domain/DataTypes.java
@@ -13,9 +13,9 @@ import eu.dzhw.fdz.metadatamanagement.common.domain.I18nString;
 public class DataTypes {
 
   public static final I18nString QUANTITATIVE_DATA = 
-      new I18nString("Quanitative Daten", "Quantitative data");
+      new I18nString("Quanitative Daten", "Quantitative Data");
   public static final I18nString QUALITATIVE_DATA = 
-      new I18nString("Qualitative Daten", "Qualitative data");
+      new I18nString("Qualitative Daten", "Qualitative Data");
   public static final Set<I18nString> ALL =
       Collections.unmodifiableSet(new HashSet<>(
           Arrays.asList(QUANTITATIVE_DATA, QUALITATIVE_DATA)));
