38e7ffb98c2df3f0ee3380457adfedb5590bc448
René Reitmann <reitmann@dzhw.eu>
2017-09-19 11:31:59 +0200

    #1335 releasing version 1.0.54

--- src/main/webapp/scripts/app.constants.js
+++ src/main/webapp/scripts/app.constants.js
@@ -6,7 +6,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'local')
 
-.constant('VERSION', '1.0.54-SNAPSHOT')
+.constant('VERSION', '1.0.54')
 
 .constant('ElasticSearchProperties', {apiVersion:'5.1',logLevel:'error',pageSize:10})
 
