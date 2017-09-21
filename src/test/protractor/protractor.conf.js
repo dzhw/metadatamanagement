91027ef43805ddbbe85bf2f050b4ef0b78185fb1
René Reitmann <reitmann@dzhw.eu>
2017-09-21 17:13:12 +0200

    #1343 doing only a smoke test now (more or less)

--- src/test/protractor/protractor.conf.js
+++ src/test/protractor/protractor.conf.js
@@ -10,14 +10,14 @@ exports.config = {
     //disable these if you want to run tests locally
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
-    sauceBuild: process.env.TRAVIS_BUILD_NUMBER,
+    //sauceBuild: process.env.TRAVIS_BUILD_NUMBER,
     baseUrl: 'https://metadatamanagement-dev.cfapps.io/',
-    rootElement: '[ng-app]',
+    //rootElement: 'html',
     specs: [
       'home/home.spec.js',
-      'disclosure/disclosure.spec.js',
-      'common/navbar.spec.js',
-      'common/toolbar.spec.js'
+      //'disclosure/disclosure.spec.js',
+      //'common/navbar.spec.js',
+      //'common/toolbar.spec.js'
     ],
     multiCapabilities: [{
       'name': 'Win10/Chrome',
@@ -31,13 +31,13 @@ exports.config = {
       'browserName': 'firefox',
       'screenResolution': '1280x1024',
       'version': '54.0'
-    }, /*{
+    }, {
       'name': 'Win10/Edge',
       'platform': 'Windows 10',
       'browserName': 'MicrosoftEdge',
       'version': 'latest',
       'screenResolution': '1280x1024'
-    },*/ {
+    } ,{
       'name': 'Win10/IE11',
       'platform': 'Windows 10',
       'browserName': 'internet explorer',
