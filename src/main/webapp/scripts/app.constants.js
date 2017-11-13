1eb0b2d4d7df599ab3ded799b446d11e967467f9
Daniel Katzberg <katzberg@dzhw.eu>
2017-11-13 15:44:48 +0100

    #1408 new version number

--- src/main/webapp/scripts/app.constants.js
+++ src/main/webapp/scripts/app.constants.js
@@ -6,7 +6,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'local')
 
-.constant('VERSION', '1.0.57-SNAPSHOT')
+.constant('VERSION', '1.0.57')
 
 .constant('ElasticSearchProperties', {apiVersion:'5.1',logLevel:'error',pageSize:10})
 
