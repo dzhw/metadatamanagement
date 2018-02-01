a727bfec7ed87e9d8859dcc33e1017df6d1a824a
René Reitmann <reitmann@dzhw.eu>
2018-02-01 12:45:09 +0100

    #1528 minor fixes for GUI issues

--- src/main/webapp/scripts/surveymanagement/views/choose-previous-survey-version.controller.js
+++ src/main/webapp/scripts/surveymanagement/views/choose-previous-survey-version.controller.js
@@ -1,9 +1,11 @@
+/* global bowser */
 'use strict';
 
 angular.module('metadatamanagementApp')
   .controller('ChoosePreviousSurveyVersionController',
     function(SurveyVersionsResource, surveyId, $scope, $mdDialog,
       LanguageService, $translate) {
+      $scope.bowser = bowser;
       $scope.currentPage = {
         number: 0,
         limit: 5,
