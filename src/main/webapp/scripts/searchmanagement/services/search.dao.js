a21988f7abf52296d4dec2c970519fd1f1afcc1e
Daniel Katzberg <katzberg@dzhw.eu>
2018-02-05 15:51:06 +0100

    #1439 fixed some authorities. fixed search results for data provider

--- src/main/webapp/scripts/searchmanagement/services/search.dao.js
+++ src/main/webapp/scripts/searchmanagement/services/search.dao.js
@@ -299,7 +299,7 @@ angular.module('metadatamanagementApp').service('SearchDao',
         }
 
         //only publisher and data provider see unreleased projects
-        if (!Principal.hasAuthority('ROLE_PUBLISHER') ||
+        if (!Principal.hasAuthority('ROLE_PUBLISHER') &&
           !Principal.hasAuthority('ROLE_DATA_PROVIDER')) {
           query.body.query.bool.filter = [];
           query.body.query.bool.filter.push({
