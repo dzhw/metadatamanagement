5e3dd2ec5c273ab488bb3a1a7a6b68503f5cb905
YPeper <38426839+YPeper@users.noreply.github.com>
2018-05-08 15:54:01 +0200

    #1635 added test case for search and view of publications

--- src/test/robotframework/test.bat
+++ src/test/robotframework/test.bat
@@ -3,4 +3,4 @@ ECHO OFF
 
 
 ::start cmd /k "cd ..\..\.. & robot -P src\test\robotframework\libs -d target\test\robotframework\logs -v BROWSER:firefox .\src\test\robotframework\public_user\"
-start cmd /k "cd ..\..\.. & robot -P src\test\robotframework\libs -d target\test\robotframework\logs -v BROWSER:chrome -t "Robotframework.Public User.Publicationmanagement.Search And View Publication.*" .\src\test\robotframework"
+start cmd /k "cd ..\..\.. & robot -P src\test\robotframework\libs -d target\test\robotframework\logs -v BROWSER:firefox -t "Robotframework.Public User.*" .\src\test\robotframework"
