596a83039ea7c266e094925080c73080b4460d7e
Christian Sdunek <christian.sdunek@edicos.de>
2018-11-26 15:22:02 +0100

    Add DataAcquisitionProjectResource test

---
+++ src/test/javascript/spec/dataacquisitionprojectmanagement/resources/dataAcquisitionProject.spec.js
@@ -0,0 +1,30 @@
+/* global describe */
+/* global beforeEach */
+/* global it */
+/* global inject */
+/* global expect */
+/* global mockApis */
+
+'use strict';
+
+describe('DataAcquisitionProject', function() {
+	var mockDataSetResource;
+	var $httpBackend;
+	var data = {
+		id: 1,
+		name: 'DataAcquisitionProjectResource'
+	};
+	beforeEach(mockApis);
+	beforeEach(inject(function($injector) {
+		$httpBackend = $injector.get('$httpBackend');
+		mockDataSetResource = $injector.get('DataAcquisitionProjectResource');
+		$httpBackend.expectGET(
+		  /api\/data-acquisition-projects\?projection=complete/)
+		  .respond(data);
+	  }));
+	it('should return DataAcquisitionProject resource ', function() {
+		var resource =  mockDataSetResource.get(1);
+		$httpBackend.flush();
+		expect(resource.name).toEqual('DataAcquisitionProjectResource');
+	});
+});
