c21a248669a94cdbcdfb5034872288fb70e65cb6
tgehrke <till.gehrke@edicos.de>
2019-01-08 18:14:15 +0100

    #1902 add method to delete all by projectId into ResourceController

--- src/main/java/eu/dzhw/fdz/metadatamanagement/questionmanagement/rest/QuestionResourceController.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/questionmanagement/rest/QuestionResourceController.java
@@ -3,6 +3,7 @@ package eu.dzhw.fdz.metadatamanagement.questionmanagement.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import eu.dzhw.fdz.metadatamanagement.common.rest.GenericDomainObjectResourceController;
 import eu.dzhw.fdz.metadatamanagement.questionmanagement.domain.Question;
 import eu.dzhw.fdz.metadatamanagement.questionmanagement.repository.QuestionRepository;
+import eu.dzhw.fdz.metadatamanagement.questionmanagement.service.QuestionService;
 
 /**
  * Question REST Controller which overrides default spring data rest methods.
@@ -17,8 +19,10 @@ import eu.dzhw.fdz.metadatamanagement.questionmanagement.repository.QuestionRepo
  * @author René Reitmann
  */
 @RepositoryRestController
-public class QuestionResourceController 
+public class QuestionResourceController
     extends GenericDomainObjectResourceController<Question, QuestionRepository> {
+  @Autowired
+  QuestionService questionService;
 
   @Autowired
   public QuestionResourceController(QuestionRepository questionRepository) {
@@ -34,5 +38,17 @@ public class QuestionResourceController
   @RequestMapping(method = RequestMethod.GET, value = "/questions/{id:.+}")
   public ResponseEntity<Question> findQuestion(@PathVariable String id) {
     return super.findDomainObject(id);
+  }
+
+  /**
+   * delete all questions from data acquisition project.
+   * 
+   * @param projectId the Id of the project.
+   * @return no Content.
+   */
+  @DeleteMapping(value = "/data-acquisition-projects/questions/{projectId}")
+  public ResponseEntity<Question> deleteAllByProjectId(@PathVariable String projectId) {
+    questionService.deleteQuestionsByProjectId(projectId);
+    return ResponseEntity.noContent().build();
   }
 }
