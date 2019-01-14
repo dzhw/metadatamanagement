e56fbe383a13706b49c1260b595ba0c8814f6426
Edgar Schmidt <edgar.schmidt@flavia-it.de>
2019-01-14 10:27:57 +0100

    #1951 Allow data editing for publishers regardless of data provider state. Publishers are only allowed to edit data if the publisher ready flag is not set

--- src/main/webapp/scripts/dataacquisitionprojectmanagement/services/projectUpdateAccessService.js
+++ src/main/webapp/scripts/dataacquisitionprojectmanagement/services/projectUpdateAccessService.js
@@ -3,7 +3,7 @@
 'use strict';
 angular.module('metadatamanagementApp').service(
   'ProjectUpdateAccessService',
-  function(CurrentProjectService, Principal, SimpleMessageToastService) {
+  function(CurrentProjectService, Principal, SimpleMessageToastService, $log) {
 
     var messagePrefix = 'data-acquisition-project-management.error.project' +
       '-update-access.';
@@ -14,7 +14,11 @@ angular.module('metadatamanagementApp').service(
       projectUnreleased: messagePrefix + 'project-released',
       memberOfAssignedGroup: messagePrefix + 'member-of-assigned-group',
       assignedToProject: messagePrefix + 'assigned-to-project',
-      isNotRequired: messagePrefix + 'not-required'
+      isNotRequired: messagePrefix + 'not-required',
+      updateForPublisherAllowed: messagePrefix +
+        'update-for-publishers-allowed',
+      updateForDataProviderAllowed: messagePrefix +
+        'update-for-data-providers-allowed'
     };
 
     var isProjectSelected = function(project) {
@@ -46,38 +50,37 @@ angular.module('metadatamanagementApp').service(
       }
     };
 
-    var isTypeReady = function(project, type) {
-      var isTypeReady = false;
-      if (type) {
-        var conf = project.configuration;
-        switch (type) {
-          case 'studies':
-            isTypeReady = _.get(conf, 'studiesState.dataProviderReady') ||
-              _.get(conf, 'studiesState.publisherReady');
-            break;
-          case 'surveys':
-            isTypeReady = _.get(conf, 'surveysState.dataProviderReady') ||
-              _.get(conf, 'surveysState.publisherReady');
-            break;
-          case 'instruments':
-            isTypeReady = _.get(conf, 'instrumentsState.dataProviderReady') ||
-              _.get(conf, 'instrumentsState.publisherReady');
-            break;
-          case 'data_sets':
-            isTypeReady = _.get(conf, 'dataSetsState.dataProviderReady') ||
-              _.get(conf, 'dataSetsState.publisherReady');
-            break;
-          case 'questions':
-            isTypeReady = _.get(conf, 'questionsState.dataProviderReady') ||
-              _.get(conf, 'questionsState.publisherReady');
-            break;
-          case 'variables':
-            isTypeReady = _.get(conf, 'variablesState.dataProviderReady') ||
-              _.get(conf, 'variablesState.publisherReady');
-            break;
-        }
+    var getProjectConfigStateAttributeForType = function(type) {
+      switch (type) {
+        case 'studies':
+          return 'studiesState';
+        case 'surveys':
+          return 'surveysState';
+        case 'instruments':
+          return 'instrumentsState';
+        case 'data_sets':
+          return 'dataSetsState';
+        case 'questions':
+          return 'questionsState';
+        case 'variables':
+          return 'variablesState';
       }
-      return !isTypeReady;
+    };
+
+    var isPublisherUpdateAllowedForType = function(project, type) {
+      var stateAttribute = getProjectConfigStateAttributeForType(type);
+      return !_.get(project.configuration, stateAttribute + '.publisherReady');
+    };
+
+    var isDataProviderUpdateAllowedForType = function(project, type) {
+
+      if (isPublisherUpdateAllowedForType(project, type)) {
+        var stateAttribute = getProjectConfigStateAttributeForType(type);
+        return !_.get(project.configuration, stateAttribute +
+          '.dataProviderReady');
+      } else {
+        return false;
+      }
     };
 
     var isAssignedToProject = function(project, role) {
@@ -121,23 +124,36 @@ angular.module('metadatamanagementApp').service(
       memberOfAssignedGroup.canContinue = true;
       memberOfAssignedGroup.errorKey = errorList.memberOfAssignedGroup;
 
-      var typeReady = isTypeReady.bind(null, test, type);
-      typeReady.canContinue = true;
-      typeReady.errorKey = errorList.typeUpdateAllowed;
+      var updateForRoleAllowed;
+
+      if (Principal.hasAuthority('ROLE_PUBLISHER')) {
+        updateForRoleAllowed = isPublisherUpdateAllowedForType.bind(null, test,
+          type);
+        updateForRoleAllowed.canContinue = true;
+        updateForRoleAllowed.errorKey = errorList.updateForPublisherAllowed;
+      } else if (Principal.hasAuthority('ROLE_DATA_PROVIDER')) {
+        updateForRoleAllowed = isDataProviderUpdateAllowedForType.bind(null,
+          test, type);
+        updateForRoleAllowed.canContinue = true;
+        updateForRoleAllowed.errorKey = errorList.updateForDataProviderAllowed;
+      } else {
+        $log.warn('User has no supported authority (ROLE_PUBLISHER,' +
+          'ROLE_DATA_PROVIDER). Validation will fail');
+      }
 
       var projectUnreleased = isProjectUnreleased.bind(null, test);
       projectUnreleased.canContinue = true;
       projectUnreleased.errorKey = errorList.projectUnreleased;
 
       var validations = [projectSelected, assignedToProject, typeRequired,
-        memberOfAssignedGroup, typeReady, projectUnreleased];
+        memberOfAssignedGroup, updateForRoleAllowed, projectUnreleased];
 
       for (var i = 0; i < validations.length; i++) {
         try {
           validations[i].isValid = validations[i]();
         } catch (e) {
           isValid = false;
-          console.error('error during validation:', e);
+          $log.debug('error during validation:', e);
           break;
         }
         isValid = isValid && validations[i].isValid;
