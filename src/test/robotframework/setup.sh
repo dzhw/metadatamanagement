5a1ecaedbd0d166a0afd12a849f44349c1bb978f
René Reitmann <reitmann@dzhw.eu>
2019-01-14 12:07:13 +0100

    #1955 rolling back to trusty images

--- src/test/robotframework/setup.sh
+++ src/test/robotframework/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # this script installs all dependencies for executing robotframework tests
+export CXX=g++-4.9
 sudo apt-get update -qq
 sudo apt-get -y install python-pip python-dev --allow-unauthenticated
 pip install urllib3[secure] --user -I
