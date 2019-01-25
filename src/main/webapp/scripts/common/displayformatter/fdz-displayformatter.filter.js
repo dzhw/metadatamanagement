6e9ac298c0c824f0afd21ebfa06e31cfcde11bde
Christian Sdunek <christian.sdunek@edicos.de>
2019-01-25 16:18:21 +0100

    #1973 Allow undocumented question type and add display formatter

---
+++ src/main/webapp/scripts/common/displayformatter/fdz-displayformatter.filter.js
@@ -0,0 +1,12 @@
+/* global _*/
+'use strict';
+
+angular.module('metadatamanagementApp').filter('fdzDisplayFormatter',
+function() {
+  return function(toBeDisplayed) {
+    if (['Nicht erfasst', 'Undocumented'].indexOf(toBeDisplayed) !== -1) {
+      toBeDisplayed = toBeDisplayed + '.'
+    }
+    return toBeDisplayed;
+  };
+});
