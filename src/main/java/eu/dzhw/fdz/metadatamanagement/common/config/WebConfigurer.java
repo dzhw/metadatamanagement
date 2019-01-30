285bca497f283f3ceb2808e2a835148cd83fa352
René Reitmann <reitmann@dzhw.eu>
2019-01-30 15:40:39 +0100

    #1993 add new profile minified

--- src/main/java/eu/dzhw/fdz/metadatamanagement/common/config/WebConfigurer.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/common/config/WebConfigurer.java
@@ -26,8 +26,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Configuration
 @Slf4j
-public class WebConfigurer
-    implements ServletContextInitializer, 
+public class WebConfigurer implements ServletContextInitializer,
     WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
   @Autowired
   private Environment env;
@@ -39,7 +38,8 @@ public class WebConfigurer
     EnumSet<DispatcherType> disps =
         EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
     initCachingHttpHeadersFilter(servletContext, disps);
-    if (env.acceptsProfiles(Profiles.of("!" + Constants.SPRING_PROFILE_LOCAL))) {
+    if (env.acceptsProfiles(Profiles.of("!" + Constants.SPRING_PROFILE_LOCAL + " | ("
+        + Constants.SPRING_PROFILE_LOCAL + " & " + Constants.SPRING_PROFILE_MINIFIED + ")"))) {
       initStaticResourcesProductionFilter(servletContext, disps);
     }
     log.info("Web application fully configured");
@@ -103,7 +103,7 @@ public class WebConfigurer
     cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/dist/*");
     cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/index.html");
     cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/robots.txt");
-    cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/manifest.json");  
+    cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/manifest.json");
     cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/bower_components/*");
     cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/assets/*");
     cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/scripts/*");
