310d5261bff850c55aa343515c60a0e1d104edde
René Reitmann <reitmann@dzhw.eu>
2019-02-11 16:54:17 +0100

    #1867 add project check to user modifications

--- src/main/webapp/scripts/dataacquisitionprojectmanagement/services/dataAcquisitionProjectRepositoryClient.service.js
+++ src/main/webapp/scripts/dataacquisitionprojectmanagement/services/dataAcquisitionProjectRepositoryClient.service.js
@@ -21,7 +21,71 @@ angular.module('metadatamanagementApp').factory(
       });
     };
 
+    var findAssignedProjects = function(login) {
+      return $http({
+        method: 'GET',
+        url: '/api/data-acquisition-projects/search/' +
+          'findAllByConfigurationPublishersContains' +
+          'OrConfigurationDataProvidersContains',
+        params: {
+          login: login,
+          projection: 'complete'
+        },
+        transformResponse: function(data) {
+          var response = angular.fromJson(data);
+          if (response._embedded &&
+              response._embedded.dataAcquisitionProjects) {
+            return response._embedded.dataAcquisitionProjects;
+          }
+          return response;
+        }
+      });
+    };
+
+    var findAssignedProjectsAsDataProvider = function(login) {
+      return $http({
+        method: 'GET',
+        url: '/api/data-acquisition-projects/search/' +
+          'findAllByConfigurationDataProvidersContains',
+        params: {
+          login: login,
+          projection: 'complete'
+        },
+        transformResponse: function(data) {
+          var response = angular.fromJson(data);
+          if (response._embedded &&
+              response._embedded.dataAcquisitionProjects) {
+            return response._embedded.dataAcquisitionProjects;
+          }
+          return response;
+        }
+      });
+    };
+
+    var findAssignedProjectsAsPublisher = function(login) {
+      return $http({
+        method: 'GET',
+        url: '/api/data-acquisition-projects/search/' +
+          'findAllByConfigurationPublishersContains',
+        params: {
+          login: login,
+          projection: 'complete'
+        },
+        transformResponse: function(data) {
+          var response = angular.fromJson(data);
+          if (response._embedded &&
+              response._embedded.dataAcquisitionProjects) {
+            return response._embedded.dataAcquisitionProjects;
+          }
+          return response;
+        }
+      });
+    };
+
     return {
+      findAssignedProjects: findAssignedProjects,
+      findAssignedProjectsAsPublisher: findAssignedProjectsAsPublisher,
+      findAssignedProjectsAsDataProvider: findAssignedProjectsAsDataProvider,
       findByIdLikeOrderByIdAsc: findByIdLikeOrderByIdAsc
     };
   });
