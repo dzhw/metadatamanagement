79cb4ad209230d9b1ab368112f6e110922c08342
René Reitmann <reitmann@dzhw.eu>
2019-02-25 10:24:27 +0100

    #2051 prepare hotfix

--- src/main/webapp/scripts/app.constants.js
+++ src/main/webapp/scripts/app.constants.js
@@ -6,7 +6,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'local')
 
-.constant('VERSION', '1.0.79')
+.constant('VERSION', '1.0.79-HOTFIX')
 
 .constant('ElasticSearchProperties', {apiVersion:'6.3',logLevel:'error',pageSize:10})
 
