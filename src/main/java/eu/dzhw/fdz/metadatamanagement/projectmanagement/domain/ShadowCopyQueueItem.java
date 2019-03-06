95b9e315ec1671c94d2309a4c7e33466c84e08a5
Edgar Schmidt <edgar.schmidt@flavia-it.de>
2019-03-06 09:05:14 +0100

    #1991 Add @Document annotation to ShadowCopyQueueItem in order to set the collection name correctly.

--- src/main/java/eu/dzhw/fdz/metadatamanagement/projectmanagement/domain/ShadowCopyQueueItem.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/projectmanagement/domain/ShadowCopyQueueItem.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 /**
  * Represents a queued shadow copy task of a project.
  */
+@Document(collection = "shadow_copy_queue_item")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
