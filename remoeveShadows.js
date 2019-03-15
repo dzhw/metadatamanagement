72e63bcc1ecc0558e438c747526ded61e17a74cb
René Reitmann <reitmann@dzhw.eu>
2019-03-15 10:45:10 +0100

    Remove unused files

--- remoeveShadows.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var collections = ['data_acquisition_projects', 'data_sets', 'instruments', 'questions',
-    'studies', 'surveys', 'variables'];
-
-  collections.forEach(function(collection) {
-    db[collection].remove({shadow: true});
-  });
-})();
