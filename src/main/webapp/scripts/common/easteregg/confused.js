89d46a35f49caf7990348ee11dafc5d78ef5b2cb
René Reitmann <reitmann@dzhw.eu>
2019-04-02 15:20:53 +0200

    #1942 confuse people

---
+++ src/main/webapp/scripts/common/easteregg/confused.js
@@ -0,0 +1,28 @@
+/* global cheet */
+/* global document */
+'use strict';
+
+var showConfusedGif = function(name) {
+  var node = document.createElement('div');
+  node.className = 'egg-confused';
+  document.body.appendChild(node);
+
+  var img = document.createElement('img');
+  img.src = 'assets/images/' + name + '.gif';
+  node.appendChild(img);
+
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode === 27) {
+      document.removeEventListener('keydown', this);
+      node.remove();
+    }
+  }, false);
+};
+
+cheet('c o n f u s e d space t r a v o l t a', function() {
+  showConfusedGif('travolta');
+});
+
+cheet('c o n f u s e d space a n d y', function() {
+  showConfusedGif('andy');
+});
