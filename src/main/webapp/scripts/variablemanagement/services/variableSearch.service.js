a8c3f92984517c52133b8e4896479919f44d45ef
Edgar Schmidt <edgar.schmidt@flavia-it.de>
2019-04-03 16:27:04 +0200

    #2124 Pass the correct filter object to addShadowCopyFilter in variableSearch service for panel identifier

--- src/main/webapp/scripts/variablemanagement/services/variableSearch.service.js
+++ src/main/webapp/scripts/variablemanagement/services/variableSearch.service.js
@@ -286,7 +286,7 @@ angular.module('metadatamanagementApp').factory('VariableSearchService',
 
         SearchHelperService.addQuery(query, queryterm);
         SearchHelperService.addFilter(query);
-        SearchHelperService.addShadowCopyFilter(query, termFilters);
+        SearchHelperService.addShadowCopyFilter(query, filter);
 
         return ElasticSearchClient.search(query).then(function(result) {
           return result.aggregations.panelIdentifiers.buckets;
