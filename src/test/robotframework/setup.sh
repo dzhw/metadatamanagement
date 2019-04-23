3795aac0d1caf2fef45b9e2d10e84dc691ee0231
René Reitmann <reitmann@dzhw.eu>
2019-04-23 11:27:13 +0200

    Removing python ssl warnings

--- src/test/robotframework/setup.sh
+++ src/test/robotframework/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # this script installs all dependencies for executing robotframework tests
 export CXX=g++-4.9
+export PYTHONWARNINGS="ignore"
 sudo apt-get update -qq
 sudo apt-get -y install python-pip python-dev --allow-unauthenticated
 pip install urllib3[secure]==1.24.1 --user -Iv
