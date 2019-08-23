05adc383e44a8835504ee4f2e5da05d06d0b214a
René Reitmann <reitmann@dzhw.eu>
2019-08-23 12:56:09 +0200

    #2212 fix shadows without successors causing duplicates

---
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/projectmanagement/service/DataAcquisitionProjectVersionsServiceTest.java
@@ -0,0 +1,87 @@
+package eu.dzhw.fdz.metadatamanagement.projectmanagement.service;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import java.time.LocalDateTime;
+
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import eu.dzhw.fdz.metadatamanagement.AbstractTest;
+import eu.dzhw.fdz.metadatamanagement.common.service.JaversService;
+import eu.dzhw.fdz.metadatamanagement.common.unittesthelper.util.UnitTestCreateDomainObjectUtils;
+import eu.dzhw.fdz.metadatamanagement.projectmanagement.domain.DataAcquisitionProject;
+import eu.dzhw.fdz.metadatamanagement.projectmanagement.domain.Release;
+import eu.dzhw.fdz.metadatamanagement.projectmanagement.repository.DataAcquisitionProjectRepository;
+import eu.dzhw.fdz.metadatamanagement.usermanagement.security.AuthoritiesConstants;
+
+@WithMockUser(authorities = AuthoritiesConstants.PUBLISHER)
+public class DataAcquisitionProjectVersionsServiceTest extends AbstractTest {
+  @Autowired
+  private DataAcquisitionProjectVersionsService versionsService;
+
+  @Autowired
+  private DataAcquisitionProjectManagementService projectManagementService;
+
+  @Autowired
+  private DataAcquisitionProjectRepository repository;
+
+  @Autowired
+  private JaversService javersService;
+
+  @After
+  public void tearDown() {
+    repository.deleteAll();
+    javersService.deleteAll();
+  }
+
+  @Test
+  public void testMultipleReleases() {
+    DataAcquisitionProject project = UnitTestCreateDomainObjectUtils.buildDataAcquisitionProject();
+    project = projectManagementService.save(project);
+
+    project.setRelease(Release.builder().version("0.0.1").lastDate(LocalDateTime.now()).build());
+    project = projectManagementService.save(project);
+
+    Release previousRelease =
+        versionsService.findPreviousRelease(project.getId(), project.getRelease());
+    assertNull(previousRelease);
+
+    // unrelease
+    project.setRelease(null);
+    project = projectManagementService.save(project);
+
+    previousRelease = versionsService.findPreviousRelease(project.getId(), null);
+    assertThat(previousRelease.getVersion(), equalTo("0.0.1"));
+
+    // release again
+    project.setRelease(Release.builder().version("0.1.0").lastDate(LocalDateTime.now()).build());
+    project = projectManagementService.save(project);
+
+    previousRelease = versionsService.findPreviousRelease(project.getId(), project.getRelease());
+    assertThat(previousRelease.getVersion(), equalTo("0.0.1"));
+
+    // unrelease
+    project.setRelease(null);
+    project = projectManagementService.save(project);
+
+    previousRelease = versionsService.findPreviousRelease(project.getId(), null);
+    assertThat(previousRelease.getVersion(), equalTo("0.1.0"));
+
+    // release again
+    project.setRelease(Release.builder().version("0.2.0").lastDate(LocalDateTime.now()).build());
+    project = projectManagementService.save(project);
+
+    previousRelease = versionsService.findPreviousRelease(project.getId(), project.getRelease());
+    assertThat(previousRelease.getVersion(), equalTo("0.1.0"));
+
+    // now we have 0.0.1 -> 0.1.0 -> 0.2.0
+    previousRelease = versionsService.findPreviousRelease(project.getId(),
+        Release.builder().version("0.1.0").build());
+    assertThat(previousRelease.getVersion(), equalTo("0.0.1"));
+  }
+}
