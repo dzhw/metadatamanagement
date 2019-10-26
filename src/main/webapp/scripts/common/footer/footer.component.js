224899e117153c9cbbfe898db74510db14753ee8
Michael Spreu <michael.spreu@googlemail.com>
2019-10-26 17:41:42 +0200

    Add footer component

---
+++ src/main/webapp/scripts/common/footer/footer.component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var FooterComponent = {
+  templateUrl: 'scripts/common/footer/footer.html.tmpl'
+};
+
+angular
+  .module('metadatamanagementApp')
+  .component('fdzFooter', FooterComponent);
