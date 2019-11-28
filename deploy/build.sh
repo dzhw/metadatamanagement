215860029f9adf09ecb3e92901e8ecaaa672efe0
René Reitmann <reitmann@dzhw.eu>
2019-11-28 15:39:02 +0100

    fix missing mvn profile for docker push (#2465)

--- deploy/build.sh
+++ deploy/build.sh
@@ -33,6 +33,6 @@ if [ "${TRAVIS_BRANCH}" = "rreitmann/aws-deployment" ]; then
   echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
   echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
   $(aws ecr get-login --no-include-email --region eu-central-1 --profile mdm)
-  mvn dockerfile:push dockerfile:push@push-image-latest
+  mvn -P${PROFILE} dockerfile:push dockerfile:push@push-image-latest
   aws ecs list-services --cluster metadatamanagement-dev --profile mdm | jq -r ".serviceArns[]" | awk '{print "aws ecs update-service --cluster metadatamanagement-dev --profile mdm --force-new-deployment  --service \""$0"\""}' | sh
 fi
