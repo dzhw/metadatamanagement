f79a32a78575b7dd085cdb8dfb04bde3b95eea73
René Reitmann <reitmann@dzhw.eu>
2019-12-17 13:31:05 +0100

    Make run.sh executable (#2474)

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec java -XX:ActiveProcessorCount=$(nproc) -Djava.security.egd=file:/dev/./urandom -classpath /app.war org.springframework.boot.loader.WarLauncher ${@}
+exec java -noverify -XX:ActiveProcessorCount=$(nproc) -Djava.security.egd=file:/dev/./urandom -classpath /app.war org.springframework.boot.loader.WarLauncher ${@}
