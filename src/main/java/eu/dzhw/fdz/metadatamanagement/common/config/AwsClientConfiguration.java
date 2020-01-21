d50a69cef8fe6e21fb7a810f25b07ed0d4283bb5
René Reitmann <reitmann@dzhw.eu>
2020-01-21 11:04:05 +0100

    Change cloudwatch client configuration (#2465)

--- src/main/java/eu/dzhw/fdz/metadatamanagement/common/config/AwsClientConfiguration.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/common/config/AwsClientConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsync;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsyncClientBuilder;
 import com.amazonaws.services.ecs.AmazonECS;
 import com.amazonaws.services.ecs.AmazonECSClientBuilder;
 
@@ -11,11 +13,16 @@ import com.amazonaws.services.ecs.AmazonECSClientBuilder;
  * Configure the AWS API Clients.
  */
 @Configuration
+@Profile({Constants.SPRING_PROFILE_PROD, Constants.SPRING_PROFILE_TEST,
+    Constants.SPRING_PROFILE_DEVELOPMENT})
 public class AwsClientConfiguration {
   @Bean
-  @Profile({Constants.SPRING_PROFILE_PROD, Constants.SPRING_PROFILE_TEST,
-      Constants.SPRING_PROFILE_DEVELOPMENT})
   AmazonECS amazonEcs() {
     return AmazonECSClientBuilder.defaultClient();
+  }
+
+  @Bean
+  AmazonCloudWatchAsync cloudWatchCLient() {
+    return AmazonCloudWatchAsyncClientBuilder.defaultClient();
   }
 }
