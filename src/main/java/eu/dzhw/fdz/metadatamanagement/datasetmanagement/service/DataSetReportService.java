1113a6a3ab42dc9791495ceb12c7926efe4e2adb
René Reitmann <reitmann@dzhw.eu>
2020-05-12 13:32:05 +0200

    #2525 fix missing import

--- src/main/java/eu/dzhw/fdz/metadatamanagement/datasetmanagement/service/DataSetReportService.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/datasetmanagement/service/DataSetReportService.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.loader.tools.RunProcess;
 import org.springframework.core.env.Environment;
