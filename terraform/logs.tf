4b551d7541734eeaccbfadeea1c23cdef84f30ff
René Reitmann <reitmann@dzhw.eu>
2020-05-26 11:57:44 +0200

    #2554 decrease log retention

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -4,7 +4,7 @@
 resource "aws_cloudwatch_log_group" "log_group" {
   count             = length(var.stages)
   name              = "/ecs/metadatamanagement-${var.stages[count.index]}"
-  retention_in_days = 30
+  retention_in_days = 14
 
   tags = {
     Name = "metadatamanagement-${var.stages[count.index]}-log-group"
@@ -14,7 +14,7 @@ resource "aws_cloudwatch_log_group" "log_group" {
 resource "aws_cloudwatch_log_group" "log_group_dataset_report" {
   count             = length(var.stages)
   name              = "/ecs/dataset-report-task-${var.stages[count.index]}"
-  retention_in_days = 30
+  retention_in_days = 14
 
   tags = {
     Name = "dataset-report-task-${var.stages[count.index]}-log-group"
