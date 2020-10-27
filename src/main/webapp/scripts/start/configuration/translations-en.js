6ecc60d9b4000b8f038e39fc0aaba11f5b300505
René Reitmann <reitmann@dzhw.eu>
2020-10-27 10:11:46 +0100

    Change heading on start page (#2479)

--- src/main/webapp/scripts/start/configuration/translations-en.js
+++ src/main/webapp/scripts/start/configuration/translations-en.js
@@ -7,7 +7,7 @@ angular.module('metadatamanagementApp').config(
       'start': {
         'data-search': 'Find Higher Education and Science Research Data Packages',
         'image-title': 'Selected Data',
-        'pinned-data-package-title': 'Most Recent Data Package',
+        'pinned-data-package-title': 'Featured Data Package',
         'fdz-text': '<p>With the data search you can quickly and easily search the metadata of the data packages stored at the fdz.DZHW. This way you will find all the information you need and can order the corresponding data packages directly.</p>',
         'show-all': 'Show all Data Packages'
       }
