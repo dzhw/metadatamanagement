46fb967b4fae601315dc9bfc27f2a813f772fc11
René Reitmann <reitmann@dzhw.eu>
2021-02-08 11:15:01 +0100

    change colors, add bowser (#2236)

--- src/main/webapp/scripts/relatedpublicationmanagement/components/report-publications.controller.js
+++ src/main/webapp/scripts/relatedpublicationmanagement/components/report-publications.controller.js
@@ -2,7 +2,8 @@
 
 angular.module('metadatamanagementApp')
   .controller('ReportPublicationsController',
-    function($scope, $sessionStorage) {
+    function($scope, $sessionStorage, $rootScope) {
+      $scope.bowser = $rootScope.bowser;
       $scope.hideSpeechBubble = $sessionStorage.get(
         'report-publication.hideSpeechBubble') || false;
       $scope.closeSpeechBubble = function() {
