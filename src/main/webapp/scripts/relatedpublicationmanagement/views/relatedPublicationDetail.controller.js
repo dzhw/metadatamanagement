7f2e9c92fcc8da4a8698c4f3d7ac2108c18c8cd6
René Reitmann <reitmann@dzhw.eu>
2021-04-08 16:37:59 +0200

    disable tabs on publication details, improve line breaks (#2841)

--- src/main/webapp/scripts/relatedpublicationmanagement/views/relatedPublicationDetail.controller.js
+++ src/main/webapp/scripts/relatedpublicationmanagement/views/relatedPublicationDetail.controller.js
@@ -19,12 +19,7 @@ angular.module('metadatamanagementApp')
         ctrl.relatedPublication = result;
         // We need this for the tab creation
         ctrl.counts = {};
-        ctrl.counts.dataSetsCount = 0;
-        ctrl.counts.variablesCount = 0;
         ctrl.counts.dataPackagesCount = 0;
-        ctrl.counts.instrumentsCount = 0;
-        ctrl.counts.questionsCount = 0;
-        ctrl.counts.surveysCount = 0;
 
         PageMetadataService.setPageTitle('related-publication-management.' +
         'detail.title', {
