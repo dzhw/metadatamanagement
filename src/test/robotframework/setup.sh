654b77aa542e866f567476bcdd390aeadee8861c
René Reitmann <reitmann@dzhw.eu>
2021-06-16 10:59:56 +0200

    add python 3. setup (#2829)

--- src/test/robotframework/setup.sh
+++ src/test/robotframework/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # this script installs all dependencies for executing robotframework tests
-sudo apt-get update -qq
-sudo apt-get -y install python-pip python-dev --allow-unauthenticated
 pip install robotframework
-pip install robotframework-seleniumlibrary==3.2.0
 pip install robotframework-angularjs
+pip install robotframework-seleniumlibrary==3.2.0
 pip install robotframework-requests
 pip install pyyaml
 pip install six
