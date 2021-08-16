3a0f747c775866d4be226088ce6688ef2e4e080e
René Reitmann <reitmann@dzhw.eu>
2021-08-16 12:58:56 +0200

    minor fixes after testing (#2758)

--- src/main/webapp/scripts/common/i18n/configuration/translations-de.js
+++ src/main/webapp/scripts/common/i18n/configuration/translations-de.js
@@ -441,7 +441,7 @@ angular.module('metadatamanagementApp').config(
               'orcid': 'Sobald Sie den Nachnamen der Person angegeben haben, können Sie eine ORCID nach Vorname und Nachname suchen.'
             },
             'orcid-search': {
-              'title': 'ORCID suchen (Suchparameter: Vorname={{ firstName }}, Nachname={{ lastName }})',
+              'title': 'ORCID suchen',
               'cancel-tooltip': 'Klicken, um den Dialog, ohne eine ORCID auszuwählen, zu schließen.',
               'results-found-text': 'Die folgenden Einträge wurden bei ORCID.org gefunden (Suchparameter: Vorname={{ firstName }}, Nachname={{ lastName }}):',
               'no-results-found-text': 'Es wurden keine Einträge bei ORCID.org gefunden (Suchparameter: Vorname={{ firstName }}, Nachname={{ lastName }}).',
