5190d9d8f5c3f03bb7022e61dbf80740e1167cd6
René Reitmann <reitmann@dzhw.eu>
2021-09-13 09:26:03 +0200

    rollback to java 15 (#2971)

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd app
-exec java -agentpath:$PWD/jvmkill.so -noverify -XX:ActiveProcessorCount=$(nproc) -Djava.security.egd=file:/dev/./urandom --add-opens java.base/java.lang.reflect=ALL-UNNAMED org.springframework.boot.loader.WarLauncher ${@}
+exec java -agentpath:$PWD/jvmkill.so -noverify -XX:ActiveProcessorCount=$(nproc) -Djava.security.egd=file:/dev/./urandom org.springframework.boot.loader.WarLauncher ${@}
