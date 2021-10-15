5457e2440ac77211d90bb45356e4c8267631a3b5
René Reitmann <reitmann@dzhw.eu>
2021-10-15 11:39:46 +0200

    use fix version of selenium library

--- src/test/robotframework/setup.sh
+++ src/test/robotframework/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # this script installs all dependencies for executing robotframework tests
+pip install selenium==3.141.0
 pip install robotframework==4.0.3
-pip install robotframework-angularjs==0.0.10
 pip install robotframework-seleniumlibrary==3.2.0
+pip install robotframework-angularjs==0.0.10
 pip install robotframework-requests==0.9.1
 pip install pyyaml==5.4.1
 pip install six==1.16.0
