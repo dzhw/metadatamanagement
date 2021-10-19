df6e5559e718e1a6d7ea170eeee7cf0be2a979bf
René Reitmann <reitmann@dzhw.eu>
2021-10-19 11:12:10 +0200

    preprare release (#2996)

--- src/main/webapp/scripts/app.constants.js
+++ src/main/webapp/scripts/app.constants.js
@@ -6,7 +6,7 @@ angular.module('metadatamanagementApp')
 
 .constant('ENV', 'local')
 
-.constant('VERSION', '1.0.115-SNAPSHOT')
+.constant('VERSION', '1.0.115')
 
 .constant('ElasticSearchProperties', {apiVersion:'7.x',logLevel:'error',pageSize:10})
 
