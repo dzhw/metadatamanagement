bfa092abae1d4f1259aad877d071ae951248345a
Michael Spreu <mr.spreu@gmail.com>
2021-10-19 14:15:02 +0200

    #2978 Add translations to analysis-package-edit-or-create.html.tmpl

--- src/main/webapp/scripts/analysispackagemanagement/configuration/translations-de.js
+++ src/main/webapp/scripts/analysispackagemanagement/configuration/translations-de.js
@@ -7,11 +7,16 @@ angular.module('metadatamanagementApp').config(
       'analysis-package-management': {
         'detail': {
           'label': {
+            'additional-links': 'Weiterführende Links',
             'analysisPackage': 'Analysepaket',
             'analysisPackages': 'Analysepakete',
+            'annotations': 'Anmerkungen',
             'authors': 'Autor:innen',
             'data-curators': 'Data Curation',
             'doi': 'DOI',
+            'institution': 'Institution',
+            'institutions': 'Institution(en)',
+            'sponsors': 'Gefördert von',
             'title': 'Titel',
             'attachments': {
               'title': 'Titel',
@@ -21,6 +26,20 @@ angular.module('metadatamanagementApp').config(
             }
           },
           'attachments': {
+            'table-title': 'Materialien zu diesem Analysepaket',
+            'delete-attachment-tooltip': 'Klicken, um die Datei "{{ filename }}" zu löschen!',
+            'edit-attachment-tooltip': 'Klicken, um die Datei "{{ filename }}" zu bearbeiten.',
+            'select-attachment-tooltip': 'Klicken, um Datei "{{ filename }}" zum Verschieben auszuwählen.',
+            'move-attachment-up-tooltip': 'Klicken, um die ausgewählte Datei nach oben zu verschieben.',
+            'move-attachment-down-tooltip': 'Klicken, um die ausgewählte Datei nach unten zu verschieben.',
+            'save-attachment-order-tooltip': 'Klicken, um die geänderte Reihenfolge der Dateien zu speichern.',
+            'add-attachment-tooltip': 'Klicken, um einen neue Datei zu diesem Analysepaket hinzuzufügen.',
+            'change-file-tooltip': 'Klicken, um eine Datei auszuwählen.',
+            'open-choose-previous-version-tooltip': 'Klicken, um eine ältere Version der Metadaten wiederherzustellen.',
+            'current-version-restored-toast': 'Die aktuelle Version der Metadaten von Datei "{{ filename }}" wurde wiederhergestellt.',
+            'previous-version-restored-toast': 'Die ältere Version der Metadaten von Datei "{{ filename }}" kann jetzt gespeichert werden.',
+            'language-not-found': 'Keine gültige Sprache gefunden!',
+            'save-analysis-package-before-adding-attachment': 'Das Analysepaket muss erst gespeichert werden, bevor Materialien hinzugefügt werden können.',
             'create-title': 'Neue Datei zu Analysepaket "{{ analysisPackageId }}" hinzufügen',
             'edit-title': 'Datei "{{ filename }}" von Analysepaket "{{ analysisPackageId }}" bearbeiten',
             'attachment-deleted-toast': 'Datei "{{ filename }}" wurde gelöscht!',
@@ -29,45 +48,122 @@ angular.module('metadatamanagementApp').config(
               'filename': 'Wählen Sie eine Datei aus, die Sie des Analysepakets hinzufügen wollen.'
             }
           },
-          'edit': {
-            'add-package-author-tooltip': 'Klicken, um eine neue Autor:in diesem Datenpaket hinzuzufügen.',
-            'delete-package-author-tooltip': 'Klicken, um die Autor:in aus diesem Analysepaket zu löschen.',
-            'move-package-author-up-tooltip': 'Klicken, um die ausgewählte Autor:in nach oben zu verschieben.',
-            'move-package-author-down-tooltip': 'Klicken, um die ausgewählte Autor:in nach unten zu verschieben.',
-            'add-curator-tooltip': 'Klicken, um eine neue Datenkurator:in diesem Analysepaket hinzuzufügen.',
-            'delete-curator-tooltip': 'Klicken, um die ausgewählte Datenkurator:in aus diesem Analysepaket zu löschen.',
-            'move-curator-up-tooltip': 'Klicken, um die ausgewählte Datenkurator:in nach oben zu verschieben.',
-            'move-curator-down-tooltip': 'Klicken, um die ausgewählte Datenkurator:in nach unten zu verschieben.',
-            'choose-unreleased-project-toast': 'Analysepakete dürfen nur bearbeitet werden, wenn das Projekt aktuell nicht freigegeben ist!',
-            'not-authorized-toast': 'Sie sind nicht berechtigt Analysepakete zu bearbeiten oder anzulegen!',
-            'error-on-save-toast': 'Ein Fehler trat beim Speichern von Analysepaket {{analysisPackageId}} auf!',
-            'analysis-package-has-validation-errors-toast': 'Das Analysepaket wurde nicht gespeichert, weil es noch ungültige Felder gibt!',
-            'success-on-save-toast': 'Analysepaket {{analysisPackageId}} wurde erfolgreich gespeichert',
-            'previous-version-restored-toast': 'Die ältere Version von Analysepaket {{ analysisPackageId }} kann jetzt gespeichert werden.',
-            'current-version-restored-toast': 'Die aktuelle Version von Analysepaket {{ analysisPackageId }} wurde wiederhergestellt.',
-            'choose-previous-version': {
-              'next-page-tooltip': 'Klicken, um ältere Versionen anzuzeigen.',
-              'previous-page-tooltip': 'Klicken, um aktuellere Versionen anzuzeigen.',
-              'title': 'Ältere Version des Analysepakets {{ analysisPackageId }} wiederherstellen',
-              'text': 'Wählen Sie eine ältere Analysepaketversion aus, die wiederhergestellt werden soll:',
-              'cancel-tooltip': 'Klicken, um ohne eine ältere Analysepaketversion auszuwählen zurückzukehren.',
-              'no-versions-found': 'Es wurden keine älteren Versionen von Analysepaket {{ analysisPackageId }} gefunden.',
-              'data-package-deleted': 'Das Analysepaket wurde gelöscht!'
-            },
-            'hints': {
-              'authors': {
-                'first-name': 'Geben Sie den Vornamen der Autor:in ein.',
-                'middle-name': 'Geben Sie, falls vorhanden, den zweiten Vornamen der Autor:in ein.',
-                'last-name': 'Geben Sie den Nachnamen der Autor:in ein.'
-              },
-              'curators': {
-                'first-name': 'Geben Sie den Vornamen der Person ein, die an der Datenaufbereitung beteiligt ist.',
-                'middle-name': 'Geben Sie, falls vorhanden, den zweiten Vornamen der Person ein.',
-                'last-name': 'Geben Sie den Nachnamen der Person ein, die an der Datenaufbereitung beteiligt ist.'
-              }
+          'description': 'Analysepaketbeschreibung',
+          'not-found': 'Die id {{id}} referenziert auf eine unbekanntes Analysepaket.',
+        },
+        'edit': {
+          'open-choose-previous-version-tooltip': 'Klicken, um eine ältere Version dieses Analysepakets wieder herzustellen.',
+          'save-tooltip': 'Klicken, um das Analysepaket zu speichern.',
+          'move-sponsor-up-tooltip': 'Klicken, um die ausgewählte Geldgeber:in nach oben zu verschieben.',
+          'move-sponsor-down-tooltip': 'Klicken, um die ausgewählte Geldgeber:in nach unten zu verschieben.',
+          'add-sponsor-tooltip': 'Klicken, um eine weitere Geldgeber:in diesem Analysepaket hinzuzufügen.',
+          'delete-sponsor-tooltip': 'Klicken, um die Geldgeber:in aus diesem Analysepaket zu entfernen.',
+          'move-institution-up-tooltip': 'Klicken, um die ausgewählte Institution nach oben zu verschieben.',
+          'move-institution-down-tooltip': 'Klicken, um die ausgewählte Institution nach unten zu verschieben.',
+          'add-institution-tooltip': 'Klicken, um eine weitere Institution diesem Analysepaket hinzuzufügen.',
+          'add-link-tooltip': 'Klicken, um einen weiteren Link diesem Analysepaket hinzuzufügen.',
+          'move-link-up-tooltip': 'Klicken, um den ausgewählten Link nach oben zu verschieben.',
+          'move-link-down-tooltip': 'Klicken, um den ausgewählten Link nach unten zu verschieben.',
+          'delete-institution-tooltip': 'Klicken, um die Institution aus diesem Analysepaket zu entfernen.',
+          'add-package-author-tooltip': 'Klicken, um eine neue Autor:in diesem Analysepaket hinzuzufügen.',
+          'delete-package-author-tooltip': 'Klicken, um die Autor:in aus diesem Analysepaket zu löschen.',
+          'move-package-author-up-tooltip': 'Klicken, um die ausgewählte Autor:in nach oben zu verschieben.',
+          'move-package-author-down-tooltip': 'Klicken, um die ausgewählte Autor:in nach unten zu verschieben.',
+          'add-curator-tooltip': 'Klicken, um eine neue Datenkurator:in diesem Analysepaket hinzuzufügen.',
+          'delete-curator-tooltip': 'Klicken, um die ausgewählte Datenkurator:in aus diesem Analysepaket zu löschen.',
+          'move-curator-up-tooltip': 'Klicken, um die ausgewählte Datenkurator:in nach oben zu verschieben.',
+          'move-curator-down-tooltip': 'Klicken, um die ausgewählte Datenkurator:in nach unten zu verschieben.',
+          'choose-unreleased-project-toast': 'Analysepakete dürfen nur bearbeitet werden, wenn das Projekt aktuell nicht freigegeben ist!',
+          'not-authorized-toast': 'Sie sind nicht berechtigt Analysepakete zu bearbeiten oder anzulegen!',
+          'error-on-save-toast': 'Ein Fehler trat beim Speichern von Analysepaket {{analysisPackageId}} auf!',
+          'analysis-package-has-validation-errors-toast': 'Das Analysepaket wurde nicht gespeichert, weil es noch ungültige Felder gibt!',
+          'success-on-save-toast': 'Analysepaket {{analysisPackageId}} wurde erfolgreich gespeichert',
+          'previous-version-restored-toast': 'Die ältere Version von Analysepaket {{ analysisPackageId }} kann jetzt gespeichert werden.',
+          'current-version-restored-toast': 'Die aktuelle Version von Analysepaket {{ analysisPackageId }} wurde wiederhergestellt.',
+          'choose-previous-version': {
+            'next-page-tooltip': 'Klicken, um ältere Versionen anzuzeigen.',
+            'previous-page-tooltip': 'Klicken, um aktuellere Versionen anzuzeigen.',
+            'title': 'Ältere Version des Analysepakets {{ analysisPackageId }} wiederherstellen',
+            'text': 'Wählen Sie eine ältere Analysepaketversion aus, die wiederhergestellt werden soll:',
+            'cancel-tooltip': 'Klicken, um ohne eine ältere Analysepaketversion auszuwählen zurückzukehren.',
+            'no-versions-found': 'Es wurden keine älteren Versionen von Analysepaket {{ analysisPackageId }} gefunden.',
+            'data-package-deleted': 'Das Analysepaket wurde gelöscht!'
+          },
+          'label': {
+            'create-analysis-package': 'Analysepaket anlegen:',
+            'edit-analysis-package': 'Analysepaket bearbeiten:',
+            'first-name': 'Vorname',
+            'middle-name': 'Zweiter Vorname',
+            'last-name': 'Nachname',
+            'tags': 'Tags (Schlagwörter) zum Analysepaket',
+            'publication-year': 'Jahr der Veröffentlichung',
+            'institution': 'Institution',
+            'sponsor': 'Geldgeber:in',
+            'location': 'Ort',
+            'additional-links': {
+              'url': 'URL',
+              'display-text': 'Anzeigetext'
             }
           },
-          'not-found': 'Die id {{id}} referenziert auf eine unbekanntes Analysepaket.',
+          'hints': {
+            'annotations': {
+              'de': 'Geben Sie zusätzliche Anmerkungen zum Analysepaket auf Deutsch ein.',
+              'en': 'Geben Sie zusätzliche Anmerkungen zum Analysepaket auf Englisch ein.'
+            },
+            'authors': {
+              'first-name': 'Geben Sie den Vornamen der Autor:in ein.',
+              'middle-name': 'Geben Sie, falls vorhanden, den zweiten Vornamen der Autor:in ein.',
+              'last-name': 'Geben Sie den Nachnamen der Autor:in ein.'
+            },
+            'curators': {
+              'first-name': 'Geben Sie den Vornamen der Person ein, die an der Datenaufbereitung beteiligt ist.',
+              'middle-name': 'Geben Sie, falls vorhanden, den zweiten Vornamen der Person ein.',
+              'last-name': 'Geben Sie den Nachnamen der Person ein, die an der Datenaufbereitung beteiligt ist.'
+            },
+            'description': {
+              'de': 'Geben Sie eine Beschreibung des Analysepakets auf Deutsch ein.',
+              'en': 'Geben Sie eine Beschreibung des Analysepakets auf Englisch ein.'
+            },
+            'institution': {
+              'de': 'Geben Sie den deutschen Namen der Institution ein, die an dem Analysepaket beteiligt waren.',
+              'en': 'Geben Sie den englischen Namen der Institution ein, die an dem Analysepaket beteiligt waren.'
+            },
+            'sponsor': {
+              'de': 'Geben Sie den deutschen Namen der Geldgeber:in für dieses Analysepaket ein.',
+              'en': 'Geben Sie den englischen Namen der Geldgeber:in für dieses Analysepaket ein.'
+            },
+            'title': {
+              'de': 'Geben Sie den Titel des Analysepakets auf Deutsch ein.',
+              'en': 'Geben Sie den Titel des Analysepakets auf Englisch ein.'
+            }
+          }
+        },
+        'error': {
+          'analysis-package': {
+            'annotations': {
+              'i18n-string-size': 'Die Maximallänge der Anmerkungen ist 2048 Zeichen.'
+            },
+            'description': {
+              'not-null': 'Die Beschreibung eines Analysepakets darf nicht leer sein!',
+              'i18n-string-size': 'Die Maximallänge der Beschreibung eines Analysepakets ist 2048 Zeichen.',
+              'i18n-string-not-empty': 'Die Beschreibung eines Analysepakets muss in beiden Sprachen vorhanden sein.'
+            },
+            'institution': {
+              'not-null': 'Die Institution eines Analysepakets darf nicht leer sein!',
+              'i18n-string-size': 'Die Maximallänge der Institution eines Analysepakets ist 512 Zeichen.',
+              'i18n-string-entire-not-empty': 'Die Institution eines Analysepakets muss in beiden Sprachen vorhanden sein.'
+            },
+            'sponsor': {
+              'not-null': 'Die Geldgeber:in eines Analysepakets darf nicht leer sein!',
+              'i18n-string-size': 'Die Maximallänge der Geldgeber:in eines Analysepakets ist 512 Zeichen.',
+              'i18n-string-entire-not-empty': 'Die Geldgeber:in eines Analysepakets muss in beiden Sprachen vorhanden sein.'
+            },
+            'title': {
+              'not-null': 'Der Titel eines Analysepaket darf nicht leer sein!',
+              'i18n-string-size': 'Die Maximallänge des Titels eines Analysepaket ist 2048 Zeichen.',
+              'i18n-string-entire-not-empty': 'Der Titel eines Analysepaket muss in allen Sprachen vorhanden sein.'
+            },
+          }
         }
       }
       //jscs:enable
--- src/main/webapp/scripts/analysispackagemanagement/configuration/translations-en.js
+++ src/main/webapp/scripts/analysispackagemanagement/configuration/translations-en.js
@@ -7,11 +7,16 @@ angular.module('metadatamanagementApp').config(
       'analysis-package-management': {
         'detail': {
           'label': {
+            'additional-links': 'Additional Links',
             'analysisPackage': 'Analysis Package',
             'analysisPackages': 'Analysis Packages',
+            'annotations': 'Annotations',
             'authors': 'Authors',
             'data-curators': 'Datenkuratierung',
             'doi': 'DOI',
+            'institution': 'Institution',
+            'institutions': 'Institution(s)',
+            'sponsors': 'Sponsored by',
             'title': 'Title',
             'attachments': {
               'title': 'Title',
@@ -21,50 +26,143 @@ angular.module('metadatamanagementApp').config(
             }
           },
           'attachments': {
+            'table-title': 'Documents related to this Analysis Package',
+            'delete-attachment-tooltip': 'Click to delete document "{{ filename }}"!',
+            'edit-attachment-tooltip': 'Click to edit the metadata for document "{{ filename }}".',
+            'select-attachment-tooltip': 'Click to select document "{{ filename }}" for moving it up or down.',
+            'move-attachment-down-tooltip': 'Click to move the selected document down.',
+            'move-attachment-up-tooltip': 'Click to move the selected document up.',
+            'save-attachment-order-tooltip': 'Click to save the modified order of the documents.',
+            'add-attachment-tooltip': 'Click to add a new document to this analysis package.',
             'create-title': 'Add new Document to Analysis Package "{{ analysisPackageId }}"',
             'edit-title': 'Modify Document "{{ filename }}" of Analysis Package "{{ analysisPackageId }}"',
             'attachment-deleted-toast': 'Document "{{ filename }}" has been deleted!',
             'attachment-order-saved-toast': 'The modified order of the documents has been saved.',
+            'change-file-tooltip': 'Click to choose a file.',
+            'open-choose-previous-version-tooltip': 'Click to restore a previous version of the metadata.',
+            'current-version-restored-toast': 'Current version of the metadata for document "{{ filename }}" has been restored.',
+            'previous-version-restored-toast': 'Previous version of the metadata for document "{{ filename }}" can be saved now.',
+            'language-not-found': 'No valid language found!',
+            'save-analysis-package-before-adding-attachment': 'The analysis package has to be saved to enable attaching documents.',
             'hints': {
               'filename': 'Choose a file which you want to attach to the analysis package.'
             }
           },
-          'edit': {
-            'delete-package-author-tooltip': 'Click to remove the author from this analysis package.',
-            'move-package-author-up-tooltip': 'Click to move the selected author up.',
-            'move-package-author-down-tooltip': 'Click to move the selected author down.',
-            'add-curator-tooltip': 'Click to add a new data curator to this analysis package.',
-            'delete-curator-tooltip': 'Click to remove the data curator from this analysis package.',
-            'move-curator-up-tooltip': 'Click to move the selected data curator up.',
-            'move-curator-down-tooltip': 'Click to move the selected data curator down.',
-            'choose-unreleased-project-toast': 'Analysis Packages may be edited if and only if the project is currently not released!',
-            'not-authorized-toast': 'You are not authorized to create or edit analysis packages!',
-            'error-on-save-toast': 'An error occurred during saving of Analysis Package {{analysisPackageId}}!',
-            'analysis-package-has-validation-errors-toast': 'The Analysis Package has not been saved because there are invalid fields!',
-            'success-on-save-toast': 'Analysis Package {{analysisPackageId}} has been saved successfully.',
-            'previous-version-restored-toast': 'Previous version of Analysis Package {{ analysisPackageId }} can be saved now.',
-            'current-version-restored-toast': 'Current version of Analysis Package {{ analysisPackageId }} has been restored.',
-            'choose-previous-version': {
-              'title': 'Restore Previous Version of Analysis Package {{ analysisPackageId }}',
-              'text': 'Choose a previous version of this analysis package which shall be restored:',
-              'cancel-tooltip': 'Click to return without choosing a previous analysis package version.',
-              'no-versions-found': 'There are no previous versions of analysis package {{ analysisPackageId }}.',
-              'data-package-deleted': 'The analysis package has been deleted!'
-            },
-            'hints': {
-              'authors': {
-                'first-name': 'Enter the first name of this author.',
-                'middle-name': 'If available enter the middle-name of this author.',
-                'last-name': 'Enter the last name of this author.'
-              },
-              'curators': {
-                'first-name': 'Enter the first name of the person involved in data preparation.',
-                'middle-name': 'If available enter the middle-name of this person.',
-                'last-name': 'Enter the last name of the person involved in data preparation.'
-              }
+          'description': 'Analysis Package Description',
+          'not-found': 'The {{id}} references to an unknown Analysis Package.'
+        },
+        'edit': {
+          'open-choose-previous-version-tooltip': 'Click for restoring a previous version of this analysis package.',
+          'save-tooltip': 'Click to save this analysis package.',
+          'add-package-author-tooltip': 'Click to add a new author to this analysis package.',
+          'move-sponsor-up-tooltip': 'Click to move the selected sponsor up.',
+          'move-sponsor-down-tooltip': 'Click to move the selected sponsor down.',
+          'add-sponsor-tooltip': 'Click to add another sponsor to this analysis package.',
+          'delete-sponsor-tooltip': 'Click to remove the sponsor from this analysis package.',
+          'move-institution-up-tooltip': 'Click to move the selected institution up.',
+          'move-institution-down-tooltip': 'Click to move the selected institution down.',
+          'add-institution-tooltip': 'Click to add another institution to this analysis package.',
+          'add-link-tooltip': 'Click to add another link to this analysis package.',
+          'move-link-up-tooltip': 'Click to move the selected link up.',
+          'move-link-down-tooltip': 'Click to move the selected link down.',
+          'delete-link-tooltip': 'Click to remove the link from this analysis package.',
+          'delete-institution-tooltip': 'Click to remove the institution from this analysis package.',
+          'delete-package-author-tooltip': 'Click to remove the author from this analysis package.',
+          'move-package-author-up-tooltip': 'Click to move the selected author up.',
+          'move-package-author-down-tooltip': 'Click to move the selected author down.',
+          'add-curator-tooltip': 'Click to add a new data curator to this analysis package.',
+          'delete-curator-tooltip': 'Click to remove the data curator from this analysis package.',
+          'move-curator-up-tooltip': 'Click to move the selected data curator up.',
+          'move-curator-down-tooltip': 'Click to move the selected data curator down.',
+          'choose-unreleased-project-toast': 'Analysis Packages may be edited if and only if the project is currently not released!',
+          'not-authorized-toast': 'You are not authorized to create or edit analysis packages!',
+          'error-on-save-toast': 'An error occurred during saving of Analysis Package {{analysisPackageId}}!',
+          'analysis-package-has-validation-errors-toast': 'The Analysis Package has not been saved because there are invalid fields!',
+          'success-on-save-toast': 'Analysis Package {{analysisPackageId}} has been saved successfully.',
+          'previous-version-restored-toast': 'Previous version of Analysis Package {{ analysisPackageId }} can be saved now.',
+          'current-version-restored-toast': 'Current version of Analysis Package {{ analysisPackageId }} has been restored.',
+          'choose-previous-version': {
+            'title': 'Restore Previous Version of Analysis Package {{ analysisPackageId }}',
+            'text': 'Choose a previous version of this analysis package which shall be restored:',
+            'cancel-tooltip': 'Click to return without choosing a previous analysis package version.',
+            'no-versions-found': 'There are no previous versions of analysis package {{ analysisPackageId }}.',
+            'data-package-deleted': 'The analysis package has been deleted!'
+          },
+          'label': {
+            'create-analysis-package': 'Create Analysis Package:',
+            'edit-analysis-package': 'Edit Analysis Package:',
+            'first-name': 'First Name',
+            'middle-name': 'Middle Name',
+            'last-name': 'Last Name',
+            'tags': 'Tags (Keywords) for the Analysis Package',
+            'publication-year': 'Year of Publication',
+            'institution': 'Institution',
+            'sponsor': 'Sponsor',
+            'location': 'Location',
+            'additional-links': {
+              'url': 'URL',
+              'display-text': 'Display Text'
             }
           },
-          'not-found': 'The {{id}} references to an unknown Analysis Package.'
+          'hints': {
+            'authors': {
+              'first-name': 'Enter the first name of this author.',
+              'middle-name': 'If available enter the middle-name of this author.',
+              'last-name': 'Enter the last name of this author.'
+            },
+            'annotations': {
+              'de': 'Enter additional annotations for this analysis package in German.',
+              'en': 'Enter additional annotations for this analysis package in English.'
+            },
+            'curators': {
+              'first-name': 'Enter the first name of the person involved in data preparation.',
+              'middle-name': 'If available enter the middle-name of this person.',
+              'last-name': 'Enter the last name of the person involved in data preparation.'
+            },
+            'description': {
+              'de': 'Enter a description of this analysis package in German.',
+              'en': 'Enter a description of this analysis package in English.'
+            },
+            'institution': {
+              'de': 'Please enter the German name of the institution which participated in the analysis package.',
+              'en': 'Please enter the English name of the institution which participated in the analysis package.'
+            },
+            'sponsor': {
+              'de': 'Enter the German name of the sponsor of this analysis package.',
+              'en': 'Enter the English name of the sponsor of this analysis package.'
+            },
+            'title': {
+              'de': 'Please enter the title of this analysis package in German.',
+              'en': 'Please enter the title of this analysis package in English.'
+            },
+          }
+        },
+        'error': {
+          'analysis-package': {
+            'annotations': {
+              'i18n-string-size': 'The max length of the annotations is 2048 signs.'
+            },
+            'description': {
+              'not-null': 'The description of the analysis package must not be empty!',
+              'i18n-string-size': 'The max length of the analysis package description is 2048.',
+              'i18n-string-not-empty': 'The description of the analysis package must not be empty for all languages.'
+            },
+            'institution': {
+              'not-null': 'The institution of the analysis package must not be empty!',
+              'i18n-string-size': 'The max length of the institution is 512.',
+              'i18n-string-entire-not-empty': 'The institution of the analysis package must not be empty for both languages.'
+            },
+            'sponsor': {
+              'not-null': 'The sponsor of the analysis package must not be empty!',
+              'i18n-string-size': 'The max length of the sponsor of the analysis package is 512.',
+              'i18n-string-entire-not-empty': 'The sponsor of the analysis package must not be empty for both languages.'
+            },
+            'title': {
+              'not-null': 'The title of the analysis package must not be empty!',
+              'i18n-string-size': 'The max length of the analysis package title is 2048.',
+              'i18n-string-entire-not-empty': 'The title of the analysis package must not be empty for all languages.'
+            },
+          }
         }
       }
       //jscs:enable
