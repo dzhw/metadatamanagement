3a4f87c4a29b857416553e3c0783e75d86857ad5
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-10-25 15:55:29 +0200

    Add findAllByAuthoritiesContaining to AuthUserService

--- src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/service/AuthUserService.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/service/AuthUserService.java
@@ -1,12 +1,29 @@
 package eu.dzhw.fdz.metadatamanagement.authmanagement.service;
 
+import eu.dzhw.fdz.metadatamanagement.usermanagement.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class AuthUserService {
+
+  /**
+   * TODO implement logic
+   *
+   * TODO For now usermanagement.domain.User will be used. An authmanagement version of this class will be created
+   * once the usermanagement User can be removed without compile issues.
+   *
+   * @param authority
+   * @return
+   */
+  public List<User> findAllByAuthoritiesContaining(String authority) {
+    throw new IllegalStateException("Not implemented!");
+  }
+
 
 }
