f5bf513d37ae0789f9b026e843f0f087bdf85350
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-10-25 15:53:09 +0200

    Create AuthUserService Service

---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/service/AuthUserService.java
@@ -0,0 +1,12 @@
+package eu.dzhw.fdz.metadatamanagement.authmanagement.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AuthUserService {
+
+}
