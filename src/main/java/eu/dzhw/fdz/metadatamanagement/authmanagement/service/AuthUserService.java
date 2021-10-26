396d591a856ec54ae901cb0b3de69eec8abd6afb
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-10-26 12:59:29 +0200

    Add AuthUserService#findAllByLoginIn

--- src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/service/AuthUserService.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/service/AuthUserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * A Service which will fetch user related info from a specified Authentication Server.
@@ -29,6 +30,16 @@ public class AuthUserService {
    * @return A list of Users which have the provided authority
    */
   public List<AuthUser> findAllByAuthoritiesContaining(String authority) {
+    throw new IllegalStateException("Not implemented!");
+  }
+
+  /**
+   * TODO implement logic and add documentation
+   *
+   * @param logins the logins used to search for Users
+   * @return all users that have a "login" in the logins parameter
+   */
+  public List<AuthUser> findAllByLoginIn(Set<String> logins) {
     throw new IllegalStateException("Not implemented!");
   }
 
