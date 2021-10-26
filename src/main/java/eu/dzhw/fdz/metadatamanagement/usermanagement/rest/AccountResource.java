aca7cac85d60ba46b3cb3480b6eaa4ff727cc91f
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-10-26 15:08:38 +0200

    Remove update password logic
    NOTE: This commit also removes UserRepository and
    UserService

--- src/main/java/eu/dzhw/fdz/metadatamanagement/usermanagement/rest/AccountResource.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/usermanagement/rest/AccountResource.java
@@ -2,18 +2,13 @@ package eu.dzhw.fdz.metadatamanagement.usermanagement.rest;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.CacheControl;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import eu.dzhw.fdz.metadatamanagement.usermanagement.rest.dto.UserDto;
-import eu.dzhw.fdz.metadatamanagement.usermanagement.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,8 +21,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AccountResource {
 
-  private final UserService userService;
-
   /**
    * Check if the user is authenticated, and return its login.
    */
@@ -36,23 +29,5 @@ public class AccountResource {
   public ResponseEntity<String> isAuthenticated(HttpServletRequest request) {
     log.debug("REST request to check if the current user is authenticated");
     return ResponseEntity.ok().cacheControl(CacheControl.noCache()).body(request.getRemoteUser());
-  }
-
-  /**
-   * Changes the current user's password.
-   */
-  @RequestMapping(value = "/account/change-password", method = RequestMethod.POST,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<?> changePassword(@RequestBody String password) {
-    if (!checkPasswordLength(password)) {
-      return new ResponseEntity<>("Incorrect password", HttpStatus.BAD_REQUEST);
-    }
-    userService.changePassword(password);
-    return new ResponseEntity<>(HttpStatus.OK);
-  }
-
-  private boolean checkPasswordLength(String password) {
-    return !StringUtils.isEmpty(password) && password.length() >= UserDto.PASSWORD_MIN_LENGTH
-        && password.length() <= UserDto.PASSWORD_MAX_LENGTH;
   }
 }
--- src/test/java/eu/dzhw/fdz/metadatamanagement/usermanagement/rest/AccountResourceTest.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/usermanagement/rest/AccountResourceTest.java
@@ -1,44 +1,27 @@
 package eu.dzhw.fdz.metadatamanagement.usermanagement.rest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import eu.dzhw.fdz.metadatamanagement.AbstractTest;
 import eu.dzhw.fdz.metadatamanagement.common.unittesthelper.util.UnitTestUserManagementUtils;
-import eu.dzhw.fdz.metadatamanagement.usermanagement.repository.UserRepository;
-import eu.dzhw.fdz.metadatamanagement.usermanagement.service.UserService;
 
 /**
  * Test class for the AccountResource REST controller.
  *
  * @author Daniel Katzberg
  *
- * @see UserService
  */
 public class AccountResourceTest extends AbstractTest {
-
-  @Autowired
-  private UserRepository userRepository;
-
-  @Autowired
-  private UserService userService;
-
-  @Mock
-  private UserService mockUserService;
 
   private MockMvc restUserMockMvc;
 
@@ -49,10 +32,10 @@ public class AccountResourceTest extends AbstractTest {
     MockitoAnnotations.initMocks(this);
 
     AccountResource accountResource =
-        new AccountResource(userService);
+        new AccountResource();
 
     AccountResource accountUserMockResource =
-        new AccountResource(mockUserService);
+        new AccountResource();
 
     this.restMvc = MockMvcBuilders.standaloneSetup(accountResource).build();
     this.restUserMockMvc = MockMvcBuilders.standaloneSetup(accountUserMockResource).build();
@@ -61,8 +44,6 @@ public class AccountResourceTest extends AbstractTest {
   @AfterEach
   public void cleanUp() {
     UnitTestUserManagementUtils.logout();
-    // assert that all tests remove their users!
-    assertThat(userRepository.count()).isEqualTo(4);
   }
 
   @Test
@@ -80,29 +61,4 @@ public class AccountResourceTest extends AbstractTest {
         .andExpect(content().string("test"));
   }
 
-
-  @Test
-  public void testChangePassword() throws Exception {
-    // Arrange
-
-    // Act
-
-    // Assert
-    this.restMvc.perform(
-        post("/api/account/change-password").accept(MediaType.APPLICATION_JSON).content("password"))
-        .andExpect(status().isOk());
-  }
-
-  @Test
-  public void testChangePasswordError() throws Exception {
-    // Arrange
-
-    // Act
-
-    // Assert
-    this.restMvc
-        .perform(
-            post("/api/account/change-password").accept(MediaType.APPLICATION_JSON).content("no"))
-        .andExpect(status().is4xxClientError());
-  }
 }
