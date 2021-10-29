d519577f5516867069ea4c86f04961e7e074a1b7
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-10-29 10:51:31 +0200

    Add Oauth2ResourceServerConfiguration

---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/config/Oauth2ResourceServerConfiguration.java
@@ -0,0 +1,95 @@
+package eu.dzhw.fdz.metadatamanagement.authmanagement.config;
+
+import eu.dzhw.fdz.metadatamanagement.authmanagement.security.AuthoritiesConstants;
+import eu.dzhw.fdz.metadatamanagement.authmanagement.security.Http401UnauthorizedEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * TODO add documentation.
+ */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class Oauth2ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private Http401UnauthorizedEntryPoint unauthorizedEntryPoint;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .requiresChannel()
+          .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+          .requiresSecure()
+      .and()
+          .sessionManagement()
+          .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          .enableSessionUrlRewriting(false)
+      .and().headers().frameOptions().disable()
+      .and().exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint)
+      .and().csrf().ignoringAntMatchers(
+          "/api/**",
+          "/management/**"
+      )
+      .and().authorizeRequests(
+          authorize ->
+              authorize
+              .mvcMatchers(
+                "/api/orders/**",
+                "/public/files/**",
+                "/api/variables/**",
+                "/api/surveys/**",
+                "/api/i18n/**",
+                "/api/instruments/**",
+                "/api/data-packages/**",
+                "/api/analysis-packages/**",
+                "/api/concepts/**",
+                "/api/study-serieses/**",
+                "/api/related-publications/**",
+                "/api/data-acquisition-projects/**/releases",
+                "/api/data-acquisition-projects/**/attachments",
+                "/api/swagger-ui/**",
+                "/api/swagger-ui.html",
+                "/api/api-docs/**"
+              ).permitAll()
+              .mvcMatchers(
+                "/management/info",
+                "/management/metrics",
+                "/management/prometheus",
+                "/management/health/**"
+              ).permitAll()
+              .mvcMatchers("/api/**").authenticated()
+              .mvcMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+      )
+      .oauth2ResourceServer().jwt();
+  }
+
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring()
+        .mvcMatchers(
+            "/scripts/**/*.{js,html}",
+            "/node_modules/**",
+            "/websocket/**",
+            "/i18n/**",
+            "/assets/**"
+        )
+        .mvcMatchers(
+            HttpMethod.GET,
+            "/management/info",
+            "/api/search/**"
+        )
+        .mvcMatchers(
+            HttpMethod.POST,
+            "/api/search/**/_search",
+            "/api/search/**/_mget",
+            "/api/search/**/_count"
+      );
+  }
+}
