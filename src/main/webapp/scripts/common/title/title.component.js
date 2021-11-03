77e56f207dad3b5cf7a62340fdff540a90fdf82f
Michael Spreu <mr.spreu@gmail.com>
2021-11-03 10:09:19 +0100

    #2978 Implement title component

--- src/main/webapp/scripts/common/title/title.component.js
+++ src/main/webapp/scripts/common/title/title.component.js
@@ -1,20 +1,48 @@
+//  title Component
+//
+//  Component bindings:
+//  index                 boolean   Used for naming and switch between templates
+//                                   template
+//  name                  string    Used for naming:
+//                                  name="{{$ctrl.name}}TitleDe_{{$ctrl.index}}"
+//  currentForm            form
+//  translationKeyPackage  string    eg. analysis-package
+//  translationKeyName     string    eg. license
+//  maxLength              integer
+//  content                model     eg. ctrl.analysisPackage.example = ''
+//                                   or for bilingual:
+//                                   ctrl.analysisPackage.example = {
+//                                     de: '',
+//                                     en: ''
+//                                   }
+//
+//                                  content must be declared in the parent
+//                                  controller
+
 (function() {
   'use strict';
 
   var Component = {
     controller: 'titleController',
-    templateUrl: 'scripts/common/' +
-      'title/title.html.tmpl',
     bindings: {
       index: '<',
       name: '@',
-      notNull: '<',
       currentForm: '<',
       translationKeyPackage: '@',
       translationKeyName: '@',
       maxLength: '<',
       content: '='
-    }
+    },
+    templateUrl: ['$attrs', function($attrs) {
+      var tmpl = {
+        false: 'scripts/common/title/title.html.tmpl',
+        true: 'scripts/common/title/title-with-index.html.tmpl'
+      };
+      if ($attrs.index) {
+        return tmpl.true;
+      }
+      return tmpl.false;
+    }]
   };
 
   angular
