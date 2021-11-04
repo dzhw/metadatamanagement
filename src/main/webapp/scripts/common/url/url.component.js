24281c1c8c4d1f3142410ec5df7a563f3266842b
Michael Spreu <mr.spreu@gmail.com>
2021-11-04 16:15:38 +0100

    #2978 Change additionalLink section to component. Add dynamic template handling to url component

--- src/main/webapp/scripts/common/url/url.component.js
+++ src/main/webapp/scripts/common/url/url.component.js
@@ -13,15 +13,18 @@
       content: '='
     },
     templateUrl: ['$attrs', function($attrs) {
-      var tmpl = {
-        false: 'scripts/common/url/url.html.tmpl',
-        true: 'scripts/common/url/url-with-index.html.tmpl'
-      };
-      if ($attrs.index) {
-        return tmpl.true;
-      }
-      return tmpl.false;
+      return $attrs.templateUrl;
     }]
+    // templateUrl: ['$attrs', function($attrs) {
+    //   var tmpl = {
+    //     false: 'scripts/common/url/url.html.tmpl',
+    //     true: 'scripts/common/url/url-with-index.html.tmpl'
+    //   };
+    //   if ($attrs.index) {
+    //     return tmpl.true;
+    //   }
+    //   return tmpl.false;
+    // }]
   };
 
   angular
