4762fc33f8156bc0efa0643c732cded77383f38e
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-11-12 09:52:00 +0100

    Improve SecurityUtils#getCurrentUserLogin

--- src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/security/SecurityUtils.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/security/SecurityUtils.java
@@ -18,18 +18,9 @@ public final class SecurityUtils {
    * Get the login of the current user.
    */
   public static String getCurrentUserLogin() {
-    SecurityContext securityContext = SecurityContextHolder.getContext();
-    Authentication authentication = securityContext.getAuthentication();
-    String userName = null;
-    if (authentication != null) {
-      if (authentication.getPrincipal() instanceof UserDetails) {
-        UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
-        userName = springSecurityUser.getUsername();
-      } else if (authentication.getPrincipal() instanceof String) {
-        userName = (String) authentication.getPrincipal();
-      }
-    }
-    return userName;
+    var authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    return authentication != null ? authentication.getName() : null;
   }
 
   /**
