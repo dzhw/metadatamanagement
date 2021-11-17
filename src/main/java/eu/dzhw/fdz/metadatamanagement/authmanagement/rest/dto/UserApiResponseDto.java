12dc6978628d956bb1353d65b69cdcb9334f03d5
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-11-17 15:39:43 +0100

    Add generic and wrappers to UserApiResponseDto

--- src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/rest/dto/UserApiResponseDto.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/rest/dto/UserApiResponseDto.java
@@ -1,6 +1,7 @@
 package eu.dzhw.fdz.metadatamanagement.authmanagement.rest.dto;
 
 import eu.dzhw.fdz.metadatamanagement.authmanagement.common.dto.UserDto;
+import eu.dzhw.fdz.metadatamanagement.authmanagement.common.dto.UserWithRolesDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,19 @@ import java.util.List;
  */
 @Getter
 @Setter
-public class UserApiResponseDto {
+public class UserApiResponseDto<T extends UserDto> {
 
-  private List<UserDto> data;
+  private List<T> data;
+
+  /**
+   * Convenience wrapper class for user API responses that should convert the Response Body to
+   * a {@link UserDto}.
+   */
+  public static final class Users extends UserApiResponseDto<UserDto> {}
+
+  /**
+   * Convenience wrapper class for user API responses that should convert the Response Body to
+   * a {@link UserWithRolesDto}.
+   */
+  public static final class UsersWithRoles extends UserApiResponseDto<UserWithRolesDto> {}
 }
