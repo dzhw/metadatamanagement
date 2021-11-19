ffe66a25103de5be403134ce5e48b6c5093f18b8
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-11-19 10:12:01 +0100

    Reintroduce UserResource and UserResourceTests

---
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/authmanagement/rest/UserResource.java
@@ -0,0 +1,80 @@
+package eu.dzhw.fdz.metadatamanagement.authmanagement.rest;
+
+import java.util.List;
+
+import eu.dzhw.fdz.metadatamanagement.authmanagement.common.dto.UserDto;
+import eu.dzhw.fdz.metadatamanagement.authmanagement.common.dto.UserWithRolesDto;
+import eu.dzhw.fdz.metadatamanagement.authmanagement.security.AuthoritiesConstants;
+import eu.dzhw.fdz.metadatamanagement.authmanagement.service.UserApiService;
+import eu.dzhw.fdz.metadatamanagement.authmanagement.service.exception.InvalidResponseException;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * REST controller for managing users.
+ */
+@RestController
+@RequestMapping("/api")
+@Slf4j
+@RequiredArgsConstructor
+public class UserResource {
+
+  private final UserApiService userApiService;
+
+  /**
+   * Get the "login" user with less details.
+   */
+  @RequestMapping(value = "/users/{login}/public", method = RequestMethod.GET,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  @Secured({AuthoritiesConstants.ADMIN, AuthoritiesConstants.DATA_PROVIDER,
+      AuthoritiesConstants.PUBLISHER})
+  public ResponseEntity<UserWithRolesDto> getUserPublic(@PathVariable String login) {
+    log.debug("REST request to get User (with roles) : {}", login);
+    try {
+      var user = userApiService.findOneWithAuthoritiesByLogin(login);
+      return user.map(userDto -> ResponseEntity.ok()
+          .cacheControl(CacheControl.noCache())
+          .body(
+              userDto
+          )).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    } catch (InvalidResponseException e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  /**
+   * Search for privileged users.
+   */
+  @RequestMapping(value = "/users/findUserWithRole", method = RequestMethod.GET,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  @Secured({AuthoritiesConstants.ADMIN, AuthoritiesConstants.DATA_PROVIDER,
+      AuthoritiesConstants.PUBLISHER})
+  public ResponseEntity<List<UserDto>> findUserWithRole(
+      @RequestParam String login,
+      @RequestParam String role
+  ) {
+    try {
+      return ResponseEntity.ok()
+          .cacheControl(CacheControl.noCache().mustRevalidate()).body(
+              userApiService.findAllByLoginLikeOrEmailLikeAndByAuthoritiesContaining(
+                  login,
+                  login,
+                  role
+              )
+          );
+    } catch (InvalidResponseException e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
---
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/authmanagement/rest/UserResourceTests.java
@@ -0,0 +1,132 @@
+package eu.dzhw.fdz.metadatamanagement.authmanagement.rest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import eu.dzhw.fdz.metadatamanagement.authmanagement.security.AuthoritiesConstants;
+import eu.dzhw.fdz.metadatamanagement.authmanagement.service.AbstractUserApiTests;
+import eu.dzhw.fdz.metadatamanagement.authmanagement.service.utils.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import eu.dzhw.fdz.metadatamanagement.common.rest.TestUtil;
+
+/**
+ * Test class for the UserResource REST controller.
+ *
+ * @author Daniel Katzberg
+ *
+ * @see UserResource
+ */
+public class UserResourceTests extends AbstractUserApiTests {
+
+  @Autowired
+  private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+  private MockMvc restUserMockMvc;
+
+  @BeforeEach
+  public void setup() {
+    UserResource userResource =
+        new UserResource(this.userApiService);
+    this.restUserMockMvc = MockMvcBuilders.standaloneSetup(userResource)
+        .setCustomArgumentResolvers(pageableArgumentResolver).build();
+  }
+
+  @BeforeEach
+  public void addUsers() {
+    this.mockServer.users(
+        new User(
+            "user",
+            "user@local",
+            "de",
+            false,
+            "user"
+        ),
+        new User(
+            "admin",
+            "admin@local",
+            "de",
+            false,
+            "admin"
+        )
+    );
+  }
+
+  @Test
+  @WithMockUser(authorities = AuthoritiesConstants.PUBLISHER)
+  public void findUserWithRole() throws Exception {
+    final var USER_LOGIN = "user";
+    final var USER_ROLE = AuthoritiesConstants.USER;
+    final var ADMIN_EMAIL = "admin@local";
+    final var ADMIN_ROLE = AuthoritiesConstants.ADMIN;
+
+    this.addFindAllByLoginLikeOrEmailLikeAndByAuthoritiesContainingRequest(
+        USER_LOGIN,
+        USER_LOGIN,
+        AuthoritiesConstants.toSearchValue(USER_ROLE)
+    );
+    this.addFindAllByLoginLikeOrEmailLikeAndByAuthoritiesContainingRequest(
+        ADMIN_EMAIL,
+        ADMIN_EMAIL,
+      AuthoritiesConstants.toSearchValue(ADMIN_ROLE)
+    );
+    this.addFindAllByLoginLikeOrEmailLikeAndByAuthoritiesContainingRequest(
+        USER_LOGIN,
+        USER_LOGIN,
+      AuthoritiesConstants.toSearchValue(ADMIN_ROLE)
+    );
+
+    // search with login;
+    restUserMockMvc
+        .perform(get("/api/users/findUserWithRole/")
+            .param("login", USER_LOGIN)
+            .param("role", USER_ROLE)
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.[0].login").value(USER_LOGIN));
+
+    // search with email substring
+    restUserMockMvc
+        .perform(get("/api/users/findUserWithRole/")
+            .param("login", ADMIN_EMAIL)
+            .param("role", ADMIN_ROLE)
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.[0].login").value("admin"));
+    // search for user with false role
+
+    restUserMockMvc
+        .perform(get("/api/users/findUserWithRole/")
+            .param("login", USER_LOGIN)
+            .param("role", ADMIN_ROLE)
+            .contentType(TestUtil.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andExpect(content().json("[]"));
+
+  }
+
+  @Test
+  @WithMockUser(authorities = AuthoritiesConstants.PUBLISHER)
+  public void testGetUserPublic() throws Exception {
+    this.addFindOneWithAuthoritiesByLoginRequest("unknown");
+    this.addFindOneWithAuthoritiesByLoginRequest("admin");
+
+    restUserMockMvc.perform(get("/api/users/unknown/public")
+          .accept(MediaType.APPLICATION_JSON))
+          .andExpect(status().isNotFound());
+    restUserMockMvc.perform(get("/api/users/admin/public")
+          .accept(MediaType.APPLICATION_JSON))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.login").value("admin"));
+  }
+}
