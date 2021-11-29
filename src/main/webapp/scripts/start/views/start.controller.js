e9fa40836729de322d870d6471c8ab49dd17848e
Michael Spreu <mr.spreu@gmail.com>
2021-11-29 13:26:14 +0100

    #3025 Add number of packages

--- src/main/webapp/scripts/start/views/start.controller.js
+++ src/main/webapp/scripts/start/views/start.controller.js
@@ -3,7 +3,7 @@
 
   function StartController(
     $scope, LanguageService, Principal, $state, PageMetadataService,
-    PinnedDataPackagesService) {
+    PinnedDataPackagesService, SearchDao) {
     PageMetadataService.setPageTitle('start.data-search');
     PageMetadataService.setPageDescription('start.fdz-text');
 
@@ -11,6 +11,11 @@
       $state.go('search');
       return;
     }
+    SearchDao.search('', 0, null, null, 'data_packages', 0, null, null, null,
+      'relevance', false, 'analysis_packages').then(function(data) {
+        $scope.dataPackagesCount = data.responses[0].hits.total.value;
+        $scope.analysisPackagesCount = data.responses[1].hits.total.value;
+    });
 
     PinnedDataPackagesService.getPinnedDataPackage().then(
       function(response) {
