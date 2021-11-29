56a30e8a3353d554b8d03a45e50cdaf985c5c9be
jschwoerer <1652235+jschwoerer@users.noreply.github.com>
2021-11-29 15:43:45 +0100

    Revert "Temporary commit to make MockJwtDecoder primary"
    This reverts commit 146d8086a39e8de797304ba4186c8f5f060dcb97.

--- src/test/java/eu/dzhw/fdz/metadatamanagement/authmanagement/config/MockJwtDecoder.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/authmanagement/config/MockJwtDecoder.java
@@ -1,12 +1,10 @@
 package eu.dzhw.fdz.metadatamanagement.authmanagement.config;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 
-@Primary
 @Component
 public class MockJwtDecoder implements JwtDecoder {
 
