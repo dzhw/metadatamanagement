db091f08b6d2036b091bf458351c38c464be2ade
Kai Stenkamp <19388988+kaisten@users.noreply.github.com>
2021-12-07 08:52:03 +0100

    authinterceptor

--- src/main/webapp/scripts/usermanagement/auth/auth.interceptor.js
+++ src/main/webapp/scripts/usermanagement/auth/auth.interceptor.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('metadatamanagementApp').factory('authInterceptor', function(
-  localStorageService, $injector) {
+  localStorageService, $injector, $q) {
   return {
     // Add authorization token to headers to all API requests
     request: function(config) {
@@ -14,7 +14,7 @@ angular.module('metadatamanagementApp').factory('authInterceptor', function(
         var AuthServiceProvider = $injector.get('AuthServiceProvider');
         // check expire timestamp if token is valid for more than one minute
         if (AuthServiceProvider.hasToken() && AuthServiceProvider.accessTokenInfo().exp < new Date(Date.now() - 60000) / 1000) {
-          return AuthServiceProvider.refreshToken().then(function (){
+          return AuthServiceProvider.refreshToken().then(function() {
             config.headers.Authorization = 'Bearer ' + localStorageService.get('tokens').access_token;
             return config;
           });
@@ -27,6 +27,21 @@ angular.module('metadatamanagementApp').factory('authInterceptor', function(
         return config;
       }
       //jscs:enable
+    },
+    responseError: function(response) {
+      if (response.status === 401 && (response.config.url.indexOf('/api/') === 0 ||
+        response.config.url.indexOf('api/') === 0 ||
+        response.config.url.indexOf('/management/') === 0 ||
+        response.config.url.indexOf('management/') === 0)) {
+
+        var AuthServiceProvider = $injector.get('AuthServiceProvider');
+        AuthServiceProvider.logout();
+
+        return response;
+      } else {
+        return $q.reject(response);
+      }
+
     }
   };
 });
