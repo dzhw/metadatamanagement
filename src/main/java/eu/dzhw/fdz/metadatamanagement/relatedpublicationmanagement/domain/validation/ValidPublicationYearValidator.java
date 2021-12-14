99b9ad88e0f79a5ca621a06e400e0451aeb41541
René Reitmann <reitmann@dzhw.eu>
2021-12-14 12:51:08 +0100

    remove future constraint (#3048)

--- src/main/java/eu/dzhw/fdz/metadatamanagement/relatedpublicationmanagement/domain/validation/ValidPublicationYearValidator.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/relatedpublicationmanagement/domain/validation/ValidPublicationYearValidator.java
@@ -1,7 +1,5 @@
 package eu.dzhw.fdz.metadatamanagement.relatedpublicationmanagement.domain.validation;
 
-import java.time.LocalDate;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
@@ -34,9 +32,7 @@ public class ValidPublicationYearValidator
     if (relatedPublication.getYear() == null) {
       return true;
     }
-    LocalDate date = LocalDate.now();
-    return date.getYear() >= relatedPublication.getYear() 
-        && relatedPublication.getYear() > 1960;
+    return relatedPublication.getYear() > 1960;
   }
 
 }
