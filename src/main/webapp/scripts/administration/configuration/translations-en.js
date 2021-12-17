01ae1f203f8c984415c379f6216d8c83c34af066
Kai Stenkamp <19388988+kaisten@users.noreply.github.com>
2021-12-17 14:51:50 +0100

    userApi translation

--- src/main/webapp/scripts/administration/configuration/translations-en.js
+++ src/main/webapp/scripts/administration/configuration/translations-en.js
@@ -24,7 +24,8 @@ angular.module('metadatamanagementApp').config(
             'dara': 'Dara',
             'messageBroker': 'Message Broker (for Websockets)',
             'rabbit': 'RabbitMQ',
-            'seo4Ajax': 'Seo4Ajax (Prerender as a Service)'
+            'seo4Ajax': 'Seo4Ajax (Prerender as a Service)',
+            'userApi': 'DLP User-API'
           },
           'table': {
             'service': 'Service name',
