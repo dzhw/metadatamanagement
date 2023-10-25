a2dc282022a1fbf1d9faba6dc6d61acccde8a7a3
theresa <tmoeller@codematix.de>
2023-10-25 14:13:36 +0200

    #3220: implemented collapsable license fields on detail page

--- mdm-frontend/src/app/legacy/common/details/detail.component.js
+++ mdm-frontend/src/app/legacy/common/details/detail.component.js
@@ -4,6 +4,21 @@
   var DetailComponent = {
     controller: ['$scope', '$rootScope', function($scope, $rootScope) {
       $scope.bowser = $rootScope.bowser;
+      $scope.expandLicenses = {};
+
+      /**
+       * Creates a mapping of the available license fields
+       * to set if they are expanded or collapsed. This ensures they 
+       * can be expanded seperately.
+       * @param {*} index the index of the analysis data package
+       */
+      $scope.toggleLicenses = function(index) {
+        if (index in $scope.expandLicenses) {
+          $scope.expandLicenses[index] = !$scope.expandLicenses[index]
+        } else {
+          $scope.expandLicenses[index] = true
+        }
+      };
     }],
     bindings: {
       lang: '<',
