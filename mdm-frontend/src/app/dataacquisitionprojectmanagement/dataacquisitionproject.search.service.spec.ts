9200a824c1e4b40bb963521114bf2d81ad840f15
theresa <tmoeller@codematix.de>
2023-11-24 14:10:15 +0100

    #3224: fixed indentations

--- mdm-frontend/src/app/dataacquisitionprojectmanagement/dataacquisitionproject.search.service.spec.ts
+++ mdm-frontend/src/app/dataacquisitionprojectmanagement/dataacquisitionproject.search.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { DataacquisitionprojectSearchService } from './dataacquisitionproject.search.service';
 
 describe('DataacquisitionprojectSearchService', () => {
-  let service: DataacquisitionprojectSearchService;
+    let service: DataacquisitionprojectSearchService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DataacquisitionprojectSearchService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(DataacquisitionprojectSearchService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
