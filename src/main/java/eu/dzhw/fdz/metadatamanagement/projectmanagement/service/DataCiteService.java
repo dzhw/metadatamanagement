77ab21f26b79e4c6ad62c0c062b4a4d62ae6e3c7
theresa <tmoeller@codematix.de>
2025-05-23 15:06:06 +0200

    #3240: removed unused import

--- src/main/java/eu/dzhw/fdz/metadatamanagement/projectmanagement/service/DataCiteService.java
+++ src/main/java/eu/dzhw/fdz/metadatamanagement/projectmanagement/service/DataCiteService.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
